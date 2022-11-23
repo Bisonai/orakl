8c8f2d58f212d70987bcf75c40c8c77d81807730
Martin Kersner <martin@bisonai.com>
2022-11-23 11:24:10 +0900

    Connect Event Listener and Worker through BullMQ (#18)
    feat: Receive job through BullMQ in naive worker

--- contracts/scripts/emit_mock.ts
+++ contracts/scripts/emit_mock.ts
@@ -1,25 +1,29 @@
 import { ethers } from 'hardhat'
+import assert from 'node:assert'
 
 async function main() {
   const [owner] = await ethers.getSigners()
 
+  const count = 2
   const addresses = [
     '0xe78A0F7E598Cc8b0Bb87894B0F60dD2a88d6a8Ab',
     '0x5b1869D9A4C187F2EAa108f3062412ecf0526b24'
   ]
+  const specIds = [...Array(count)].map((i) => ethers.utils.id(String(i)))
+  const requesters = [...Array(count)].map(() => ethers.Wallet.createRandom().address)
+  const payments = [...Array(count)].map(() => Math.floor(Math.random() * 10))
 
-  for (let i = 0; i < addresses.length; ++i) {
+  assert(addresses.length == count)
+  assert(specIds.length == count)
+  assert(requesters.length == count)
+  assert(payments.length == count)
+
+  for (let i = 0; i < count; ++i) {
     const EventEmitter = await ethers.getContractFactory('EventEmitterMock')
     const eventEmitter = await EventEmitter.attach(addresses[i])
 
-    // parameters
-    const specId = ethers.utils.id(12)
-    const requester = owner.address
-    const payment = 123
-    console.log(requester)
-
     const nonce = await ethers.provider.getTransactionCount(owner.address)
-    const tx = await eventEmitter.buildRequest(specId, requester, payment, { nonce })
+    const tx = await eventEmitter.buildRequest(specIds[i], requesters[i], payments[i], { nonce })
     const txReceipt = await tx.wait()
     console.log(txReceipt)
   }
---
+++ core/src/utils.ts
@@ -0,0 +1,17 @@
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+export function buildBullMqConnection() {
+  // FIXME Move to separate factory file?
+  return {
+    connection: {
+      host: process.env.REDIS_HOST || 'localhost',
+      port: Number(process.env.REDIS_PORT || 6379)
+    }
+  }
+}
+
+export function buildQueueName() {
+  return 'worker-request-queue'
+}
