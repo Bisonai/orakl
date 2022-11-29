681e1b7c369cec7ec1c2a6355f8bd4b5472c462a
Zahid Ahmed <zahidzahmed@gmail.com>
2022-11-29 11:19:12 +0400

    Buffer Library (#26)
    * feat: oracle codebase issue fixed
    
    * feat: stable rebase for library
    
    * feat: encoding fixed numeric
    
    * feat: binary object representation

--- contracts/src/v0.1/Oracle.sol
+++ contracts/src/v0.1/Oracle.sol
@@ -3,26 +3,29 @@ pragma solidity ^0.8.16;
 
 contract ICNOracle {
   // Mapping to store completion statuses for requests to verify
-  mapping(uint256 => bool) public jobStatuses;
+
+  mapping(uint256 => bool) public s_jobStatuses;
 
   // Mapping to store results of requests done
-  mapping(uint256 => bytes32) public jobResults;
+  mapping(uint256 => bytes32) public s_jobResults;
 
-  uint256 jobId;
+  uint256 s_jobId;
 
   event NewJob(uint256 jobId, string url);
 
   function createNewJob(string calldata url) external {
-    emit NewJob(jobId, url);
-    jobId++;
+
+    emit NewJob(s_jobId, url);
+    s_jobId++;
   }
 
   function fulfillJob(bytes32 data, uint256 _jobId) external {
-    jobResults[_jobId] = data;
-    jobStatuses[_jobId] = true;
+    s_jobResults[_jobId] = data;
+    s_jobStatuses[_jobId] = true;
   }
 
   function getData(uint256 _jobId) external view returns (bytes32) {
-    return jobResults[_jobId];
+    return s_jobResults[_jobId];
+
   }
 }
