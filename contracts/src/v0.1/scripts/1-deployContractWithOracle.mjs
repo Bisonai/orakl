95748e036a0488efbc26f60adb7ece1603177b59
Martin Kersner <martin@bisonai.com>
2022-12-01 18:09:28 +0900

    feat: Split `eventListener` test file to two

---
+++ contracts/src/v0.1/scripts/1-deployContractWithOracle.mjs
@@ -0,0 +1,17 @@
+import Web3 from 'web3'
+import pkg from 'hardhat'
+const { ethers } = pkg
+
+async function main() {
+  let OracleContract = await ethers.getContractFactory('ICNOracle')
+  let ICNOracle = await OracleContract.deploy()
+  await ICNOracle.deployed()
+  console.log('Deployed ICNOracle Address:', ICNOracle.address)
+
+  let UserContract = await ethers.getContractFactory('ICNMock')
+  UserContract = await UserContract.deploy(ICNOracle.address)
+  await UserContract.deployed()
+  console.log('Deployed User Contract Address:', UserContract.address)
+}
+
+main()
---
+++ contracts/src/v0.1/scripts/2-requestDataFromOracle.mjs
@@ -0,0 +1,14 @@
+import Web3 from 'web3'
+import pkg from 'hardhat'
+const { ethers } = pkg
+
+async function main() {
+  let UserContract = await ethers.getContractFactory('ICNMock')
+  UserContract = await UserContract.attach('0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512')
+  console.log('Loaded Contract Address:', UserContract.address)
+
+  await UserContract.requestData()
+  console.log('Data Requested')
+}
+
+main()
