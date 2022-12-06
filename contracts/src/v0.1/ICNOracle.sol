867e9fbdc1b0f46c764e65baaaa3336657d16086
Zahid Ahmed <zahidzahmed@gmail.com>
2022-12-06 22:09:18 +0400

    feat: Oracle fulfilment for ICNOracle

--- contracts/src/v0.1/ICNOracle.sol
+++ contracts/src/v0.1/ICNOracle.sol
@@ -3,7 +3,13 @@ pragma solidity ^0.8.16;
 
 import './interfaces/IOracle.sol';
 
+error RequestAlreadyExists();
+error IncorrectRequestFulfillment();
+
 contract ICNOracle is IOracle {
+  // Storing requests
+  mapping(bytes32 => bytes32) private requests;
+
   event NewRequest(
     bytes32 indexed requestId,
     bytes32 jobId,
@@ -21,6 +27,40 @@ contract ICNOracle is IOracle {
     bytes4 _callbackFunctionId,
     bytes calldata _data
   ) external {
+    if (requests[_requestId] != 0) {
+      revert RequestAlreadyExists();
+    }
+    requests[_requestId] = keccak256(
+      abi.encodePacked(_requestId, _callbackAddress, _callbackFunctionId)
+    );
+
     emit NewRequest(_requestId, _jobId, _nonce, _callbackAddress, _callbackFunctionId, _data);
+  }
+
+  /**
+   * //TODO - Add validator node checks
+   * @notice Fulfils oracle request
+   * @param _requestId - ID of the Oracle Request
+   * @param _callbackAddress - Callback Address of Oracle Fulfilment
+   * @param _callbackFunctionId - Return functionID callback
+   * @param _data - Return data for fulfilment
+   */
+  function fulfillOracleRequest(
+    bytes32 _requestId,
+    address _callbackAddress,
+    bytes4 _callbackFunctionId,
+    bytes32 _data
+  ) external returns (bool) {
+    bytes32 paramsHash = keccak256(
+      abi.encodePacked(_requestId, _callbackAddress, _callbackFunctionId)
+    );
+    if (requests[_requestId] != paramsHash) {
+      revert IncorrectRequestFulfillment();
+    }
+    delete requests[_requestId];
+    (bool success, ) = _callbackAddress.call(
+      abi.encodeWithSelector(_callbackFunctionId, _requestId, _data)
+    );
+    return success;
   }
 }
