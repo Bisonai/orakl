22b82ab259bcc7bc7b81da82246f25ad6a0fb330
Zahid Ahmed <zahidzahmed@gmail.com>
2022-12-12 10:56:01 +0400

    feat: Aggregator with Aggregation Algo

--- contracts/src/v0.1/ICNAggregator.sol
+++ contracts/src/v0.1/ICNAggregator.sol
@@ -1,8 +1,9 @@
 //SPDX-License-Identifier: MIT
-
+// https://github.com/smartcontractkit/chainlink/blob/develop/contracts/src/v0.4/Aggregator.sol
 pragma solidity ^0.8.16;
 
 import "./ICNClient.sol";
+import "./libraries/Math.sol";
 
 /**
  * @title onChain Aggregation Contract
@@ -33,17 +34,46 @@ contract ICNAggregator is ICNClient {
 
     event NewRound(uint256 answerCounter, address aggregatorAddress, uint256 roundTimestamp);
     event ResponseRecieved(int256 indexed response, uint256 indexed answerId, address indexed sender);
+    event AnswerUpdated(int256 _answerResponse, uint256 _answerId, uint256 dateAnswerUpdated);
 
     /**
      * @notice - Validating if oracles length match requirements of maximum and minimum
      */
-    modifier validateAnswer(uint128 _minimumResponses, address[] memory _oracles, bytes32[] memory _jobIds) {
+    modifier validateRequestInfo(uint128 _minimumResponses, address[] memory _oracles, bytes32[] memory _jobIds) {
         require(_oracles.length <= MAX_ORACLE_COUNT, "Cannot exceed max oracles");
         require(
             _oracles.length >= _minimumResponses, "Must have atleast minimum amount of oracles to obtain min responses"
         );
         require(_oracles.length == _jobIds.length, "Must have same amount of oracles as jobIds");
         _;
+    }
+
+    /**
+     * @dev Prevents taking an action if not all responses are received for an answer.
+     * @param _answerId The the identifier of the answer that keeps track of the responses.
+     */
+    modifier ensureAllResponsesReceived(uint256 _answerId) {
+        if (answers[_answerId].responses.length == answers[_answerId].maxResponses) {
+            _;
+        }
+    }
+
+    /**
+     * @notice Prevents taking an action if minimum number of responses has not been recieved
+     */
+    modifier ensureMinResponsesRecieved(uint256 _answerId) {
+        if (answers[_answerId].responses.length >= answers[_answerId].minimumResponses) {
+            _;
+        }
+    }
+
+    /**
+     * @notice Prevents taking an action if a newer answer has been recorded
+     */
+    modifier ensureOnlyLatestAnswer(uint256 _answerId) {
+        if (latestCompletedAnswer <= _answerId) {
+            _;
+        }
     }
 
     /**
@@ -58,13 +88,17 @@ contract ICNAggregator is ICNClient {
      */
     function updateRequestDetails(uint128 _minimumResponses, address[] memory _oracles, bytes32[] memory _jobIds)
         public
-        validateAnswer(_minimumResponses, _oracles, _jobIds)
+        validateRequestInfo(_minimumResponses, _oracles, _jobIds)
     {
         minimumResponses = _minimumResponses;
         oracles = _oracles;
         jobIds = _jobIds;
     }
 
+    /**
+     * @notice requestRate - Creates an ICN Request for each oracle in the oracle array
+     *
+     */
     function requestRate() external {
         ICN.Request memory request;
         bytes32 requestId;
@@ -83,6 +117,10 @@ contract ICNAggregator is ICNClient {
         answerCounter++;
     }
 
+    /**
+     * @notice Recieves the answer from the ICN Node
+     * @dev this function can only called by the ICN Node Oracle that recieved the request
+     */
     function ICNCallback(bytes32 _requestId, int256 _response) external {
         validateCallback(_requestId);
         uint256 answerId = requestAnswers[_requestId];
@@ -90,7 +128,42 @@ contract ICNAggregator is ICNClient {
 
         answers[answerId].responses.push(_response);
         emit ResponseRecieved(_response, answerId, msg.sender);
-        //updateLatestAnswer(answerId);
-        //deleteAnswer(answerId);
+        updateLatestAnswer(answerId);
+        deleteAnswer(answerId);
+    }
+
+    /**
+     * @notice Performs aggregation of the answers recieved from the ICN Node
+     * Assuming atleast half of the oracles are honest.
+     */
+    function updateLatestAnswer(uint256 _answerId)
+        private
+        ensureMinResponsesRecieved(_answerId)
+        ensureOnlyLatestAnswer(_answerId)
+    {
+        uint256 responseLength = answers[_answerId].responses.length;
+        uint256 middleIndex = responseLength / 2;
+        int256 currentAnswerTemp;
+        if (responseLength % 2 == 0) {
+            int256 median1 = Math.quickselect(answers[_answerId].responses, middleIndex);
+            int256 median2 = Math.quickselect(answers[_answerId].responses, middleIndex + 1);
+            currentAnswerTemp = median1 + median2 / 2;
+        } else {
+            currentAnswerTemp = Math.quickselect(answers[_answerId].responses, middleIndex + 1);
+        }
+        currentAnswerValue = currentAnswerTemp;
+        latestCompletedAnswer = _answerId;
+        updatedTimestampValue = block.timestamp;
+        updatedTimestamps[_answerId] = block.timestamp;
+        currentAnswers[_answerId] = currentAnswerTemp;
+        emit AnswerUpdated(currentAnswerTemp, _answerId, block.timestamp);
+    }
+
+    /**
+     * @dev Cleans up the answer record if all responses have been received.
+     * @param _answerId The identifier of the answer to be deleted
+     */
+    function deleteAnswer(uint256 _answerId) private ensureAllResponsesReceived(_answerId) {
+        delete answers[_answerId];
     }
 }
