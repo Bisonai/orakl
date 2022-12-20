942cfe27b9d1bde933f95c39ac295f9e44229914
Martin Kersner <martin@bisonai.com>
2022-12-20 11:33:28 +0900

    Refactoring Listener (#70)
    * refactor: Listener
    
    * feat: Updated listener

--- core/src/types.ts
+++ core/src/types.ts
@@ -178,3 +178,9 @@ export interface IListenerBlock {
   startBlock: number
   filePath: string
 }
+
+export interface IListenerConfig {
+  address: string
+  eventName: string
+  factoryName: string
+}
