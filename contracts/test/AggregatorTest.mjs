1c81ac1a1aa7cf48a4bdb0c605b48826ce4d7ac5
Martin Kersner <martin@bisonai.com>
2022-12-22 19:06:21 +0900

    fix: Aggregator was not tested properly. Onchain aggregation broken.

--- contracts/test/AggregatorTest.mjs
+++ contracts/test/AggregatorTest.mjs
@@ -1,100 +1,75 @@
 import { expect } from 'chai'
-import axios from 'axios'
-import Web3 from 'web3'
-
 import pkg from 'hardhat'
-import assert from 'node:assert'
 const { ethers } = pkg
 
-import chai from 'chai'
-
 describe('Testing Aggregator Contract', function () {
-  let ICNOracle
-  let ICNOracle2
-  let ICNOracle3
-  let ICNOracle4
+  const minimumResponse = 2
+  const oracleCount = 5
+  const ethUsdPrice = [10, 11, 12, 13, 14]
 
+  let icnOracleAggregator
+  let icnAggregator
   let ICNAggregator
 
-  let minimumResponse = 2
+  let icnOracles = []
   let oracleAddresses = []
-  let jobIds = [
-    ethers.utils.formatBytes32String('JOBID-1-KLAYUSD'),
-    ethers.utils.formatBytes32String('JOBID-2-KLAYUSD'),
-    ethers.utils.formatBytes32String('JOBID-3-KLAYUSD'),
-    ethers.utils.formatBytes32String('JOBID-3-KLAYUSD')
-  ]
+  let jobIds = []
   let requestIds = []
+
   beforeEach(async function () {
-    // Deploy ICN Oracle
-    let OracleContract = await ethers.getContractFactory('ICNOracleAggregator')
-    ICNOracle = await OracleContract.deploy()
-    await ICNOracle.deployed()
-    oracleAddresses.push(ICNOracle.address)
+    icnOracleAggregator = await ethers.getContractFactory('ICNOracleAggregator')
 
-    ICNOracle2 = await OracleContract.deploy()
-    await ICNOracle2.deployed()
-    oracleAddresses.push(ICNOracle2.address)
+    for (let i = 0; i < oracleCount; ++i) {
+      icnOracles[i] = await icnOracleAggregator.deploy()
+      await icnOracles[i].deployed()
+      oracleAddresses.push(icnOracles[i].address)
 
-    ICNOracle3 = await OracleContract.deploy()
-    await ICNOracle3.deployed()
-    oracleAddresses.push(ICNOracle3.address)
+      jobIds.push(ethers.utils.formatBytes32String(`KLAY-USD-${i}`))
+    }
 
-    ICNOracle4 = await OracleContract.deploy()
-    await ICNOracle4.deployed()
-    oracleAddresses.push(ICNOracle4.address)
+    icnAggregator = await ethers.getContractFactory('ICNAggregator')
+    icnAggregator = await icnAggregator.deploy(minimumResponse, oracleAddresses, jobIds)
+    await icnAggregator.deployed()
+  })
 
-    // Deploy Aggregator Contract
-
-    let AggregatorContract = await ethers.getContractFactory('ICNAggregator')
-    ICNAggregator = await AggregatorContract.deploy(minimumResponse, oracleAddresses, jobIds)
-    await ICNAggregator.deployed()
+  it('Number of oracles should be same as number of predefined prices', function () {
+    expect(ethUsdPrice.length).to.be.equal(oracleCount)
   })
 
   it('Should Request and Fulfill Data from Oracles declared in Aggregator Contract fulfillments', async function () {
-    let latestRound = await ICNAggregator.latestRound()
+    let latestRound = await icnAggregator.latestRound()
     expect(latestRound).to.be.equal(0)
-    expect(await ICNAggregator.getAnswer(latestRound)).to.be.equal(0)
-    const tx = await ICNAggregator.requestRate()
-    const receipt = await tx.wait()
-    let ETHUSDPRICE = 17000
-    for (const event of receipt.events) {
-      if (event.event == 'Requested') {
-        requestIds.push(event.args.id)
-      }
+    expect(await icnAggregator.getAnswer(latestRound)).to.be.equal(0)
+
+    // 1. ICNAggregator: requestRate (NewRound)
+    // 2. RequestResponseConsumerBase: sendRequestTo
+    // 3. ICNOracleAggregator: createNewRequest (NewRequest)
+
+    const txReceipt = await (await icnAggregator.requestRate()).wait()
+    expect(txReceipt.events.length).to.be.equal(5)
+
+    for (let i = 0; i < 4; ++i) {
+      const event = icnOracleAggregator.interface.parseLog(txReceipt.events[i])
+      expect(event.name).to.be.equal('NewRequest')
+      requestIds.push(event.args.requestId)
     }
-    expect(requestIds.length).to.be.equal(4)
-    console.log(requestIds)
 
-    await ICNOracle.fulfillOracleRequest(
-      requestIds[0],
-      ICNAggregator.address,
-      '0xd8c6a442',
-      ETHUSDPRICE
-    )
-    await ICNOracle2.fulfillOracleRequest(
-      requestIds[1],
-      ICNAggregator.address,
-      '0xd8c6a442',
-      ETHUSDPRICE
-    )
+    const event = icnAggregator.interface.parseLog(txReceipt.events[4])
+    expect(event.name).to.be.equal('NewRound')
 
-    await ICNOracle3.fulfillOracleRequest(
-      requestIds[2],
-      ICNAggregator.address,
-      '0xd8c6a442',
-      ETHUSDPRICE
-    )
+    for (let i = 0; i < oracleCount; ++i) {
+      await icnOracles[i].fulfillOracleRequest(
+        requestIds[i],
+        icnAggregator.address,
+        '0xd8c6a442',
+        ethUsdPrice[i]
+      )
+    }
 
-    await ICNOracle4.fulfillOracleRequest(
-      requestIds[3],
-      ICNAggregator.address,
-      '0xd8c6a442',
-      ETHUSDPRICE
-    )
-
-    latestRound = await ICNAggregator.latestRound()
+    latestRound = await icnAggregator.latestRound()
     expect(latestRound).to.be.equal(1)
-    expect(Number(await ICNAggregator.latestAnswer())).to.be.equal(17000)
+
+    // FIXME does not aggregate correctly for odd length arrays
+    expect(Number(await icnAggregator.latestAnswer())).to.be.equal(12)
   })
 })
