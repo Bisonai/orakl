f453e4cad28527bda7fd8130dde26494c7d51b80
Martin Kersner <martin@bisonai.com>
2022-12-29 07:21:11 +0000

    feat: deployment script for request response

---
+++ contracts/deploy/1-RequestResponseCoordinator.ts
@@ -0,0 +1,32 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts } = hre;
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  console.log("1-RequestResponseCoordinator.ts");
+
+  const requestResponseCoordinator = await deploy(
+    "RequestResponseCoordinator",
+    {
+      from: deployer,
+      log: true,
+    }
+  );
+
+  // TODO deploy only for tests
+  const requestResponseConsumerMock = await deploy(
+    "RequestResponseConsumerMock",
+    {
+      args: [requestResponseCoordinator.address],
+      from: deployer,
+      log: true,
+    }
+  );
+};
+
+export default func;
+func.id = "deploy-request-response";
+func.tags = ["request-response"];
