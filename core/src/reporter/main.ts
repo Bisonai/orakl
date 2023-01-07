93801074ca766eff43101f168e6df4def02553b7
jo.lol <jo.lol@krustuniverse.com>
2023-01-07 19:16:02 +0900

    fixed run curl command below NPM_TOKEN
    review: Review & update of #104
    
    feat: Trigger contracts deployment
    
    fix: Update and test docker-compose.yaml
    
    feat: Use contracts v0.2.0
    
    feat: Pass HEALTH_CHECK_PORT to docker-compose
    
    feat: Copy aggregator directory to image
    
    feat: Remove export & /health-check endpoint

--- core/src/reporter/main.ts
+++ core/src/reporter/main.ts
@@ -2,7 +2,7 @@ import { parseArgs } from 'node:util'
 import { aggregatorReporter } from './aggregator'
 import { vrfReporter } from './vrf'
 import { anyApiReporter } from './any-api'
-import { healthChack } from '../health-checker'
+import { healthCheck } from '../health-checker'
 
 const REPORTERS = {
   AGGREGATOR: aggregatorReporter,
@@ -13,7 +13,7 @@ const REPORTERS = {
 async function main() {
   const reporter = loadArgs()
   REPORTERS[reporter]()
-  healthChack()
+  healthCheck()
 }
 
 function loadArgs() {
