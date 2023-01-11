54b180aae8c411e93116c760418a90a788dee8f4
Martin Kersner <martin@bisonai.com>
2023-01-11 18:11:11 +0900

    feat: Different deployment based on target chain

--- contracts/deploy/0-Prepayment.ts
+++ contracts/deploy/0-Prepayment.ts
@@ -2,11 +2,16 @@ import { HardhatRuntimeEnvironment } from 'hardhat/types'
 import { DeployFunction } from 'hardhat-deploy/types'
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { deployments, getNamedAccounts } = hre
+  const { deployments, getNamedAccounts, network } = hre
   const { deploy } = deployments
   const { deployer } = await getNamedAccounts()
 
   console.log('0-Prepayment.ts')
+
+  if (network.name == 'baobab') {
+    console.log('Skipping')
+    return
+  }
 
   const prepaymentDeployment = await deploy('Prepayment', {
     from: deployer,
