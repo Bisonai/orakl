22a3bd69eedceb525844167630aeed2e90c35033
Martin Kersner <martin@bisonai.com>
2023-01-11 06:15:52 +0000

    fix: Update off-chain aggregator scripts

--- contracts/scripts/v0.1/off-chain-aggregator/README.md
+++ contracts/scripts/v0.1/off-chain-aggregator/README.md
@@ -7,5 +7,5 @@ To deploy the smart contracts, run `npx hardhat deploy --network localhost`.
 ## Read data
 
 ```
-npx hardhat --network localhost run read-data.mjs
+npx hardhat --network localhost run read-data.ts
 ```
--- contracts/scripts/v0.1/off-chain-aggregator/read-data.ts
+++ contracts/scripts/v0.1/off-chain-aggregator/read-data.ts
@@ -2,27 +2,32 @@ import { ethers } from 'hardhat'
 import hre from 'hardhat'
 
 async function main() {
-  const { consumer } = await hre.getNamedAccounts()
+    const { consumer } = await hre.getNamedAccounts()
 
-  const dataFeedConsumerMock = await ethers.getContract('DataFeedConsumerMock')
-  const dataFeedConsumerSigner = await ethers.getContractAt(
-    'DataFeedConsumerMock',
-    dataFeedConsumerMock.address,
-    consumer
-  )
+    const dataFeedConsumerMock = await ethers.getContract('DataFeedConsumerMock')
+    const dataFeedConsumerSigner = await ethers.getContractAt(
+        'DataFeedConsumerMock',
+        dataFeedConsumerMock.address,
+        consumer
+    )
 
-  console.log('DataFeedConsumerMock', dataFeedConsumerMock.address)
+    console.log('DataFeedConsumerMock', dataFeedConsumerMock.address)
 
-  await dataFeedConsumerSigner.getLatestPrice()
-  const price = await dataFeedConsumerSigner.s_price()
-  const decimals = await dataFeedConsumerSigner.decimals()
-  const round = await dataFeedConsumerSigner.s_roundID()
-  console.log(`Price\t${price}`)
-  console.log(`Decimals\t${decimals}`)
-  console.log(`Round\t${round}`)
+    try {
+        await dataFeedConsumerSigner.getLatestPrice()
+        const price = await dataFeedConsumerSigner.s_price()
+        const decimals = await dataFeedConsumerSigner.decimals()
+        const round = await dataFeedConsumerSigner.s_roundID()
+        console.log(`Price\t${price}`)
+        console.log(`Decimals\t${decimals}`)
+        console.log(`Round\t${round}`)
+    }
+    catch (e) {
+        console.error("Most likely no submission yet.")
+    }
 }
 
 main().catch((error) => {
-  console.error(error)
-  process.exitCode = 1
+    console.error(error)
+    process.exitCode = 1
 })
