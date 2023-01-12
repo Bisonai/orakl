7df864a5b86cafb8afadcd888cbbf3ee9342eed2
Martin Kersner <martin@bisonai.com>
2023-01-12 20:49:40 +0900

    fix: Allow to use both localhost and other chains in agg. read data

--- contracts/scripts/v0.1/off-chain-aggregator/read-data.ts
+++ contracts/scripts/v0.1/off-chain-aggregator/read-data.ts
@@ -2,26 +2,37 @@ import { ethers } from 'hardhat'
 import hre from 'hardhat'
 
 async function main() {
-  const { consumer } = await hre.getNamedAccounts()
+  const { network } = hre
+  let consumer
+
+  if (network.name == 'localhost') {
+    const { _consumer } = await hre.getNamedAccounts()
+    consumer = _consumer
+  } else {
+    const PROVIDER = process.env.PROVIDER
+    const MNEMONIC = process.env.MNEMONIC
+    const provider = new ethers.providers.JsonRpcProvider(PROVIDER)
+    consumer = ethers.Wallet.fromMnemonic(MNEMONIC).connect(provider)
+  }
 
   const dataFeedConsumerMock = await ethers.getContract('DataFeedConsumerMock')
   const dataFeedConsumerSigner = await ethers.getContractAt(
     'DataFeedConsumerMock',
-    dataFeedConsumerMock.address,
-    consumer
+    dataFeedConsumerMock.address
   )
 
   console.log('DataFeedConsumerMock', dataFeedConsumerMock.address)
 
   try {
-    await dataFeedConsumerSigner.getLatestPrice()
+    await dataFeedConsumerSigner.connect(consumer).getLatestPrice()
     const price = await dataFeedConsumerSigner.s_price()
     const decimals = await dataFeedConsumerSigner.decimals()
     const round = await dataFeedConsumerSigner.s_roundID()
-    console.log(`Price\t${price}`)
+    console.log(`Price\t\t${price}`)
     console.log(`Decimals\t${decimals}`)
-    console.log(`Round\t${round}`)
+    console.log(`Round\t\t${round}`)
   } catch (e) {
+    console.log(e)
     console.error('Most likely no submission yet.')
   }
 }
