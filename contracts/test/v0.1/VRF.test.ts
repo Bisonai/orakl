66fdf1f0932276bcb1325700ad552082089c2377
Martin Kersner <martin@bisonai.com>
2023-01-18 15:14:47 +0900

    fix: Remove an extra function from VRF.test

--- contracts/test/v0.1/VRF.test.ts
+++ contracts/test/v0.1/VRF.test.ts
@@ -5,17 +5,6 @@ import { vrfConfig } from './VRF.config'
 import { parseKlay } from './utils'
 import { createAccount } from './Prepayment.utils'
 
-async function createAccount(prepaymentContract) {
-  const txReceipt = await (await prepaymentContract.createAccount()).wait()
-  expect(txReceipt.events.length).to.be.equal(1)
-
-  const txEvent = prepaymentContract.interface.parseLog(txReceipt.events[0])
-  const { accId } = txEvent.args
-  expect(accId).to.be.equal(1)
-
-  return accId
-}
-
 describe('VRF contract', function () {
   async function deployFixture() {
     const { deployer, consumer } = await hre.getNamedAccounts()
