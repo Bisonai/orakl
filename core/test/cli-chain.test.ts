f84ffd12a7c5e92234341251ccc92a1cf73b787d
Martin Kersner <martin@bisonai.com>
2023-01-19 16:47:30 +0900

    refactor: Global name unification

--- core/test/cli-chain.test.ts
+++ core/test/cli-chain.test.ts
@@ -1,36 +1,36 @@
 import { describe, expect, beforeEach, test } from '@jest/globals'
 import { listHandler, insertHandler, removeHandler } from '../src/cli/operator/chain'
-import { openDb } from '../src/cli/operator/utils-test'
+import { openDB } from '../src/cli/operator/utils-test'
 
 describe('CLI Chain', function () {
-  let db
+  let DB
   beforeEach(async () => {
-    db = await openDb({ migrate: true })
+    DB = await openDB({ migrate: true })
   })
 
   test('Should list chain', async function () {
-    const chain = await listHandler(db)()
+    const chain = await listHandler(DB)()
     expect(chain.length).toBeGreaterThan(0)
   })
 
   test('Should insert new chain', async function () {
-    const chainBefore = await listHandler(db)()
-    await insertHandler(db)({ name: 'ethereum' })
-    const chainAfter = await listHandler(db)()
+    const chainBefore = await listHandler(DB)()
+    await insertHandler(DB)({ name: 'ethereum' })
+    const chainAfter = await listHandler(DB)()
     expect(chainAfter.length).toEqual(chainBefore.length + 1)
   })
 
   test('Should not allow to insert the same chain more than once', async function () {
-    await insertHandler(db)({ name: 'ethereum' })
+    await insertHandler(DB)({ name: 'ethereum' })
     await expect(async () => {
-      await insertHandler(db)({ name: 'ethereum' })
+      await insertHandler(DB)({ name: 'ethereum' })
     }).rejects.toThrow()
   })
 
   test('Should delete chain based on id', async function () {
-    const chainBefore = await listHandler(db)()
-    await removeHandler(db)({ id: 1 })
-    const chainAfter = await listHandler(db)()
+    const chainBefore = await listHandler(DB)()
+    await removeHandler(DB)({ id: 1 })
+    const chainAfter = await listHandler(DB)()
     expect(chainAfter.length).toEqual(chainBefore.length - 1)
   })
 })
--- core/test/cli-service.test.ts
+++ core/test/cli-service.test.ts
@@ -1,36 +1,36 @@
 import { describe, expect, beforeEach, test } from '@jest/globals'
 import { listHandler, insertHandler, removeHandler } from '../src/cli/operator/service'
-import { openDb } from '../src/cli/operator/utils-test'
+import { openDB } from '../src/cli/operator/utils-test'
 
 describe('CLI Service', function () {
-  let db
+  let DB
   beforeEach(async () => {
-    db = await openDb({ migrate: true })
+    DB = await openDB({ migrate: true })
   })
 
   test('Should list service', async function () {
-    const service = await listHandler(db)()
+    const service = await listHandler(DB)()
     expect(service.length).toBeGreaterThan(0)
   })
 
   test('Should insert new service', async function () {
-    const serviceBefore = await listHandler(db)()
-    await insertHandler(db)({ name: 'Automation' })
-    const serviceAfter = await listHandler(db)()
+    const serviceBefore = await listHandler(DB)()
+    await insertHandler(DB)({ name: 'Automation' })
+    const serviceAfter = await listHandler(DB)()
     expect(serviceAfter.length).toEqual(serviceBefore.length + 1)
   })
 
   test('Should not allow to insert the same service more than once', async function () {
-    await insertHandler(db)({ name: 'Automation' })
+    await insertHandler(DB)({ name: 'Automation' })
     await expect(async () => {
-      await insertHandler(db)({ name: 'Automation' })
+      await insertHandler(DB)({ name: 'Automation' })
     }).rejects.toThrow()
   })
 
   test('Should delete service based on id', async function () {
-    const serviceBefore = await listHandler(db)()
-    await removeHandler(db)({ id: 1 })
-    const serviceAfter = await listHandler(db)()
+    const serviceBefore = await listHandler(DB)()
+    await removeHandler(DB)({ id: 1 })
+    const serviceAfter = await listHandler(DB)()
     expect(serviceAfter.length).toEqual(serviceBefore.length - 1)
   })
 })
