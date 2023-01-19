ed02940be18eee729e4c1b07e72b1b244a97ee5d
Martin Kersner <martin@bisonai.com>
2023-01-19 16:45:48 +0900

    feat: Add migrate command

--- core/src/cli/operator/core.ts
+++ core/src/cli/operator/core.ts
@@ -2,6 +2,7 @@ import { chainSub } from './chain'
 import { serviceSub } from './service'
 import { listenerSub } from './listener'
 import { vrfSub } from './vrf'
+import { migrateCmd } from './migrate'
 import { openDb } from './utils'
 
 import { binary, subcommands, run } from 'cmd-ts'
@@ -9,16 +10,15 @@ import { binary, subcommands, run } from 'cmd-ts'
 async function main() {
   const db = await openDb()
 
-  // await db.migrate({ force: true }) // FIXME
-
   const chain = chainSub(db)
   const service = serviceSub(db)
   const listener = listenerSub(db)
   const vrf = vrfSub(db)
+  const migrate = migrateCmd(db)
 
   const cli = subcommands({
     name: 'operator',
-    cmds: { chain, service, listener, vrf }
+    cmds: { migrate, chain, service, listener, vrf }
   })
 
   run(binary(cli), process.argv)
