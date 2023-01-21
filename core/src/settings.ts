98a8e01e8f11ed3c54632354188ca8b249806897
Martin Kersner <martin@bisonai.com>
2023-01-21 18:24:48 +0900

    feat: Load aggregators and adapters from SQLite

--- core/src/settings.ts
+++ core/src/settings.ts
@@ -11,7 +11,7 @@ export const NODE_ENV = process.env.NODE_ENV
 export const CHAIN = process.env.CHAIN || 'localhost'
 
 export const SETTINGS_DB_FILE = './settings.sqlite' // FIXME
-const DB = await openDb()
+export const DB = await openDb()
 
 export const PROVIDER_URL = await loadKeyValuePair({ db: DB, key: 'PROVIDER_URL', chain: CHAIN })
 export const REDIS_HOST = await loadKeyValuePair({ db: DB, key: 'REDIS_HOST', chain: CHAIN })
@@ -135,4 +135,18 @@ export async function getVrfConfig(chain: string): Promise<IVrfConfig> {
     INNER JOIN Chain ON Chain.id = VrfKey.chainId AND Chain.name='${chain}'`
   const vrfConfig = await db.get(query)
   return vrfConfig
+}
+
+export async function getAdapters(db, chain: string) {
+  const query = `SELECT data FROM Adapter
+    INNER JOIN Chain ON Chain.id = Adapter.chainId AND Chain.name='${chain}'`
+  const adapters = await db.all(query)
+  return adapters
+}
+
+export async function getAggregators(db, chain: string) {
+  const query = `SELECT data FROM Aggregator
+    INNER JOIN Chain ON Chain.id = Aggregator.chainId AND Chain.name='${chain}'`
+  const aggregators = await db.all(query)
+  return aggregators
 }
