cda7cd610a74d28b61f9e324fbf17984f512ee9a
Martin Kersner <martin@bisonai.com>
2023-02-09 23:43:22 +0900

    refactor: Reducers

--- core/src/worker/reducer.ts
+++ core/src/worker/reducer.ts
@@ -1,34 +1,58 @@
+import { IcnError, IcnErrorCode } from '../errors'
+
 export const reducerMapping = {
-  PARSE: parse,
-  MUL: mul,
-  POW10: pow10,
-  ROUND: round
+  PARSE: parseFn,
+  MUL: mulFn,
+  POW10: pow10Fn,
+  ROUND: roundFn,
+
+  // request-response
+  path: parseFn,
+  mul: mulFn,
+  pow10: pow10Fn
 }
 
-export function parse(args: string[]) {
+/**
+ * Access data in JSON based on given path.
+ *
+ * Example
+ * let obj = {
+ *     RAW: { ETH: { USD: { PRICE: 123 } } },
+ *     DISPLAY: { ETH: { USD: [Object] } }
+ * }
+ * const fn = parseFn(['RAW', 'ETH', 'USD', 'PRICE'])
+ * fn(obj) // return 123
+ */
+export function parseFn(args: string | string[]) {
+  if (typeof args == 'string') {
+    args = args.split(',')
+  }
+
   function wrapper(obj) {
     for (const a of args) {
-      obj = obj[a]
+      if (a in obj) obj = obj[a]
+      else throw new IcnError(IcnErrorCode.MissingKeyInJson)
     }
     return obj
   }
   return wrapper
 }
 
-export function mul(args: number[]) {
+export function mulFn(args: number) {
   function wrapper(value: number) {
-    return args.reduce((acc, v) => Number(acc) * Number(v), value)
+    return args * value
   }
   return wrapper
 }
 
-export function pow10(args: number) {
+export function pow10Fn(args: number) {
   function wrapper(value: number) {
     return Number(Math.pow(10, args)) * value
   }
   return wrapper
 }
-export function round() {
+
+export function roundFn() {
   function wrapper(value: number) {
     return Math.round(value)
   }
