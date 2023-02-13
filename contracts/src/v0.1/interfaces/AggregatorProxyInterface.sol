aa7d5660ba970f8d5ab8929f527a686f37e74b4f
Martin Kersner <martin@bisonai.com>
2023-02-13 19:19:19 +0900

    fix: Trigger pipeline

--- contracts/src/v0.1/interfaces/AggregatorProxyInterface.sol
+++ contracts/src/v0.1/interfaces/AggregatorProxyInterface.sol
@@ -6,33 +6,35 @@ pragma solidity ^0.8.16;
 import "./AggregatorInterface.sol";
 
 interface AggregatorProxyInterface is AggregatorInterface {
-  function phaseAggregators(uint16 phaseId) external view returns (address);
+    function phaseAggregators(uint16 phaseId) external view returns (address);
 
-  function phaseId() external view returns (uint16);
+    function phaseId() external view returns (uint16);
 
-  function proposedAggregator() external view returns (address);
+    function proposedAggregator() external view returns (address);
 
-  function proposedGetRoundData(uint80 roundId)
-    external
-    view
-    returns (
-      uint80 id,
-      int256 answer,
-      uint256 startedAt,
-      uint256 updatedAt,
-      uint80 answeredInRound
-    );
+    function proposedGetRoundData(
+        uint80 roundId
+    )
+        external
+        view
+        returns (
+            uint80 id,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        );
 
-  function proposedLatestRoundData()
-    external
-    view
-    returns (
-      uint80 id,
-      int256 answer,
-      uint256 startedAt,
-      uint256 updatedAt,
-      uint80 answeredInRound
-    );
+    function proposedLatestRoundData()
+        external
+        view
+        returns (
+            uint80 id,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        );
 
-  function aggregator() external view returns (address);
+    function aggregator() external view returns (address);
 }
