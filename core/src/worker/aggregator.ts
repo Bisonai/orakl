176b00f859d079031fa34722fbc742e59cd39766
Martin Kersner <martin@bisonai.com>
2023-02-17 11:35:08 +0900

    feat: Allow random heartbeat queue

--- core/src/worker/aggregator.ts
+++ core/src/worker/aggregator.ts
@@ -57,16 +57,16 @@ export async function aggregatorWorker(_logger: Logger) {
   )
 
   // Random heartbeat worker
-  // new Worker(
-  //   RANDOM_HEARTBEAT_QUEUE_NAME,
-  //   randomHeartbeatJob(
-  //     RANDOM_HEARTBEAT_QUEUE_NAME,
-  //     REPORTER_AGGREGATOR_QUEUE_NAME,
-  //     aggregatorsWithAdapters,
-  //     _logger
-  //   ),
-  //   BULLMQ_CONNECTION
-  // )
+  new Worker(
+    RANDOM_HEARTBEAT_QUEUE_NAME,
+    randomHeartbeatJob(
+      RANDOM_HEARTBEAT_QUEUE_NAME,
+      REPORTER_AGGREGATOR_QUEUE_NAME,
+      aggregatorsWithAdapters,
+      _logger
+    ),
+    BULLMQ_CONNECTION
+  )
 }
 
 function aggregatorJob(reporterQueueName: string, aggregatorsWithAdapters, _logger: Logger) {
