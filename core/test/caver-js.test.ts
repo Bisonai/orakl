450cd51bfe60a9c0b00a0dd520a732d39b026320
bayram98 <bayramguwanjow@gmail.com>
2023-02-20 18:11:10 +0900

    feat: add conditional testing

--- core/test/caver-js.test.ts
+++ core/test/caver-js.test.ts
@@ -3,48 +3,20 @@ import { BigNumber, ethers } from 'ethers'
 import Caver from 'caver-js'
 import { NODE_ENV } from '../src/settings'
 
-let PROVIDER_URL
-if (NODE_ENV != 'test') {
-  PROVIDER_URL = 'https://api.baobab.klaytn.net:8651'
-} else {
-  // PROVIDER_URL = 'http://127.0.0.1:8551' // Local Klaytn Sandbox
-  PROVIDER_URL = 'http://127.0.0.1:8545'
-}
-
-const caver = new Caver(PROVIDER_URL)
-const password = 'trb}RROVYs#ye2rq'
-const key = {
-  address: '14fadded4f98064c3188765df46a055edebf0cf2',
-  keyring: [
-    [
-      {
-        cipher: 'aes-128-ctr',
-        ciphertext: 'acdce40f546d876430ec072118fe1d71c86e727e9ab03b70581bb6d9e3d7e48a',
-        cipherparams: { iv: '59f02c6cf41fc704dd8fe700b763b607' },
-        kdf: 'scrypt',
-        kdfparams: {
-          dklen: 32,
-          n: 262144,
-          p: 1,
-          r: 8,
-          salt: '226fa77e4832c3aabbe83e0ed896e08b56880f83e4a593ef5b58ea5d1185e345'
-        },
-        mac: '3a29ff9234099b04a95b1009d884fe82610ea05e8c745332c550a118e5255b5c'
-      }
-    ]
-  ],
-  id: 'fe0ee297-6fdd-414f-991b-e7f563a10b2d',
-  version: 4
-}
-
 describe('Test Caver-js', function () {
-  jest.setTimeout(30000)
+  jest.setTimeout(10000)
   console.log('NODE_ENV:', NODE_ENV)
-  if (!NODE_ENV)
-    test.only('Send signed tx with is caver-js on Baobab', async function () {
-      const account1 = caver.klay.accounts.decrypt(key, password)
+
+  if (NODE_ENV == 'development') {
+    test('Send signed tx with is caver-js on Baobab', async function () {
+      const PROVIDER_URL = 'https://api.baobab.klaytn.net:8651'
+      const caver = new Caver(PROVIDER_URL)
+
+      const privateKey = '0xced84838ee2119943c7cf8eca25b5d6bf83f322cc0121504f8979f3411badd2e'
+      const address = '14fadded4f98064c3188765df46a055edebf0cf2'
+      const account1 = caver.klay.accounts.createWithAccountKeyPublic(address, privateKey)
       caver.klay.accounts.wallet.add(account1.privateKey)
-      console.log('Wallet connected')
+
       const amount = ethers.utils.parseEther('0.001')
       const to = '0xeF5cd886C7f8d85fbe8023291761341aCBb4DA01'
       const beforeBalanceOfTo = await caver.klay.getBalance(to)
@@ -55,15 +27,20 @@ describe('Test Caver-js', function () {
         value: amount,
         gas: '300000'
       }
+
       // Sign transaction
       const signTx: any = await caver.klay.accounts.signTransaction(tx)
+
       // Send signed transaction
       const txReceipt = await caver.klay.sendSignedTransaction(signTx)
+      console.log('Baobab txReceipt:', txReceipt)
+
       const txFee = BigNumber.from(txReceipt.effectiveGasPrice).mul(
         BigNumber.from(txReceipt.gasUsed)
       )
       const afterBalanceOfTo = await caver.klay.getBalance(to)
       const afterBalanceOfAccount1 = await caver.klay.getBalance(account1.address)
+
       expect(
         BigNumber.from(afterBalanceOfTo).eq(
           BigNumber.from(beforeBalanceOfTo).add(BigNumber.from(amount))
@@ -75,9 +52,10 @@ describe('Test Caver-js', function () {
         )
       ).toBe(true)
     })
-
-  if (NODE_ENV)
-    test.only('Send signed tx with is ethers on local', async function () {
+  }
+  if (NODE_ENV != 'development') {
+    test('Send signed tx with is ethers on local', async function () {
+      const PROVIDER_URL = 'http://127.0.0.1:8545'
       const provider = new ethers.providers.JsonRpcProvider(PROVIDER_URL)
       const privateKey = '0x4bbbf85ce3377467afe5d46f804f221813b2bb87f24d81f60f1fcdbf7cbf4356' // Account 7
       const wallet = await new ethers.Wallet(privateKey, provider)
@@ -102,4 +80,5 @@ describe('Test Caver-js', function () {
         )
       ).toBe(true)
     })
+  }
 })
