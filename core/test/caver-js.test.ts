4936a7d80225374f5b43126759767f364a167be8
bayram98 <bayramguwanjow@gmail.com>
2023-02-21 11:17:30 +0900

    feat: use github actions variable

--- core/test/caver-js.test.ts
+++ core/test/caver-js.test.ts
@@ -1,22 +1,20 @@
 import { describe, test, expect, jest } from '@jest/globals'
 import { BigNumber, ethers } from 'ethers'
 import Caver from 'caver-js'
-import { NODE_ENV } from '../src/settings'
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 describe('Test Caver-js', function () {
   jest.setTimeout(10000)
-  console.log('NODE_ENV:', NODE_ENV)
+  console.log('GITHUB_ACTIONS:', process.env.GITHUB_ACTIONS)
 
-  if (NODE_ENV == 'development') {
+  if (process.env.GITHUB_ACTIONS) {
     test('Send signed tx with is caver-js on Baobab', async function () {
       const PROVIDER_URL = 'https://api.baobab.klaytn.net:8651'
       const caver = new Caver(PROVIDER_URL)
-
-      const privateKey = '0xced84838ee2119943c7cf8eca25b5d6bf83f322cc0121504f8979f3411badd2e'
-      const address = '14fadded4f98064c3188765df46a055edebf0cf2'
-      const account1 = caver.klay.accounts.createWithAccountKeyPublic(address, privateKey)
-      caver.klay.accounts.wallet.add(account1.privateKey)
-
+      const privateKey = process.env.CAVER_PRIVATE_KEY || ''
+      const account1 = caver.klay.accounts.privateKeyToAccount(privateKey)
+      console.log('Address:', account1.address)
       const amount = ethers.utils.parseEther('0.001')
       const to = '0xeF5cd886C7f8d85fbe8023291761341aCBb4DA01'
       const beforeBalanceOfTo = await caver.klay.getBalance(to)
@@ -25,22 +23,18 @@ describe('Test Caver-js', function () {
         from: account1.address,
         to: to,
         value: amount,
-        gas: '300000'
+        gas: '21000'
       }
-
       // Sign transaction
-      const signTx: any = await caver.klay.accounts.signTransaction(tx)
-
+      const signTx = await caver.klay.accounts.signTransaction(tx)
       // Send signed transaction
       const txReceipt = await caver.klay.sendSignedTransaction(signTx)
       console.log('Baobab txReceipt:', txReceipt)
-
       const txFee = BigNumber.from(txReceipt.effectiveGasPrice).mul(
         BigNumber.from(txReceipt.gasUsed)
       )
       const afterBalanceOfTo = await caver.klay.getBalance(to)
       const afterBalanceOfAccount1 = await caver.klay.getBalance(account1.address)
-
       expect(
         BigNumber.from(afterBalanceOfTo).eq(
           BigNumber.from(beforeBalanceOfTo).add(BigNumber.from(amount))
@@ -52,14 +46,11 @@ describe('Test Caver-js', function () {
         )
       ).toBe(true)
     })
-  }
-  if (NODE_ENV != 'development') {
+  } else {
     test('Send signed tx with is ethers on local', async function () {
-      const PROVIDER_URL = 'http://127.0.0.1:8545'
-      const provider = new ethers.providers.JsonRpcProvider(PROVIDER_URL)
-      const privateKey = '0x4bbbf85ce3377467afe5d46f804f221813b2bb87f24d81f60f1fcdbf7cbf4356' // Account 7
+      const provider = new ethers.providers.JsonRpcProvider('http://127.0.0.1:8545')
+      const privateKey = '0x4bbbf85ce3377467afe5d46f804f221813b2bb87f24d81f60f1fcdbf7cbf4356' // Hardhat account
       const wallet = await new ethers.Wallet(privateKey, provider)
-
       const amount = ethers.utils.parseEther('0.001')
       const to = '0xeF5cd886C7f8d85fbe8023291761341aCBb4DA01'
       const beforeBalanceOfTo = await provider.getBalance(to)
@@ -73,7 +64,7 @@ describe('Test Caver-js', function () {
       // Send transaction
       const txReceipt = await wallet.sendTransaction(tx)
       const afterBalanceOfTo = await provider.getBalance(to)
-      console.log('txReceipt:', txReceipt)
+
       expect(
         BigNumber.from(afterBalanceOfTo).eq(
           BigNumber.from(beforeBalanceOfTo).add(BigNumber.from(amount))
