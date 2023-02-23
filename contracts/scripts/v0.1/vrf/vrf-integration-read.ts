9d92c744d261d75905744aa06e22938d5137da58
KelvinThai <quan.thai1987@gmail.com>
2023-02-23 17:01:07 +0700

    test get vrf random words

--- contracts/scripts/v0.1/vrf/vrf-integration-read.ts
+++ contracts/scripts/v0.1/vrf/vrf-integration-read.ts
@@ -5,11 +5,12 @@ async function main() {
   const userContract = await ethers.getContract('VRFConsumerMock')
 
   const randomWord = await userContract.s_randomWord()
-  //console.log(randomWord.toString())
+  console.log(randomWord.toString())
   expect(randomWord.toString()).to.not.equal('0')
 }
-
-main().catch((error) => {
-  console.error(error)
-  process.exitCode = 1
-})
+setInterval(() => {
+  main().catch((error) => {
+    console.error(error)
+    process.exitCode = 1
+  })
+}, 1000 * 20)
