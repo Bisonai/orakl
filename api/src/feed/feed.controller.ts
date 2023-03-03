cd5a20dd81179b0d62e9b8abc50e283c7ae2127d
Martin Kersner <martin@bisonai.com>
2023-03-03 10:44:40 +0900

    feat: Feed cannot be created or updated directly

--- api/src/feed/feed.controller.ts
+++ api/src/feed/feed.controller.ts
@@ -7,11 +7,6 @@ import { UpdateFeedDto } from './dto/update-feed.dto'
 export class FeedController {
   constructor(private readonly feedService: FeedService) {}
 
-  @Post()
-  create(@Body() createFeedDto: CreateFeedDto) {
-    return this.feedService.create(createFeedDto)
-  }
-
   @Get()
   findAll() {
     return this.feedService.findAll()
@@ -20,15 +15,5 @@ export class FeedController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.feedService.findOne(+id)
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateFeedDto: UpdateFeedDto) {
-    return this.feedService.update(+id, updateFeedDto)
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.feedService.remove(+id)
   }
 }
