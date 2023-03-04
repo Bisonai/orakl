7ecc066fbb38090898b653ee41f4adc5e16ed3ea
Martin Kersner <martin@bisonai.com>
2023-03-04 18:16:57 +0900

    fix: lint

--- api/src/data/data.controller.ts
+++ api/src/data/data.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common'
+import { Controller, Get, Post, Body, Param } from '@nestjs/common'
 import { DataService } from './data.service'
 import { DatumDto } from './dto/datum.dto'
 
