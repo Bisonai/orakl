b7d3f124e180e30ce81e224886cdfb00431b5b7e
KelvinThai <quan.thai1987@gmail.com>
2023-03-06 16:42:16 +0700

    used node-cron to set schedule for reporter, add variable to check when request is running

--- testbots/consumer-testbot-VRF-RR/src/reporter/main.ts
+++ testbots/consumer-testbot-VRF-RR/src/reporter/main.ts
@@ -1,10 +1,19 @@
 import { reportRR } from "./request-response";
 import { reportVRF } from "./vrf";
+import nodeCron from "node-cron";
 
 async function main() {
   await reportVRF();
   await reportRR();
 }
+
+// run at 0h
+nodeCron.schedule("0 0 * * *", async () => {
+  main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
+});
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
--- testbots/consumer-testbot-VRF-RR/src/request/main.ts
+++ testbots/consumer-testbot-VRF-RR/src/request/main.ts
@@ -1,14 +1,18 @@
+import { boolean } from "mathjs";
 import { sendRequestData } from "./request-response";
 import { sendRequestDataDirect } from "./request-response-direct";
 import { sendRequestRandomWords } from "./vrf-consumer";
 import { sendRequestRandomWordsDirect } from "./vrf-consumer-direct";
-
+let running = false;
 async function main() {
   setInterval(async () => {
+    if (running) return;
+    running = true;
     await sendRequestRandomWords();
     await sendRequestRandomWordsDirect();
     await sendRequestData();
     await sendRequestDataDirect();
+    running = false;
   }, 1000 * 30);
 }
 main().catch((error) => {
