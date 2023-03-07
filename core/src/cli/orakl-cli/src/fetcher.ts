7b6d0b9a2b90cccb30bf2c386ac45781fc3cc9fd
Martin Kersner <martin@bisonai.com>
2023-03-07 11:30:44 +0900

    feat: Control (start/stop) Orakl Fetcher through `orakl-cli`

--- core/src/cli/orakl-cli/src/fetcher.ts
+++ core/src/cli/orakl-cli/src/fetcher.ts
@@ -2,18 +2,27 @@ import axios from 'axios'
 import { command, subcommands, option, string as cmdstring } from 'cmd-ts'
 import { idOption, buildUrl } from './utils'
 import { ORAKL_NETWORK_FETCHER_URL } from './settings'
-import { CliError, CliErrorCode } from './errors'
 
 export function fetcherSub() {
   const start = command({
     name: 'start',
-    args: {},
+    args: {
+      id: option({
+        type: cmdstring,
+        long: 'id'
+      })
+    },
     handler: startHandler()
   })
 
   const stop = command({
     name: 'stop',
-    args: {},
+    args: {
+      id: option({
+        type: cmdstring,
+        long: 'id'
+      })
+    },
     handler: stopHandler()
   })
 
@@ -24,17 +33,27 @@ export function fetcherSub() {
 }
 
 export function startHandler() {
-  async function wrapper() {
-    const startEndpoint = buildUrl(ORAKL_NETWORK_FETCHER_URL, 'start')
-    console.log('start')
+  async function wrapper({ id }: { id: string }) {
+    const startEndpoint = buildUrl(ORAKL_NETWORK_FETCHER_URL, `start/${id}`)
+    try {
+      const response = await axios.get(startEndpoint)
+      console.log(response?.data)
+    } catch (e) {
+      console.dir(e?.response?.data, { depth: null })
+    }
   }
   return wrapper
 }
 
 export function stopHandler() {
-  async function wrapper() {
-    const stopEndpoint = buildUrl(ORAKL_NETWORK_FETCHER_URL, 'stop')
-    console.log('stop')
+  async function wrapper({ id }: { id: string }) {
+    const stopEndpoint = buildUrl(ORAKL_NETWORK_FETCHER_URL, `stop/${id}`)
+    try {
+      const response = await axios.get(stopEndpoint)
+      console.log(response?.data)
+    } catch (e) {
+      console.dir(e?.response?.data, { depth: null })
+    }
   }
   return wrapper
 }
