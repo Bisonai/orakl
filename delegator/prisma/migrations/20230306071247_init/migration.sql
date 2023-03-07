9773213e2dd58daf33b4548709e4c2f20afcf820
bayram98 <bayramguwanjow@gmail.com>
2023-03-07 17:47:58 +0900

    feat: change prisma structure

--- delegator/prisma/migrations/20230306071247_init/migration.sql
+++ delegator/prisma/migrations/20230306071247_init/migration.sql
@@ -1,7 +1,8 @@
 -- CreateTable
 CREATE TABLE "Transaction" (
     "id" SERIAL NOT NULL,
-    "txHash" TEXT NOT NULL,
+    "tx" TEXT NOT NULL,
+    "signed" TEXT NOT NULL,
 
     CONSTRAINT "Transaction_pkey" PRIMARY KEY ("id")
 );
