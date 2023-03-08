32adde79fac2e9f5512d51297f9c70cf73ddf6bb
bayram98 <bayramguwanjow@gmail.com>
2023-03-08 14:37:14 +0900

    feat: add errors file

---
+++ delegator/src/sign/helper/errors.ts
@@ -0,0 +1,14 @@
+export class DelegatorError extends Error {
+  constructor(public readonly code: DelegatorErrorCode, message?: string, public readonly value?) {
+    super(message)
+    this.name = DelegatorErrorCode[code]
+    this.value = value
+    Object.setPrototypeOf(this, new.target.prototype)
+  }
+}
+
+export enum DelegatorErrorCode {
+  InvalidReporter,
+  InvalidContract,
+  InvalidMethod
+}
