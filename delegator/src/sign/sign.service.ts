e2bd661186bf735e8284394a48319178a88f6bc5
bayram98 <bayramguwanjow@gmail.com>
2023-03-13 18:36:40 +0900

    fix: move global functions & variables to local

--- delegator/src/sign/sign.service.ts
+++ delegator/src/sign/sign.service.ts
@@ -6,68 +6,28 @@ import Caver from 'caver-js'
 import { SignatureData } from 'caver-js'
 import { DelegatorError, DelegatorErrorCode } from './errors'
 
-const PROVIDER_URL = 'https://api.baobab.klaytn.net:8651'
-let caver, feePayerKeyring
-
-function encryptMethodName(methodName: string) {
-  return caver.klay.abi.encodeFunctionSignature(methodName)
-}
-
-async function signTxByFeePayer(input: Transaction) {
-  const signature: SignatureData = new caver.wallet.keyring.signatureData([
-    input.v,
-    input.r,
-    input.s
-  ])
-
-  const tx = await caver.transaction.feeDelegatedSmartContractExecution.create({
-    ...input,
-    signatures: [signature]
-  })
-  await caver.wallet.signAsFeePayer(feePayerKeyring.address, tx)
-  return tx.getRawTransaction()
-}
-
-function validateTransaction(tx) {
-  // FIXME remove simple whiteListing settings and setup db
-  const contractList = {
-    '0x93120927379723583c7a0dd2236fcb255e96949f': {
-      methods: ['increment()', 'decrement()'],
-      reporters: ['0x42cbc5b3fb1b7b62fb8bd7c1d475bee35ad3e5f4']
-    }
-  }
-
-  if (!(tx.to in contractList)) {
-    throw new DelegatorError(DelegatorErrorCode.InvalidContract)
-  }
-  if (!contractList[tx.to].reporters.includes(tx.from)) {
-    throw new DelegatorError(DelegatorErrorCode.InvalidReporter)
-  }
-  const isAllowedMethod = contractList[tx.to].methods.some(
-    (method) => encryptMethodName(method) == tx.input
-  )
-  if (!isAllowedMethod) {
-    throw new DelegatorError(DelegatorErrorCode.InvalidMethod)
-  }
-}
-
 @Injectable()
 export class SignService {
+  caver: any
+  feePayerKeyring: any
+
   constructor(private prisma: PrismaService) {
-    caver = new Caver(process.env.PROVIDER_URL)
-    feePayerKeyring = caver.wallet.keyring.createFromPrivateKey(process.env.DELEGATOR_PRIVATE_KEY)
-    caver.wallet.add(feePayerKeyring)
+    this.caver = new Caver(process.env.PROVIDER_URL)
+    this.feePayerKeyring = this.caver.wallet.keyring.createFromPrivateKey(
+      process.env.DELEGATOR_PRIVATE_KEY
+    )
+    this.caver.wallet.add(this.feePayerKeyring)
   }
 
   async create(data: SignDto) {
     const transaction = await this.prisma.transaction.create({ data })
     try {
-      validateTransaction(transaction)
+      this.validateTransaction(transaction)
     } catch (e) {
       const msg = `DelegatorError: ${e.name}`
       throw new HttpException(msg, HttpStatus.BAD_REQUEST)
     }
-    const signedRawTx = await signTxByFeePayer(transaction)
+    const signedRawTx = await this.signTxByFeePayer(transaction)
     const signedRawTransaction = await this.updateSignedRawTransaction(transaction.id, signedRawTx)
     return signedRawTransaction
   }
@@ -116,5 +76,47 @@ export class SignService {
     return this.prisma.transaction.delete({
       where
     })
+  }
+
+  encryptMethodName(methodName: string) {
+    return this.caver.klay.abi.encodeFunctionSignature(methodName)
+  }
+
+  async signTxByFeePayer(input: Transaction) {
+    const signature: SignatureData = new this.caver.wallet.keyring.signatureData([
+      input.v,
+      input.r,
+      input.s
+    ])
+
+    const tx = await this.caver.transaction.feeDelegatedSmartContractExecution.create({
+      ...input,
+      signatures: [signature]
+    })
+    await this.caver.wallet.signAsFeePayer(this.feePayerKeyring.address, tx)
+    return tx.getRawTransaction()
+  }
+
+  validateTransaction(tx) {
+    // FIXME remove simple whiteListing settings and setup db
+    const contractList = {
+      '0x93120927379723583c7a0dd2236fcb255e96949f': {
+        methods: ['increment()', 'decrement()'],
+        reporters: ['0x42cbc5b3fb1b7b62fb8bd7c1d475bee35ad3e5f4']
+      }
+    }
+
+    if (!(tx.to in contractList)) {
+      throw new DelegatorError(DelegatorErrorCode.InvalidContract)
+    }
+    if (!contractList[tx.to].reporters.includes(tx.from)) {
+      throw new DelegatorError(DelegatorErrorCode.InvalidReporter)
+    }
+    const isAllowedMethod = contractList[tx.to].methods.some(
+      (method) => this.encryptMethodName(method) == tx.input
+    )
+    if (!isAllowedMethod) {
+      throw new DelegatorError(DelegatorErrorCode.InvalidMethod)
+    }
   }
 }
