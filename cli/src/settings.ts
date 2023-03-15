13dc4a91d47fc4978a622dc5b96715350dd0229c
Martin Kersner <martin@bisonai.com>
2023-03-15 19:35:16 +0900

    fix: Remove default migration path

--- cli/src/settings.ts
+++ cli/src/settings.ts
@@ -2,4 +2,3 @@ export const ORAKL_NETWORK_API_URL =
   process.env.ORAKL_NETWORK_API_URL || 'http://localhost:3000/api/v1'
 export const ORAKL_NETWORK_FETCHER_URL =
   process.env.ORAKL_NETWORK_FETCHER_URL || 'http://localhost:3001/api/v1'
-export const MIGRATIONS_PATH = process.env.MIGRATIONS_PATH || 'migrations'
