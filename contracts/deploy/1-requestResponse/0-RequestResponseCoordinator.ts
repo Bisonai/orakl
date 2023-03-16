2c7b0fdcaee9256c7d1728363ed09292163c92e2
KelvinThai <54656638+KelvinThai@users.noreply.github.com>
2023-03-16 12:39:42 +0700

    Separate directories for deploy script (#391)

--- contracts/deploy/1-requestResponse/0-RequestResponseCoordinator.ts
+++ contracts/deploy/1-requestResponse/0-RequestResponseCoordinator.ts
@@ -1,7 +1,7 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types'
 import { DeployFunction } from 'hardhat-deploy/types'
-import { loadJson } from '../scripts/v0.1/utils'
-import { IRequestResponseConfig } from '../scripts/v0.1/types'
+import { loadJson } from '../../scripts/v0.1/utils'
+import { IRequestResponseConfig } from '../../scripts/v0.1/types'
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts, network } = hre
