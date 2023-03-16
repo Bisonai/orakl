1b42c84d6296f5f1889b2242305c45bc36b2da71
Martin Kersner <martin@bisonai.com>
2023-03-16 14:49:49 +0900

    feat: Read properties from to-be-flattened listener optional

--- api/src/listener/listener.utils.ts
+++ api/src/listener/listener.utils.ts
@@ -1,9 +1,9 @@
 export function flattenListener(L) {
   return {
-    id: L.id,
-    address: L.address,
-    eventName: L.eventName,
-    service: L.service.name,
-    chain: L.chain.name
+    id: L?.id,
+    address: L?.address,
+    eventName: L?.eventName,
+    service: L?.service.name,
+    chain: L?.chain?.name
   }
 }
