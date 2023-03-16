b2849a694016165a86b0a68138bd3de2453fe46b
Martin Kersner <martin@bisonai.com>
2023-03-16 14:46:12 +0900

    feat: VRF key Orakl Network API response validation

--- core/src/api.ts
+++ core/src/api.ts
@@ -24,7 +24,15 @@ export async function getVrfConfig({
 }): Promise<IVrfConfig> {
   try {
     const endpoint = buildUrl(ORAKL_NETWORK_API_URL, 'vrf')
-    return (await axios.get(endpoint, { data: { chain } }))?.data
+    const vrfKeys = (await axios.get(endpoint, { data: { chain } }))?.data
+
+    if (vrfKeys.length == 0) {
+      throw new Error(`Found no VRF key for chain [${chain}]`)
+    } else if (vrfKeys.length > 1) {
+      throw new Error(`Found more than one VRF key for chain [${chain}]`)
+    }
+
+    return vrfKeys[0]
   } catch (e) {
     logger?.error({ name: 'getVrfConfig', file: FILE_NAME, ...e }, 'error')
     throw new OraklError(OraklErrorCode.GetVrfConfigRequestFailed)
