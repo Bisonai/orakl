f93ef65507aec630a77d5a0147e2a7cf5e3b5018
jo <jo@bisonai.com>
2023-03-22 03:14:10 +0900

    fixed e2e test set time

--- api/test/app.e2e-spec.ts
+++ api/test/app.e2e-spec.ts
@@ -3,21 +3,31 @@ import { INestApplication } from '@nestjs/common'
 import * as request from 'supertest'
 import { AppModule } from './../src/app.module'
 import { setAppSettings } from './../src/app.settings'
+import { PrismaClient } from '@prisma/client'
+import { PrismaService } from './../src/prisma.service'
 
 describe('AppController (e2e)', () => {
   let app: INestApplication
+  let prisma
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule]
+      imports: [AppModule],
+      providers: [PrismaService]
     }).compile()
 
     app = moduleFixture.createNestApplication()
+    prisma = moduleFixture.get<PrismaClient>(PrismaService)
     setAppSettings(app)
     await app.init()
+  }, 10000)
+
+  afterEach(async () => {
+    jest.resetModules()
+    await prisma.$disconnect()
   })
 
   it('/api/v1 (GET)', () => {
     return request(app.getHttpServer()).get('/api/v1').expect(200).expect('Orakl Network API')
-  })
+  }, 10000)
 })
