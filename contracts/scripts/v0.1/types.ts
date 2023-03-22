bb2b50cedcee36fa163ab171a23500f96d2e6a82
Martin Kersner <martin@bisonai.com>
2023-03-22 10:25:07 +0900

    Generalize deployment scripts (#427)
    * fix: Deployment package scripts
    
    * feat: Add LICENSE
    
    * fix: Reuse deployment commands
    
    * feat: Migration solution for `Aggregator`
    
    * chore: Update README
    
    * feat: Migration style deployment for RequestResponse
    
    * fix: Validation in aggregator deployment
    
    * feat: Config validation for RequestResponse deployment
    
    * feat: VRF deployment with migrations
    
    * feat: Add validation to VRFCoordinat deployment
    
    * fix: eslint
    
    * chore: Cleanup
    
    * feat: Remove old deployment configurations
    
    * chore: Final touches
    
    * fix: Package build

--- contracts/scripts/v0.1/types.ts
+++ contracts/scripts/v0.1/types.ts
@@ -15,41 +15,85 @@ interface IFeeConfig {
   reqsForTier5: number
 }
 
-interface IVrfOracle {
-  address: string
-  publicProvingKey: [string, string]
-}
-
-interface IRequestResponseOracle {
-  address: string
-}
-
-export interface IVrfConfig {
-  maxGasLimit: number
-  gasAfterPaymentCalculation: number
-  feeConfig: IFeeConfig
-  directPaymentConfig: IDirectPaymentConfig
-  oracle: IVrfOracle[]
-  minBalance: string
-}
-
-export interface IRequestResponseConfig {
-  maxGasLimit: number
-  gasAfterPaymentCalculation: number
-  feeConfig: IFeeConfig
-  directPaymentConfig: IDirectPaymentConfig
-  oracle: IRequestResponseOracle[]
-  minBalance: string
-}
-
-export interface IAggregatorConfig {
+// Aggregator
+export interface IAggregatorDeployConfig {
   name: string
   paymentAmount: number
   timeout: number
   validator: string
-  minSubmissionCount: number
-  maxSubmissionCount: number
   decimals: number
   description: string
+  depositAmount?: number
+}
+
+export interface IAggregatorChangeOraclesConfig {
+  removed: string[]
+  added: string[]
+  addedAdmins: string[]
+  minSubmissionCount: number
+  maxSubmissionCount: number
   restartDelay: number
+  aggregatorAddress?: string
+}
+
+export interface IAggregatorConfig {
+  deploy?: IAggregatorDeployConfig
+  changeOracles?: IAggregatorChangeOraclesConfig
+}
+
+export interface ICoordinatorConfig {
+  maxGasLimit: number
+  gasAfterPaymentCalculation: number
+  feeConfig: IFeeConfig
+}
+
+// Coordinator
+export interface ICoordinatorDeploy {
+  version: string
+}
+
+export interface ICoordinatorMinBalance {
+  minBalance: string
+}
+
+export interface IAddCoordinator {
+  prepaymentAddress: string
+  coordinatorAddress: string
+}
+
+// RequestResponseCoordinator
+export interface ICoordinatorDirectPaymentConfig {
+  directPaymentConfig: IDirectPaymentConfig
+}
+
+export interface IRequestResponseCoordinatorConfig {
+  requestResponseCoordinatorAddress?: string
+  deploy?: ICoordinatorDeploy
+  registerOracle?: string[]
+  deregisterOracle?: string[]
+  setConfig?: ICoordinatorConfig
+  setDirectPaymentConfig?: ICoordinatorDirectPaymentConfig
+  setMinBalance?: ICoordinatorMinBalance
+  addCoordinator?: IAddCoordinator
+}
+
+// VRFCoordinator
+export interface IRegisterProvingKey {
+  address: string
+  publicProvingKey: [string, string]
+}
+
+export interface IDeregisterProvingKey {
+  publicProvingKey: [string, string]
+}
+
+export interface IVrfCoordinatorConfig {
+  vrfCoordinatorAddress?: string
+  deploy?: ICoordinatorDeploy
+  registerProvingKey?: IRegisterProvingKey[]
+  deregisterProvingKey?: IDeregisterProvingKey[]
+  setConfig?: ICoordinatorConfig
+  setDirectPaymentConfig?: ICoordinatorDirectPaymentConfig
+  setMinBalance?: ICoordinatorMinBalance
+  addCoordinator?: IAddCoordinator
 }
--- contracts/scripts/v0.1/utils.ts
+++ contracts/scripts/v0.1/utils.ts
@@ -1,6 +1,198 @@
-import { readFile } from 'node:fs/promises'
+import { readdir, readFile, appendFile } from 'node:fs/promises'
+import * as path from 'node:path'
+import {
+  ICoordinatorMinBalance,
+  ICoordinatorDeploy,
+  IAggregatorDeployConfig,
+  IAggregatorChangeOraclesConfig,
+  IRegisterProvingKey,
+  IDeregisterProvingKey,
+  ICoordinatorConfig,
+  ICoordinatorDirectPaymentConfig
+} from './types'
+
+const MIGRATION_LOCK_FILE_NAME = 'migration.lock'
 
 export async function loadJson(filepath: string) {
-  const json = await readFile(filepath, 'utf8')
-  return JSON.parse(json)
+  try {
+    const json = await readFile(filepath, 'utf8')
+    return JSON.parse(json)
+  } catch (e) {
+    console.error(e)
+    throw e
+  }
+}
+
+async function readMigrationLockFile(filePath: string) {
+  return (await readFile(filePath, 'utf8')).toString().trim().split('\n')
+}
+
+/**
+ * Migrations directory includes migration JSON files and migration lock file.
+ * Migration JSON files should have a names that explains the
+ * migration purpose and which can be chronological order. If there is
+ * no migration lock file, we assume that no migration has been run
+ * yet. `loadMigration` function examines migration JSON files,
+ * migration lock file and determines which migration JSON files
+ * should be used for next migration.
+ *
+ * @param {string} migrations directory
+ * @return {Promise<string[]>} list of migration files names that has
+ * not been applied yet
+ */
+export async function loadMigration(dirPath: string): Promise<string[]> {
+  const jsonFileRegex = /\.json$/
+
+  let migrationLockFileExist = false
+  const allMigrations = []
+
+  try {
+    const files = await readdir(dirPath)
+
+    for (const file of files) {
+      if (file === MIGRATION_LOCK_FILE_NAME) {
+        migrationLockFileExist = true
+      } else if (jsonFileRegex.test(file.toLowerCase())) {
+        allMigrations.push(file)
+      }
+    }
+  } catch (err) {
+    console.error(err)
+  }
+
+  let doneMigrations: string[] = []
+  if (migrationLockFileExist) {
+    const migrationLockFilePath = path.join(dirPath, MIGRATION_LOCK_FILE_NAME)
+    doneMigrations = await readMigrationLockFile(migrationLockFilePath)
+  }
+
+  // Keep only those migrations that have not been applied yet
+  const todoMigrations = allMigrations.filter((x) => !doneMigrations.includes(x))
+  todoMigrations.sort()
+  return todoMigrations
+}
+
+/**
+ * Update migration lock file located in `dirPath` with the `migrationFileName` migration.
+ *
+ * @params {dirPath} migration directory
+ * @params {migrationFileName} name of executed migration file that should be included to migration lock file
+ * @return {Promise<void>}
+ */
+export async function updateMigration(dirPath: string, migrationFileName: string) {
+  const migrationLockFilePath = path.join(dirPath, MIGRATION_LOCK_FILE_NAME)
+  await appendFile(migrationLockFilePath, `${migrationFileName}\n`)
+}
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+function validateProperties(config: any, requiredProperties: string[]) {
+  for (const rp of requiredProperties) {
+    if (config[rp] === undefined) return false
+  }
+
+  return true
+}
+
+export function validateAggregatorDeployConfig(config: IAggregatorDeployConfig): boolean {
+  const requiredProperties = [
+    'name',
+    'paymentAmount',
+    'timeout',
+    'validator',
+    'decimals',
+    'description'
+  ]
+
+  if (!validateProperties(config, requiredProperties)) return false
+
+  if (config.paymentAmount > 0 && config.depositAmount && config.depositAmount > 0) {
+    return false
+  }
+
+  return true
+}
+
+export function validateAggregatorChangeOraclesConfig(
+  config: IAggregatorChangeOraclesConfig
+): boolean {
+  const requiredProperties = [
+    'removed',
+    'added',
+    'addedAdmins',
+    'minSubmissionCount',
+    'maxSubmissionCount',
+    'restartDelay'
+  ]
+
+  if (!validateProperties(config, requiredProperties)) {
+    return false
+  } else {
+    return true
+  }
+}
+
+export function validateCoordinatorDeployConfig(config: ICoordinatorDeploy): boolean {
+  const requiredProperties = ['version']
+
+  if (!validateProperties(config, requiredProperties)) {
+    return false
+  } else {
+    return true
+  }
+}
+
+export function validateMinBalanceConfig(config: ICoordinatorMinBalance): boolean {
+  const requiredProperties = ['minBalance']
+
+  if (!validateProperties(config, requiredProperties)) {
+    return false
+  } else {
+    return true
+  }
+}
+
+export function validateSetConfig(config: ICoordinatorConfig): boolean {
+  const requiredProperties = ['maxGasLimit', 'gasAfterPaymentCalculation', 'feeConfig']
+
+  if (!validateProperties(config, requiredProperties)) {
+    return false
+  } else {
+    return true
+  }
+}
+
+export function validateDirectPaymentConfig(config: ICoordinatorDirectPaymentConfig): boolean {
+  if (!validateProperties(config, ['directPaymentConfig'])) {
+    return false
+  }
+
+  if (!validateProperties(config.directPaymentConfig, ['fulfillmentFee', 'baseFee'])) {
+    return false
+  }
+
+  return true
+}
+
+export function validateVrfRegisterProvingKey(config: IRegisterProvingKey[]): boolean {
+  const requiredProperties = ['address', 'publicProvingKey']
+
+  for (const c of config) {
+    if (!validateProperties(c, requiredProperties)) {
+      return false
+    }
+  }
+
+  return true
+}
+
+export function validateVrfDeregisterProvingKey(config: IDeregisterProvingKey[]): boolean {
+  const requiredProperties = ['publicProvingKey']
+
+  for (const c of config) {
+    if (!validateProperties(c, requiredProperties)) {
+      return false
+    }
+  }
+
+  return true
 }
