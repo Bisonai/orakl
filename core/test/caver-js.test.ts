24f68ee2e61463074793345e75f241d63d8750a8
Martin Kersner <martin@bisonai.com>
2023-03-23 11:26:49 +0900

    fix: Add await for send transaction in test

--- core/test/caver-js.test.ts
+++ core/test/caver-js.test.ts
@@ -17,13 +17,15 @@ describe('Test Caver-js', function () {
       const beforeBalanceOfTo = await caver.klay.getBalance(to)
       const beforeBalanceOfAccount = await caver.klay.getBalance(account.address)
 
-      const txReceipt = await caver.klay.sendTransaction({
-        type: 'VALUE_TRANSFER',
-        from: account.address,
-        to: to,
-        gas: '21000',
-        value: amount
-      })
+      const txReceipt = await (
+        await caver.klay.sendTransaction({
+          type: 'VALUE_TRANSFER',
+          from: account.address,
+          to: to,
+          gas: '21000',
+          value: amount
+        })
+      ).wait()
 
       const txFee = BigNumber.from(txReceipt.effectiveGasPrice).mul(
         BigNumber.from(txReceipt.gasUsed)
