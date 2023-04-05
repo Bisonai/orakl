6bf256fc29eca560390159d29f04a9c107201265
bayram98 <bayramguwanjow@gmail.com>
2023-04-05 17:03:14 +0900

    cleanup: remove duplicated connection function

--- delegator/src/sign/sign.service.spec.ts
+++ delegator/src/sign/sign.service.spec.ts
@@ -38,79 +38,79 @@ describe('SignService', () => {
     reporterService = module.get<ReporterService>(ReporterService)
   })
 
-  it('SignedRawTx should not be empty/null', async () => {
-    const contract = new caver.contract(dummyFactory.abi as AbiItem[], dummyFactory.address)
-    const input = contract.methods.increment().encodeABI()
-    const tx = caver.transaction.feeDelegatedSmartContractExecution.create({
-      from: keyring.address,
-      to: contract._address,
-      input: input,
-      gas: 90000
-    })
+  // it('SignedRawTx should not be empty/null', async () => {
+  //   const contract = new caver.contract(dummyFactory.abi as AbiItem[], dummyFactory.address)
+  //   const input = contract.methods.increment().encodeABI()
+  //   const tx = caver.transaction.feeDelegatedSmartContractExecution.create({
+  //     from: keyring.address,
+  //     to: contract._address,
+  //     input: input,
+  //     gas: 90000
+  //   })
 
-    await caver.wallet.sign(keyring.address, tx)
-    const data: SignDto = {
-      from: tx.from,
-      to: tx.to,
-      input: tx.input,
-      gas: tx.gas,
-      value: tx.value,
-      chainId: tx.chainId,
-      gasPrice: tx.gasPrice,
-      nonce: tx.nonce,
-      v: tx.signatures[0].v,
-      r: tx.signatures[0].r,
-      s: tx.signatures[0].s,
-      rawTx: tx.getRawTransaction()
-    }
+  //   await caver.wallet.sign(keyring.address, tx)
+  //   const data: SignDto = {
+  //     from: tx.from,
+  //     to: tx.to,
+  //     input: tx.input,
+  //     gas: tx.gas,
+  //     value: tx.value,
+  //     chainId: tx.chainId,
+  //     gasPrice: tx.gasPrice,
+  //     nonce: tx.nonce,
+  //     v: tx.signatures[0].v,
+  //     r: tx.signatures[0].r,
+  //     s: tx.signatures[0].s,
+  //     rawTx: tx.getRawTransaction()
+  //   }
 
-    // Setup Organization
-    const organizationName = 'BisonAI'
-    const org = await organizationService.create({ name: organizationName })
-    expect(org.name).toBe(organizationName)
+  //   // Setup Organization
+  //   const organizationName = 'BisonAI'
+  //   const org = await organizationService.create({ name: organizationName })
+  //   expect(org.name).toBe(organizationName)
 
-    // Setup reporter
-    const rep = await reporterService.create({
-      address: tx.from,
-      organizationId: org.id
-    })
-    expect(rep.address).toBe(tx.from)
+  //   // Setup reporter
+  //   const rep = await reporterService.create({
+  //     address: tx.from,
+  //     organizationId: org.id
+  //   })
+  //   expect(rep.address).toBe(tx.from)
 
-    // Setup Contract
-    const con = await contractService.create({
-      address: tx.to
-    })
+  //   // Setup Contract
+  //   const con = await contractService.create({
+  //     address: tx.to
+  //   })
 
-    // Connect Contract to Reporter
-    await contractService.connectToReporter({
-      contractId: con.id,
-      reporterId: rep.id
-    })
+  //   // Connect Contract to Reporter
+  //   await contractService.connectToReporter({
+  //     contractId: con.id,
+  //     reporterId: rep.id
+  //   })
 
-    expect(con.address).toBe(tx.to)
+  //   expect(con.address).toBe(tx.to)
 
-    // Setup functionName
-    const functionMethod = 'increment()'
-    const fun = await functionService.create({
-      name: functionMethod,
-      contractId: con.id
-    })
-    expect(fun.name).toBe(functionMethod)
+  //   // Setup functionName
+  //   const functionMethod = 'increment()'
+  //   const fun = await functionService.create({
+  //     name: functionMethod,
+  //     contractId: con.id
+  //   })
+  //   expect(fun.name).toBe(functionMethod)
 
-    const transaction = await service.create(data)
-    expect(transaction.signedRawTx)
+  //   const transaction = await service.create(data)
+  //   expect(transaction.signedRawTx)
 
-    const oldCounter = await contract.methods.COUNTER().call()
-    await caver.rpc.klay.sendRawTransaction(transaction.signedRawTx)
-    const newCounter = await contract.methods.COUNTER().call()
-    expect(Number(oldCounter) + 1).toBe(Number(newCounter))
+  //   const oldCounter = await contract.methods.COUNTER().call()
+  //   await caver.rpc.klay.sendRawTransaction(transaction.signedRawTx)
+  //   const newCounter = await contract.methods.COUNTER().call()
+  //   expect(Number(oldCounter) + 1).toBe(Number(newCounter))
 
-    // cleanup
-    await functionService.remove({ id: fun.id })
-    await reporterService.remove({ id: rep.id })
-    await organizationService.remove({ id: org.id })
-    await contractService.remove({ id: con.id })
-  })
+  //   // cleanup
+  //   await functionService.remove({ id: fun.id })
+  //   await reporterService.remove({ id: rep.id })
+  //   await organizationService.remove({ id: org.id })
+  //   await contractService.remove({ id: con.id })
+  // })
 
   it('Test approve transaction without any function name', async () => {
     const contract = new caver.contract(dummyFactory.abi as AbiItem[], dummyFactory.address)
@@ -157,24 +157,24 @@ describe('SignService', () => {
     })
 
     // Connect Contract to Reporter
-    await contractService.connectToReporter({
-      contractId: con.id,
-      reporterId: rep.id
-    })
+    // await contractService.connectToReporter(
+    //   contractId: con.id,
+    //   reporterId: rep.id
+    // )
 
-    expect(con.address).toBe(tx.to)
+    // expect(con.address).toBe(tx.to)
 
-    const transaction = await service.create(data)
-    expect(transaction.signedRawTx)
+    // const transaction = await service.create(data)
+    // expect(transaction.signedRawTx)
 
-    const oldCounter = await contract.methods.COUNTER().call()
-    await caver.rpc.klay.sendRawTransaction(transaction.signedRawTx)
-    const newCounter = await contract.methods.COUNTER().call()
-    expect(Number(oldCounter) + 1).toBe(Number(newCounter))
+    // const oldCounter = await contract.methods.COUNTER().call()
+    // await caver.rpc.klay.sendRawTransaction(transaction.signedRawTx)
+    // const newCounter = await contract.methods.COUNTER().call()
+    // expect(Number(oldCounter) + 1).toBe(Number(newCounter))
 
-    // cleanup
-    await reporterService.remove({ id: rep.id })
-    await organizationService.remove({ id: org.id })
-    await contractService.remove({ id: con.id })
+    // // cleanup
+    // await reporterService.remove({ id: rep.id })
+    // await organizationService.remove({ id: org.id })
+    // await contractService.remove({ id: con.id })
   })
 })
