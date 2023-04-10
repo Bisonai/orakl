310747d923ef1148a6272bd1116a1216ef7c59dd
bayram98 <bayramguwanjow@gmail.com>
2023-04-10 17:48:16 +0900

    feat: connect delegator contracts with cli

--- cli/src/delegator.ts
+++ cli/src/delegator.ts
@@ -12,6 +12,10 @@ export function delegatorSub() {
   // delegator reporterInsert --address {reporterAddress} --organizationId {organizationId}
   // delegator reporterRemove --id {reporterId}
 
+  // delegator contractList
+  // delegator contractInsert --address {contractAddress}
+  // delegator contractRemove --id {reporterId}
+
   const organizationList = command({
     name: 'organization-list',
     args: {},
@@ -65,6 +69,32 @@ export function delegatorSub() {
     },
     handler: reporterRemoveHandler()
   })
+
+  const contractList = command({
+    name: 'contract-list',
+    args: {},
+    handler: contractListHandler()
+  })
+
+  const contractInsert = command({
+    name: 'contract-insert',
+    args: {
+      address: option({
+        type: cmdstring,
+        long: 'address'
+      })
+    },
+    handler: contractInsertHandler()
+  })
+
+  const contractRemove = command({
+    name: 'contract-insert',
+    args: {
+      id: idOption
+    },
+    handler: contractRemoveHandler()
+  })
+
   return subcommands({
     name: 'delegator',
     cmds: {
@@ -73,7 +103,10 @@ export function delegatorSub() {
       organizationRemove,
       reporterList,
       reporterInsert,
-      reporterRemove
+      reporterRemove,
+      contractList,
+      contractInsert,
+      contractRemove
     }
   })
 }
@@ -171,6 +204,54 @@ export function reporterRemoveHandler() {
       console.log(result?.data)
     } catch (e) {
       console.error('Delegator Reporter was not deleted. Reason:')
+      console.error(e?.response?.data?.message)
+    }
+  }
+  return wrapper
+}
+
+export function contractListHandler() {
+  async function wrapper() {
+    if (!(await isOraklDelegatorHealthy())) return
+
+    try {
+      const endpoint = buildUrl(ORAKL_NETWORK_DELEGATOR_URL, `contract`)
+      const result = await axios.get(endpoint)
+      console.log(result?.data)
+    } catch (e) {
+      console.error('Delegator Orgzanization was not listed. Reason:')
+      console.error(e?.response?.data?.message)
+    }
+  }
+  return wrapper
+}
+
+export function contractInsertHandler() {
+  async function wrapper({ address }: { address: string }) {
+    if (!(await isOraklDelegatorHealthy())) return
+
+    try {
+      const endpoint = buildUrl(ORAKL_NETWORK_DELEGATOR_URL, `contract`)
+      const result = await axios.post(endpoint, { address })
+      console.log(result?.data)
+    } catch (e) {
+      console.error('Delegator Orgzanization was not inserted. Reason:')
+      console.error(e?.response?.data?.message)
+    }
+  }
+  return wrapper
+}
+
+export function contractRemoveHandler() {
+  async function wrapper({ id }: { id: number }) {
+    if (!(await isOraklDelegatorHealthy())) return
+
+    try {
+      const endpoint = buildUrl(buildUrl(ORAKL_NETWORK_DELEGATOR_URL, `contract`), id.toString())
+      const result = await axios.delete(endpoint)
+      console.log(result?.data)
+    } catch (e) {
+      console.error('Delegator Orgzanization was not deleted. Reason:')
       console.error(e?.response?.data?.message)
     }
   }
