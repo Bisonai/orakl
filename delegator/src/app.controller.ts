60038d470cbaefc49b310d24f6c8982c964f1f18
bayram98 <bayramguwanjow@gmail.com>
2023-04-10 14:43:24 +0900

    feat: add app versioning

--- delegator/src/app.controller.ts
+++ delegator/src/app.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common'
 import { AppService } from './app.service'
 
-@Controller()
+@Controller({ version: '1' })
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
