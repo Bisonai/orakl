407c971b2da37229fa80a9e55f3841b70f7e89bc
KelvinThai <quan.thai1987@gmail.com>
2023-04-14 14:39:08 +0700

    increased interval time for sending request (rr coordinator)

--- testbots/consumer-testbot-VRF-RR/src/request/main.ts
+++ testbots/consumer-testbot-VRF-RR/src/request/main.ts
@@ -3,19 +3,29 @@ import { sendRequestData } from "./request-response";
 import { sendRequestDataDirect } from "./request-response-direct";
 import { sendRequestRandomWords } from "./vrf-consumer";
 import { sendRequestRandomWordsDirect } from "./vrf-consumer-direct";
-let running = false;
+let vrfRunning = false;
+let rrRunning = false;
+
 async function main() {
   setInterval(async () => {
-    if (running) return;
-    running = true;
+    if (vrfRunning) return;
+    vrfRunning = true;
     const d = new Date();
     const m = d.toISOString().split("T")[0];
     await sendRequestRandomWords(m);
     await sendRequestRandomWordsDirect(m);
-    // await sendRequestData(m);
-    // await sendRequestDataDirect(m);
-    running = false;
+    vrfRunning = false;
   }, 1000 * 30);
+
+  setInterval(async () => {
+    if (rrRunning) return;
+    rrRunning = true;
+    const d = new Date();
+    const m = d.toISOString().split("T")[0];
+    await sendRequestData(m);
+    await sendRequestDataDirect(m);
+    rrRunning = false;
+  }, 1000 * 70);
 }
 main().catch((error) => {
   console.error(error);
