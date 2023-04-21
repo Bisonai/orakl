86c6331898a68856d5780c8363c8f9bce0b2fd47
Martin Kersner <martin@bisonai.com>
2023-04-21 15:11:08 +0900

    feat: Simplify voting code

--- contracts/src/v0.1/libraries/MajorityVoting.sol
+++ contracts/src/v0.1/libraries/MajorityVoting.sol
@@ -4,15 +4,15 @@ pragma solidity ^0.8.16;
 library MajorityVoting {
     error EvenLengthList();
 
-    function voting(bool[] memory list) pure internal returns(bool) {
+    function voting(bool[] memory list) internal pure returns (bool) {
         if (list.length % 2 == 0) {
             revert EvenLengthList();
         }
-        uint256 trueCount = 0;
-        uint256 falseCount = 0;
+        uint256 trueCount;
+        uint256 falseCount;
 
         for (uint256 i; i < list.length; ++i) {
-            if (list[i] == true) {
+            if (list[i]) {
                 trueCount++;
             } else {
                 falseCount++;
