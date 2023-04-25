c7103a4e2babb657c4629d2ba92bd168029e8402
Martin Kersner <martin@bisonai.com>
2023-04-25 12:23:35 +0900

    chore: Interface VRF coordinator comment updates

--- contracts/src/v0.1/interfaces/IVRFCoordinator.sol
+++ contracts/src/v0.1/interfaces/IVRFCoordinator.sol
@@ -5,16 +5,16 @@ interface IVRFCoordinator {
     /**
      * @notice Get configuration relevant for making requests
      * @return maxGasLimit global max for request gas limit
-     * @return s_provingKeyHashes list of registered key hashes
+     * @return sKeyHashes list of registered key hashes
      */
     function getRequestConfig() external view returns (uint32, bytes32[] memory);
 
     /**
-     * @notice Request a set of random words.
+     * @notice Request a set of random words using a [regular] account.
      * @param keyHash - Corresponds to a particular oracle job which uses
      * that key for generating the VRF proof. Different keyHash's have different gas price
      * ceilings, so you can select a specific one to bound your maximum per request cost.
-     * @param accId  - The ID of the account. Must be funded
+     * @param accId - The ID of the account. Must be funded
      * with the minimum account balance required for the selected keyHash.
      * @param callbackGasLimit - How much gas you'd like to receive in your
      * fulfillRandomWords callback. Note that gasleft() inside fulfillRandomWords
@@ -36,8 +36,7 @@ interface IVRFCoordinator {
     ) external returns (uint256 requestId);
 
     /**
-     * @notice Request a set of random words through a direct payment
-     * @notice method.
+     * @notice Request a set of random words using a [temporary] account.
      * @param keyHash - Corresponds to a particular oracle job which uses
      * that key for generating the VRF proof. Different keyHash's have different gas price
      * ceilings, so you can select a specific one to bound your maximum per request cost.
