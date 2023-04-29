7cc8e64001cecb4bc93f3c628457bbb197879872
Martin Kersner <martin@bisonai.com>
2023-04-29 15:55:05 +0900

    feat: Fee tier test

---
+++ contracts/src/v0.1/mocks/CoordinatorBaseMock.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+
+import "../CoordinatorBase.sol";
+
+contract CoordinatorBaseMock is CoordinatorBase {
+    function computeFee(uint64 reqCount) external view returns (uint256) {
+        return calculateServiceFee(reqCount);
+    }
+
+    function pendingRequestExists(
+        address /*consumer*/,
+        uint64 /*accId*/,
+        uint64 /*nonce*/
+    ) external view returns (bool) {
+        return false;
+    }
+}
