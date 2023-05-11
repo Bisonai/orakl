2a5cf225a6676f5d0903f7db6d7caffa4625418b
Martin Kersner <martin@bisonai.com>
2023-05-11 19:16:45 +0900

    chore: Update VRF script to cjs

--- contracts/scripts/v0.1/vrf/read-vrf.cjs
+++ contracts/scripts/v0.1/vrf/read-vrf.cjs
@@ -1,4 +1,4 @@
-import { ethers } from 'hardhat'
+const { ethers } = require('hardhat')
 
 async function main() {
   const userContract = await ethers.getContract('VRFConsumerMock')
--- contracts/scripts/v0.1/vrf/request-vrf-direct.cjs
+++ contracts/scripts/v0.1/vrf/request-vrf-direct.cjs
@@ -1,5 +1,5 @@
-import { ethers } from 'hardhat'
-import hre from 'hardhat'
+const { ethers } = require('hardhat')
+const hre = require('hardhat')
 
 async function main() {
   const vrfConsumerMock = await ethers.getContract('VRFConsumerMock')
@@ -12,11 +12,12 @@ async function main() {
   )
 
   const keyHash = '0x47ede773ef09e40658e643fe79f8d1a27c0aa6eb7251749b268f829ea49f2024'
-  const accId = 1
   const callbackGasLimit = 500_000
   const numWords = 1
 
-  await vrfConsumerSigner.requestRandomWords(keyHash, accId, callbackGasLimit, numWords)
+  await vrfConsumerSigner.requestRandomWordsDirectPayment(keyHash, callbackGasLimit, numWords, {
+    value: ethers.utils.parseEther('1.0')
+  })
 }
 
 main().catch((error) => {
