9f3c4c2c744cce7bb5515555fa8b44232ec221dd
joey <joey@bisonai.com>
2023-05-15 21:45:19 +0900

    feat: Bull monitor route page

---
+++ admin/src/app/bullmonitor/layout.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function BullMonitorLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
---
+++ admin/src/app/bullmonitor/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import BullMonitorTemplate from "@/components/Template/BullMonitor/main";
+
+export default function BullMonitorMain() {
+  return <BullMonitorTemplate />;
+}
