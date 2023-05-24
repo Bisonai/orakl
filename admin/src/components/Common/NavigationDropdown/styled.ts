76df6d84b6c804cdd38f486f3149ba136d1516ca
joey <joey@bisonai.com>
2023-05-24 09:55:10 +0900

    fix: Header and nav dropdown style

--- admin/src/components/Common/NavigationDropdown/styled.ts
+++ admin/src/components/Common/NavigationDropdown/styled.ts
@@ -6,7 +6,7 @@ export const NavDropdownContainer = styled.div`
   height: 100%;
   width: 200px;
   font-size: 16px;
-  background: #9ba4b5;
+  background: #222831;
   padding: 40px 0px 40px 20px;
 `;
 
@@ -20,6 +20,7 @@ export const AccordionItem = styled.div`
   display: flex;
   flex-direction: column;
   margin-bottom: 8px;
+  border-radius: 4px;
 `;
 
 export const AccordionHeader = styled.div`
@@ -27,9 +28,11 @@ export const AccordionHeader = styled.div`
   align-items: center;
   justify-content: space-between;
   padding: 8px 16px;
-  color: white;
+  color: #eeeeee;
+  border-radius: 4px;
   height: 50px;
-  background-color: #212a3e;
+  font-weight: 600;
+  background-color: #00adb5;
   cursor: pointer;
 `;
 
@@ -47,7 +50,6 @@ const slideDown = keyframes`
 export const AccordionContent = styled.div`
   display: flex;
   flex-direction: column;
-
   overflow: hidden;
   animation: slideDown 0.3s ease;
   a {
