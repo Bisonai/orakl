0a88985b8def316436f5ac7ac63477484e6202a0
bayram98 <bayramguwanjow@gmail.com>
2023-05-30 16:56:50 +0900

    feat: add test for error endpoints

--- api/src/error/error.service.spec.ts
+++ api/src/error/error.service.spec.ts
@@ -1,18 +1,48 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { ErrorService } from './error.service';
+import { Test, TestingModule } from '@nestjs/testing'
+import { ErrorService } from './error.service'
+import { PrismaService } from '../prisma.service'
+import { PrismaClient } from '@prisma/client'
 
 describe('ErrorService', () => {
-  let service: ErrorService;
+  let service: ErrorService
+  let prisma
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ErrorService],
-    }).compile();
+      providers: [ErrorService, PrismaService]
+    }).compile()
 
-    service = module.get<ErrorService>(ErrorService);
-  });
+    service = module.get<ErrorService>(ErrorService)
+    prisma = module.get<PrismaClient>(PrismaService)
+  })
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+  afterAll(async () => {
+    jest.resetModules()
+    await prisma.$disconnect()
+  })
+
+  it('should create new Error', async () => {
+    const errorMsg = `"type": "OraklError",
+    "message": "",
+    "stack":
+        MissingKeyInJson
+            at wrapper (file:///app/dist/worker/reducer.js:19:23)
+            at file:///app/dist/utils.js:11:61
+            at Array.reduce (<anonymous>)
+            at file:///app/dist/utils.js:11:44
+            at processRequest (file:///app/dist/worker/request-response.js:58:34)
+            at process.processTicksAndRejections (node:internal/process/task_queues:95:5)
+            at async Worker.wrapper [as processFn] (file:///app/dist/worker/request-response.js:27:25)
+            at async Worker.processJob (/app/node_modules/bullmq/dist/cjs/classes/worker.js:339:28)
+            at async Worker.retryIfFailed (/app/node_modules/bullmq/dist/cjs/classes/worker.js:513:24)
+    "code": 10020,
+    "name": "MissingKeyInJson"`
+    const errorObj = {
+      requestId: '66649924661314489704239946349158829048302840686075232939396730072454733114998',
+      timestamp: '123421841',
+      errorMsg: errorMsg
+    }
+    const errorIns = await service.create(errorObj)
+    expect(errorIns).toBeDefined()
+  })
+})
