38d5dad4bc5a8b55c93adc9a0169bd03433f15a4
Bayram Guvanjov <bayramguwanjow@gmail.com>
2023-06-23 17:33:01 +0900

    migrate `orakl-api` db (#711)
    * feat: migrate orakl-api db
    
    * fix: change import of PrismaService in error module

--- api/src/error/error.module.ts
+++ api/src/error/error.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common'
 import { ErrorService } from './error.service'
 import { ErrorController } from './error.controller'
-import { PrismaService } from 'src/prisma.service'
+import { PrismaService } from '../prisma.service'
 
 @Module({
   controllers: [ErrorController],
