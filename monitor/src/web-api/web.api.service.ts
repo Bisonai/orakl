7ed11e47cb1228fcc8ff63318a892a745820a877
jo.lol <jo@bisonai.com>
2023-06-28 16:25:19 +0900

    Update web.api.service.ts

--- monitor/src/web-api/web.api.service.ts
+++ monitor/src/web-api/web.api.service.ts
@@ -1,11 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import { PoolClient } from "pg";
-import { ethers } from "ethers";
 import { CommonConfigService } from "src/common/common.config";
-
-import { MonitorConfigService } from "src/monitor.config/monitor.config.service";
-import { MONITOR_CONFIG } from "src/common/types";
-import { IncomingWebhook } from "@slack/webhook";
 import { OraklServiceRepository } from "./orakl.service.repository";
 import { ErrorResultDto } from "./dto/error.dto";
 
