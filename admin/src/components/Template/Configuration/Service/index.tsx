cf33fea34b38ecd9b15bff60b2ca47fd7a87c9d9
joey <joey@bisonai.com>
2023-07-05 10:10:38 +0900

    refactor: Service config component

--- admin/src/components/Template/Configuration/Service/index.tsx
+++ admin/src/components/Template/Configuration/Service/index.tsx
@@ -1,67 +1,19 @@
 import { fetchInternalApi } from "@/utils/api";
-import { useQuery } from "react-query";
+import { useQuery, useMutation } from "react-query";
 import TwoColumnTable from "../ConfigurationTable/TwoColumnTable";
-import { useMutation } from "react-query";
+import { useApi } from "@/lib/useApi";
+
 const Service = () => {
-  const configServiceQuery = useQuery({
-    queryKey: ["configService"],
-    queryFn: () =>
-      fetchInternalApi(
-        {
-          target: "getConfigService",
-          method: "GET",
-        },
-        []
-      ),
-    refetchOnWindowFocus: false,
-
-    select: (data) => {
-      return data.data;
-    },
+  const { configQuery, addMutation, deleteMutation } = useApi({
+    name: "getServiceConfig",
+    name2: "modifyServiceConfig",
+    key: "serviceConfig",
   });
-
-  const addService = async (newServiceName: string) => {
-    const response = await fetch(`http://localhost:3030/api/v1/service`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ name: newServiceName }),
-    });
-
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    return response.json();
-  };
-
-  const deleteService = async (id: any) => {
-    const response = await fetch(`http://localhost:3030/api/v1/service/${id}`, {
-      method: "DELETE",
-    });
-
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-
-    return response.json();
-  };
-
-  const addServiceMutation = useMutation(addService, {
-    onSuccess: (data) => {
-      console.log("Data after addServiceMutation:", data);
-    },
-    onError: (error) => {
-      console.log("Error with addServiceMutation:", error);
-    },
-  });
-  const deleteServiceMutation = useMutation(deleteService);
-
   return (
     <>
       <TwoColumnTable
         title="Service List"
-        data={configServiceQuery.data}
+        data={configQuery.data}
         buttonProps={{
           text: "Add Service",
           width: "150px",
@@ -76,9 +28,9 @@ const Service = () => {
         }
         deleteTitle={"Would you like to remove the selected Service?"}
         addConfirmText={"Add"}
-        deleteConfrimText={"Delete"}
-        onDelete={(id) => deleteServiceMutation.mutate(id)}
-        onAdd={(newServiceName) => addServiceMutation.mutate(newServiceName)}
+        deleteConfirmText={"Delete"}
+        onDelete={(id) => deleteMutation.mutate(id)}
+        onAdd={(newServiceName) => addMutation.mutate(newServiceName)}
       />
     </>
   );
