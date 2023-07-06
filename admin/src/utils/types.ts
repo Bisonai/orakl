5002d67e5cd7adce25b062e4165657cf3725b336
joey <joey@bisonai.com>
2023-07-06 09:47:58 +0900

    feat: Add interfaces for Listener, vrfKeys, Adapter, Aggregator, and Reporter

--- admin/src/utils/types.ts
+++ admin/src/utils/types.ts
@@ -122,6 +122,53 @@ export interface IConfigurationProps {
   fetcher: string;
   delegator: string;
 }
+export interface IListenerProps {
+  id: string;
+  address: string;
+  eventName: string;
+  service: string;
+  chain: string;
+}
+
+export interface IvrfKeysProps {
+  id: string;
+  sk: string;
+  pk: string;
+  pkX: string;
+  pkY: string;
+  keyHash: string;
+  chain: string;
+}
+
+export interface IAdapterProps {
+  id: string;
+  adapterHash: string;
+  name: string;
+  decimals: number;
+}
+
+export interface IAggregatorProps {
+  aggregatorHash: string;
+  active: boolean;
+  name: string;
+  address: string;
+  heartbeat: number;
+  threshold: number;
+  absoluteThreshold: number;
+  adapterId: string;
+  chainId: string;
+}
+
+export interface IReporterProps {
+  id: string;
+  address: string;
+  privateKey: string;
+  oracleAddress: string;
+  service: string;
+  chain: string;
+}
+
+/** Popup */
 export interface IDimmedPopupProps {
   title: string;
   confirmText?: string;
