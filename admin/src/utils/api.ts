e584ffb8edafac70e26317edbe5b34276c302cf8
joey <joey@bisonai.com>
2023-07-06 09:48:57 +0900

    feat: Add functions to generate API endpoint URLs

--- admin/src/utils/api.ts
+++ admin/src/utils/api.ts
@@ -30,12 +30,27 @@ export const api = {
     status: string;
   }) =>
     `${NEXT_PUBLIC_API_QUEUES_URL}/queues/${serviceName}/${queueName}/${status}`,
-  getChainConfig: () => `${NEXT_PUBLIC_API_BASE_URL}/api/v1/chain/`,
-  getServiceConfig: () => `${NEXT_PUBLIC_API_BASE_URL}/api/v1/service/`,
+  getChainConfig: () => `${NEXT_PUBLIC_API_BASE_URL}/api/v1/chain`,
+  getServiceConfig: () => `${NEXT_PUBLIC_API_BASE_URL}/api/v1/service`,
+  getListenerConfig: () => `${NEXT_PUBLIC_API_BASE_URL}/api/v1/listener`,
+  getVrfKeysConfig: () => `${NEXT_PUBLIC_API_BASE_URL}/api/v1/vrf`,
+  getAdapterConfig: () => `${NEXT_PUBLIC_API_BASE_URL}/api/v1/adapter`,
+  getAggregatorConfig: () => `${NEXT_PUBLIC_API_BASE_URL}/api/v1/aggregator`,
+  getReporterConfig: () => `${NEXT_PUBLIC_API_BASE_URL}/api/v1/reporter`,
   modifyChainConfig: (id: string) =>
     `${NEXT_PUBLIC_API_BASE_URL}/api/v1/chain/${id}`,
   modifyServiceConfig: (id: string) =>
     `${NEXT_PUBLIC_API_BASE_URL}/api/v1/service/${id}`,
+  modifyListenerConfig: (id: string) =>
+    `${NEXT_PUBLIC_API_BASE_URL}/api/v1/listener/${id}`,
+  modifyVrfKeysConfig: (id: string) =>
+    `${NEXT_PUBLIC_API_BASE_URL}/api/v1/vrf/${id}`,
+  modifyAdapterConfig: (id: string) =>
+    `${NEXT_PUBLIC_API_BASE_URL}/api/v1/adapter/${id}`,
+  modifyAggregatorConfig: (id: string) =>
+    `${NEXT_PUBLIC_API_BASE_URL}/api/v1/aggregator/${id}`,
+  modifyReporterConfig: (id: string) =>
+    `${NEXT_PUBLIC_API_BASE_URL}/api/v1/reporter/${id}`,
 };
 
 export type IApi = typeof api;
@@ -51,8 +66,18 @@ export interface IApiParam {
   queueStatus: { GET: any };
   getChainConfig: { GET: any; POST: any };
   getServiceConfig: { GET: any; POST: any };
+  getListenerConfig: { GET: any; POST: any };
+  getVrfKeysConfig: { GET: any; POST: any };
+  getAdapterConfig: { GET: any; POST: any };
+  getAggregatorConfig: { GET: any; POST: any };
+  getReporterConfig: { GET: any; POST: any };
   modifyChainConfig: { PATCH: any; DELETE: any };
   modifyServiceConfig: { PATCH: any; DELETE: any };
+  modifyListenerConfig: { PATCH: any; DELETE: any };
+  modifyVrfKeysConfig: { PATCH: any; DELETE: any };
+  modifyAdapterConfig: { PATCH: any; DELETE: any };
+  modifyAggregatorConfig: { PATCH: any; DELETE: any };
+  modifyReporterConfig: { PATCH: any; DELETE: any };
 }
 export interface IApiData {
   queues: { GET: any };
@@ -62,8 +87,18 @@ export interface IApiData {
   queueStatus: { GET: any };
   getChainConfig: { GET: any; POST: any };
   getServiceConfig: { GET: any; POST: any };
+  getListenerConfig: { GET: any; POST: any };
+  getVrfKeysConfig: { GET: any; POST: any };
+  getAdapterConfig: { GET: any; POST: any };
+  getAggregatorConfig: { GET: any; POST: any };
+  getReporterConfig: { GET: any; POST: any };
   modifyChainConfig: { PATCH: any; DELETE: any };
   modifyServiceConfig: { PATCH: any; DELETE: any };
+  modifyListenerConfig: { PATCH: any; DELETE: any };
+  modifyVrfKeysConfig: { PATCH: any; DELETE: any };
+  modifyAdapterConfig: { PATCH: any; DELETE: any };
+  modifyAggregatorConfig: { PATCH: any; DELETE: any };
+  modifyReporterConfig: { PATCH: any; DELETE: any };
 }
 
 export const fetchInternalApi = async <
