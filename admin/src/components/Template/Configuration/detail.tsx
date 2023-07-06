454e5bcc774113751199bbe5e463f07067889304
joey <joey@bisonai.com>
2023-07-06 09:52:32 +0900

    feat: Render different components based on configType

--- admin/src/components/Template/Configuration/detail.tsx
+++ admin/src/components/Template/Configuration/detail.tsx
@@ -1,7 +1,12 @@
 "use client";
 
+import Adapter from "./Adapter";
+import Aggregator from "./Aggregator";
 import Chain from "./Chain";
+import Listener from "./Listener";
+import Reporter from "./Reporter";
 import Service from "./Service";
+import VrfKeys from "./VrfKeys";
 
 export default function ConfigurationDetailTemplate({
   configType,
@@ -12,6 +17,11 @@ export default function ConfigurationDetailTemplate({
     <>
       {configType === "chain" && <Chain />}
       {configType === "service" && <Service />}
+      {configType === "listener" && <Listener />}
+      {configType === "vrf-keys" && <VrfKeys />}
+      {configType === "adapter" && <Adapter />}
+      {configType === "aggregator" && <Aggregator />}
+      {configType === "reporter" && <Reporter />}
     </>
   );
 }
