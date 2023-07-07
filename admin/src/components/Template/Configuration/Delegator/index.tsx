646c427f348c057b06d7fe8eb2ba4cd51e88a940
joey <joey@bisonai.com>
2023-07-07 14:57:18 +0900

    feat: Add Delegator component

--- admin/src/components/Template/Configuration/Delegator/index.tsx
+++ admin/src/components/Template/Configuration/Delegator/index.tsx
@@ -1,6 +1,10 @@
 import { fetchInternalApi } from "@/utils/api";
 import React from "react";
 import { useQuery } from "react-query";
+import TabContextProvider from "@/components/Common/TabContextProvider";
+import TabList from "@/components/Common/TabList";
+import TabPanel from "@/components/Common/TabPanel";
+import { IQueueData, StatusTab, statusTabs } from "@/utils/types";
 
 const Delegator = () => {
   const organizationQuery = useQuery({
@@ -46,6 +50,7 @@ const Delegator = () => {
     refetchOnWindowFocus: false,
     select: (data) => data.data,
   });
+
   console.log(
     organizationQuery.data,
     contractQuery.data,
@@ -54,9 +59,57 @@ const Delegator = () => {
     "data"
   );
 
+  const delegatorTabs: StatusTab[] = [
+    { tabId: "organization", label: "Organization" },
+    { tabId: "contract", label: "Contract" },
+    { tabId: "function", label: "Function" },
+    { tabId: "reporter", label: "Reporter" },
+  ];
+
   return (
     <div>
       <h2 style={{ color: "white" }}>Delegator</h2>
+      <TabContextProvider initTab={"organization"}>
+        <TabList tabs={delegatorTabs} />
+        {delegatorTabs.map(({ tabId, label }) => (
+          <TabPanel tabId={tabId} key={tabId}>
+            <div>
+              {(() => {
+                switch (tabId) {
+                  case "organization":
+                    return organizationQuery.isLoading ? (
+                      <div>Loading organization data...</div>
+                    ) : (
+                      <pre>
+                        {JSON.stringify(organizationQuery.data, null, 2)}
+                      </pre>
+                    );
+                  case "contract":
+                    return contractQuery.isLoading ? (
+                      <div>Loading contract data...</div>
+                    ) : (
+                      <pre>{JSON.stringify(contractQuery.data, null, 2)}</pre>
+                    );
+                  case "function":
+                    return functionQuery.isLoading ? (
+                      <div>Loading function data...</div>
+                    ) : (
+                      <pre>{JSON.stringify(functionQuery.data, null, 2)}</pre>
+                    );
+                  case "reporter":
+                    return reporterQuery.isLoading ? (
+                      <div>Loading reporter data...</div>
+                    ) : (
+                      <pre>{JSON.stringify(reporterQuery.data, null, 2)}</pre>
+                    );
+                  default:
+                    return null;
+                }
+              })()}
+            </div>
+          </TabPanel>
+        ))}
+      </TabContextProvider>
     </div>
   );
 };
