8ea6ed7715f3f7ca0bb0d9b4143384fe66606318
KelvinThai <quan.thai1987@gmail.com>
2023-07-11 08:33:51 +0700

    format code Registry, added new L2Endpoint contract

--- contracts/src/v0.1/L2endpoint.sol
+++ contracts/src/v0.1/L2endpoint.sol
@@ -1,6 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.16;
 import "@openzeppelin/contracts/access/Ownable.sol";
-contract Endpoint {
+import "./interfaces/IAggregator.sol";
 
+contract Endpoint is Ownable {
+    mapping(address => bool) submitters;
+
+    error InvalidSubmitter(address submitter);
+    event SubmitterAdded(address newSubmitter);
+    event SubmitterRemoved(address newSubmitter);
+
+    constructor(address _aggregator) {}
+
+    function addSubmitter(address _newSubmitter) external onlyOwner {
+        submitters[_newSubmitter] = true;
+        emit SubmitterAdded(_newSubmitter);
+    }
+
+    function removeSubmitter(address _submitter) external onlyOwner {
+        delete submitters[_submitter];
+        emit SubmitterRemoved(_submitter);
+    }
+
+    function submit(uint256 _roundId, int256 _submission) external {
+        if (!submitters[msg.sender]) revert InvalidSubmitter(msg.sender);
+        //update submission to aggregator
+    }
 }
--- contracts/src/v0.1/Registry.sol
+++ contracts/src/v0.1/Registry.sol
@@ -3,49 +3,60 @@ pragma solidity ^0.8.16;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Registry is Ownable {
-
     error NotEnoughFee();
     error InvalidChainID();
-    
-    event ChainProposed(address sender, L2Endpoint endPoint, uint chainID);
-    event ChainConfirmed (uint chainID);
+
+    event ChainProposed(address sender, uint chainID);
+    event ChainConfirmed(uint chainID);
     event ProposeFeeSet(uint fee);
 
-    uint public ProposeFee;
+    uint256 public proposeFee;
 
-	struct L2Endpoint {
-		string jsonRpc;
-		address endpoint;
-		address owner;
-        uint startRound; // for datafeeds
-	}
-	// chainId => L2 Endpoint
-	mapping(uint256 => L2Endpoint) public chainRegistry;
+    struct L2Endpoint {
+        string jsonRpc;
+        address endpoint;
+        address owner;
+        uint256 startRound; // for datafeeds
+    }
+    // chainId => L2 Endpoint
+    mapping(uint256 => L2Endpoint) public chainRegistry;
     // pending proposal
-    mapping (uint256 => L2Endpoint) pendingProposal;
+    mapping(uint256 => L2Endpoint) pendingProposal;
 
-  // L2 consumer => L1 payer
-  // Can be updated only by Orakl Network through call from L2
-  // mapping(address => address) accountRegistry;
+    // L2 consumer => L1 payer
+    // Can be updated only by Orakl Network through call from L2
+    // mapping(address => address) accountRegistry;
 
-	function proposeChain(uint chainID, L2Endpoint memory endpoint) external payable {
-        if(msg.value<ProposeFee){
+    function proposeChain(
+        uint _chainID,
+        string memory _jsonRpc,
+        address _endpoint,
+        uint256 _startRound
+    ) external payable {
+        if (msg.value < proposeFee) {
             revert NotEnoughFee();
         }
-        pendingProposal[chainID] = endpoint;
-        emit ChainProposed(msg.sender, endpoint, chainID);
-	}
+        pendingProposal[_chainID].jsonRpc = _jsonRpc;
+        pendingProposal[_chainID].endpoint = _endpoint;
+        pendingProposal[_chainID].owner = msg.sender;
+        pendingProposal[_chainID].startRound = _startRound;
 
-    function SetProposeFee(uint fee) public onlyOwner {
-        ProposeFee = fee;
-        emit ProposeFeeSet(fee);
+        emit ChainProposed(msg.sender, _chainID);
     }
 
-	function confirmChain(uint chainId) public onlyOwner {
-        if (pendingProposal[chainId].owner == address(0))  {
+    function setProposeFee(uint _fee) public onlyOwner {
+        proposeFee = _fee;
+        emit ProposeFeeSet(_fee);
+    }
+
+    function confirmChain(uint _chainId) public onlyOwner {
+        if (pendingProposal[_chainId].owner == address(0)) {
             revert InvalidChainID();
         }
-        chainRegistry[chainId] = pendingProposal[chainId];
-        emit ChainConfirmed(chainId);
+        chainRegistry[_chainId] = pendingProposal[_chainId];
+        delete pendingProposal[_chainId];
+        emit ChainConfirmed(_chainId);
     }
+
+    receive() external payable {}
 }
