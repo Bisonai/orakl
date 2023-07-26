50da0549334ee14bf85edd1fd9cc103d994447c8
Bayram Guvanjov <bayramguwanjow@gmail.com>
2023-07-26 14:33:48 +0900

    Increase orakl contract package version to include Registry, and L2Endpoint (#768)
    * feat: bump-up orakl-contracts version from 1.0.1 to 1.1.0
    
    * feat: add Registry deployment script
    
    * feat: add simple script to proposeChain to Registry

---
+++ contracts/deploy/Registry/Registry.cjs
@@ -0,0 +1,18 @@
+const func = async function (hre) {
+  const { deployments, getNamedAccounts, network } = hre
+  const { deploy } = deployments
+  const { deployer } = await getNamedAccounts()
+
+  const registryDeployment = await deploy('Registry', {
+    args: [],
+    from: deployer,
+    log: true
+  })
+
+  console.log('registeryDeployment:', registryDeployment)
+}
+
+func.id = 'deploy-registry'
+func.tags = ['registry']
+
+module.exports = func
