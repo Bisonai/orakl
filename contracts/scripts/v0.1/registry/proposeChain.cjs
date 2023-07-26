1e199dafd15d08ff1cb5fd738b10cb04c8c66086
bayram98 <bayramguwanjow@gmail.com>
2023-07-26 10:56:19 +0900

    feat: add simple script to proposeChain to Registry

---
+++ contracts/scripts/v0.1/registry/proposeChain.cjs
@@ -0,0 +1,41 @@
+const { ethers } = require('hardhat')
+const hre = require('hardhat')
+
+async function main() {
+  const { network } = hre
+  let _consumer
+
+  if (network.name == 'localhost') {
+    const { consumer } = await hre.getNamedAccounts()
+    _consumer = consumer
+  } else {
+    const PROVIDER = process.env.PROVIDER
+    const MNEMONIC = process.env.MNEMONIC || ''
+    const provider = new ethers.providers.JsonRpcProvider(PROVIDER)
+    _consumer = ethers.Wallet.fromMnemonic(MNEMONIC).connect(provider)
+  }
+
+  let registry = await ethers.getContract('Registry')
+  registry = await ethers.getContractAt('Registry', registry.address, _consumer)
+
+  const fee = ethers.utils.parseEther('1.0')
+  const pChainID = '100001'
+  const jsonRpc = 'https://123'
+  const endpoint = _consumer
+  const l1Aggregator = _consumer
+  const l2Aggregator = _consumer
+
+  console.log('Propose chain: ', pChainID, jsonRpc, endpoint, l1Aggregator, l2Aggregator)
+
+  const tx = await (
+    await registry.proposeChain(pChainID, jsonRpc, endpoint, l1Aggregator, l2Aggregator, {
+      value: fee
+    })
+  ).wait()
+  console.log('Tx', tx)
+}
+
+main().catch((error) => {
+  console.error(error)
+  process.exitCode = 1
+})
