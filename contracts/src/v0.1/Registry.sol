4b592d8a9bfb68021175df41a97353aefb493615
KelvinThai <quan.thai1987@gmail.com>
2023-08-14 13:13:50 +0700

    added account struct

--- contracts/src/v0.1/Registry.sol
+++ contracts/src/v0.1/Registry.sol
@@ -8,15 +8,18 @@ contract Registry is Ownable {
     error InsufficientBalance();
     error NotFeePayerOwner();
     error NotChainOwner();
+    error ChainExisted();
 
     event ChainProposed(address sender, uint chainID);
     event ChainConfirmed(uint256 chainID);
     event ProposeFeeSet(uint256 fee);
     event AggregatorAdded(uint256 chainID, uint256 aggregatorID);
     event AggregatorRemoved(uint256 chainID, uint256 aggregatorID);
-    event FeePayerAdded(uint256 chainID, address feePayer);
-    event FeePayerRemoved(uint256 chainID, address feePayer);
-    event feeConsumerAdded(uint256 chaiID, address feePayer, address feeConsumer);
+    event AccountCreated(uint256 accId, uint256 chainId, address owner);
+    event AccountUpdated(uint256 accId, uint256 chainId, address owner);
+    event AccountDeleted(uint256 accId);
+    event ConsumerAdded(uint256 accId, address consumerAddress);
+    event ConsumerRemoved(uint256 accId, address consumerAddress);
 
     uint256 public proposeFee;
     struct AggregatorPair {
@@ -27,9 +30,21 @@ contract Registry is Ownable {
 
     mapping(uint256 => AggregatorPair[]) public aggregators; // chain ID to aggregator pairs
     mapping(uint256 => uint256) public aggregatorCount; // count aggregator IDs
-    mapping(address => address) feePayerOwner;
-    mapping(uint256 => mapping(address => bool)) public feePayer;
-    mapping(uint256 => mapping(address => mapping(address => bool))) public consumer;
+    // mapping(uint256 => mapping(address => address)) public feePayer; // chainID -> owner -> feepayer
+
+    // mapping(uint256 => mapping(address => mapping(address => bool))) public consumer; // chainID -> owner -> payer -> consumer
+    // mapping(uint => address) accountConsmers;
+
+    mapping(uint256 => Account) private accounts;
+    uint256 private nextAccountId = 1;
+    struct Account {
+        uint256 accId;
+        uint256 chainId;
+        address owner;
+        address feePayer;
+        address[100] consumers;
+        uint8 consumerCount;
+    }
 
     struct L2Endpoint {
         uint256 _chainID;
@@ -42,11 +57,15 @@ contract Registry is Ownable {
     // pending proposal
     mapping(uint256 => L2Endpoint) pendingProposal;
 
-
     modifier onlyConfirmedChain(uint256 chainId) {
         if (chainRegistry[chainId].owner == address(0)) {
             revert InvalidChainID();
         }
+        _;
+    }
+    modifier onlyAccountOwner(uint256 _accId) {
+        require(_accId > 0 && _accId < nextAccountId, "Account does not exist");
+        require(accounts[_accId].owner == msg.sender, "Not the account owner");
         _;
     }
     modifier onlyConfirmedChainOwner(uint256 chainId) {
@@ -59,44 +78,68 @@ contract Registry is Ownable {
         _;
     }
 
-    function addFeeConsumer(
-        uint256 chainID,
-        address _feePayer,
-        address _feeConsumer
-    ) external onlyConfirmedChain(chainID) {
-        if (feePayerOwner[_feePayer] != msg.sender) {
-            revert NotFeePayerOwner();
+    function createAccount(uint256 _chainId, address _owner, address _feePayer) external {
+        if (chainRegistry[_chainId].owner == address(0)) {
+            revert InvalidChainID();
         }
-        consumer[chainID][_feePayer][_feeConsumer] = true;
-        emit feeConsumerAdded(chainID, _feePayer, _feeConsumer);
+        Account storage newAccount = accounts[nextAccountId];
+        newAccount.accId = nextAccountId;
+        newAccount.chainId = _chainId;
+        newAccount.owner = _owner;
+        newAccount.feePayer = _feePayer;
+
+        emit AccountCreated(nextAccountId, _chainId, _owner);
+        nextAccountId++;
     }
 
-    function removeFeeConsumer(
-        uint256 chainID,
-        address _feePayer,
-        address _feeConsumer
-    ) external onlyConfirmedChain(chainID) {
-        if (feePayerOwner[_feePayer] != msg.sender) {
-            revert NotFeePayerOwner();
-        }
-        delete consumer[chainID][_feePayer][_feeConsumer];
+    function deleteAccount(uint256 _accId) external onlyAccountOwner(_accId) {
+        delete accounts[_accId];
+        emit AccountDeleted(_accId);
     }
 
-    function addFeePayer(uint256 chainID, address _feePayer) external onlyConfirmedChain(chainID) {
-        feePayer[chainID][_feePayer] = true;
-        feePayerOwner[_feePayer] = msg.sender;
-        emit FeePayerAdded(chainID, _feePayer);
-    }
-
-    function removeFeePayer(
-        uint256 chainID,
+    function editAccount(
+        uint256 _accId,
+        uint256 _chainId,
+        address _owner,
         address _feePayer
-    ) external onlyConfirmedChain(chainID) {
-        if (feePayerOwner[_feePayer] != msg.sender) {
-            revert NotFeePayerOwner();
+    ) external onlyAccountOwner(_accId) {
+        Account storage account = accounts[_accId];
+        account.chainId = _chainId;
+        account.owner = _owner;
+        account.feePayer = _feePayer;
+        emit AccountUpdated(_accId, _chainId, _owner);
+    }
+
+    function addConsumer(
+        uint256 _accId,
+        address _consumerAddress
+    ) external onlyAccountOwner(_accId) {
+        Account storage account = accounts[_accId];
+        require(account.consumerCount < 100, "Max consumers reached");
+
+        account.consumers[account.consumerCount] = _consumerAddress;
+        account.consumerCount++;
+
+        emit ConsumerAdded(_accId, _consumerAddress);
+    }
+
+    function removeConsumer(
+        uint256 _accId,
+        address _consumerAddress
+    ) external onlyAccountOwner(_accId) {
+        require(_accId > 0 && _accId < nextAccountId, "Account does not exist");
+        Account storage account = accounts[_accId];
+
+        for (uint8 i = 0; i < account.consumerCount; i++) {
+            if (account.consumers[i] == _consumerAddress) {
+                account.consumerCount--;
+                account.consumers[i] = account.consumers[account.consumerCount];
+                account.consumers[account.consumerCount] = address(0);
+
+                emit ConsumerRemoved(_accId, _consumerAddress);
+                return;
+            }
         }
-        feePayer[chainID][_feePayer] = false;
-        emit FeePayerRemoved(chainID, _feePayer);
     }
 
     function addAggregator(
@@ -132,7 +175,7 @@ contract Registry is Ownable {
         }
     }
 
-    function proposeChain(
+    function proposeNewChain(
         uint256 _chainID,
         string memory _jsonRpc,
         address _endpoint
@@ -140,10 +183,30 @@ contract Registry is Ownable {
         if (msg.value < proposeFee) {
             revert NotEnoughFee();
         }
+        if (chainRegistry[_chainID].owner != address(0)) {
+            revert ChainExisted();
+        }
         pendingProposal[_chainID].jsonRpc = _jsonRpc;
         pendingProposal[_chainID].endpoint = _endpoint;
         pendingProposal[_chainID].owner = msg.sender;
         emit ChainProposed(msg.sender, _chainID);
+    }
+
+    function editChainInfo(
+        uint256 _chainID,
+        string memory _jsonRpc,
+        address _endpoint
+    ) external payable {
+        if (msg.value < proposeFee) {
+            revert NotEnoughFee();
+        }
+        if (chainRegistry[_chainID].owner != msg.sender) {
+            revert NotChainOwner();
+        }
+        chainRegistry[_chainID].jsonRpc = _jsonRpc;
+        chainRegistry[_chainID].endpoint = _endpoint;
+        chainRegistry[_chainID].owner = msg.sender;
+        emit ChainConfirmed(_chainID);
     }
 
     function setProposeFee(uint256 _fee) public onlyOwner {
