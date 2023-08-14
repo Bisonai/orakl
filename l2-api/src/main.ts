34de5d05b32c3fc630bb41f1a0ef9659dbc8f2d3
bayram98 <bayramguwanjow@gmail.com>
2023-08-14 14:18:00 +0900

    clean-up: remove logs

--- l2-api/src/main.ts
+++ l2-api/src/main.ts
@@ -27,8 +27,6 @@ async function bootstrap() {
 
   const configService = app.get(ConfigService)
   const port = configService.get('APP_PORT')
-  console.log('Config Service:', configService)
-  console.log('App port: ', port)
   await app.listen(port)
 }
 bootstrap()
