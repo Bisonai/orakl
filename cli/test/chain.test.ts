d9929fa44f3e8a7cc53556d03257010c311dae2b
bayram98 <bayramguwanjow@gmail.com>
2023-09-20 11:10:15 +0900

    test: add test for cli proxy

--- cli/test/chain.test.ts
+++ cli/test/chain.test.ts
@@ -2,26 +2,26 @@ import { describe, expect, test } from '@jest/globals'
 import { listHandler, insertHandler, removeHandler } from '../src/chain'
 
 describe('CLI Chain', function () {
-  test.skip('Should list chain', async function () {
+  test('Should list chain', async function () {
     const chain = await listHandler()()
     expect(chain.length).toBeGreaterThan(0)
   })
 
-  test.skip('Should insert new chain', async function () {
+  test('Should insert new chain', async function () {
     const chainBefore = await listHandler()()
     await insertHandler()({ name: 'ethereum' })
     const chainAfter = await listHandler()()
     expect(chainAfter.length).toEqual(chainBefore.length + 1)
   })
 
-  test.skip('Should not allow to insert the same chain more than once', async function () {
+  test('Should not allow to insert the same chain more than once', async function () {
     await insertHandler()({ name: 'ethereum' })
     await expect(async () => {
       await insertHandler()({ name: 'ethereum' })
     }).rejects.toThrow()
   })
 
-  test.skip('Should delete chain based on id', async function () {
+  test('Should delete chain based on id', async function () {
     const chainBefore = await listHandler()()
     await removeHandler()({ id: 1 })
     const chainAfter = await listHandler()()
