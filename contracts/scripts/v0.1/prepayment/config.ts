4ffd5fa4970f278b8d3058ca61847ac681bd0d97
bayram98 <bayramguwanjow@gmail.com>
2023-10-10 14:23:13 +0900

    feat: read Account owner address from .env variables

--- contracts/scripts/v0.1/prepayment/config.ts
+++ contracts/scripts/v0.1/prepayment/config.ts
@@ -3,18 +3,17 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 // Input configuration to create Prepayment Account
+const PROVIDER_URL = process.env.PROVIDER || 'http://127.0.0.1:8545'
+const MNEMONIC = process.env.MNEMONIC || ''
+const OWNER_ADDRESS = process.env.ACCOUNT_OWNER_ADDRESS || '' // Owner of the account which we are going to create
+
 const START_TIME = Math.round(new Date().getTime() / 1000) - 60 * 60 // Start time in seconds
 const PERIOD = 60 * 60 * 24 * 7 // Duration in seconds
 const REQUEST_NUMBER = 10 // Number of requests
-const OWNER_ADDRESS = '0x30E30C3B6313FF232E93593b883fC8A8AF8BB627' // Account Owner Address
-
 const FEE_RATIO = 10000 // 100%
 const SUBSCRIPTION_PRICE = ethers.utils.parseEther('1') // Subscription price in Klay
 
 const ACC_ID = 1
-
-const PROVIDER_URL = process.env.PROVIDER || 'http://127.0.0.1:8545'
-const MNEMONIC = process.env.MNEMONIC
 
 module.exports = {
   START_TIME,
