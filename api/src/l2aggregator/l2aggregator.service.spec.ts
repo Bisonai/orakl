abec4dfe8b75c5486d554a97046ee7d8238b5541
KelvinThai <quan.thai1987@gmail.com>
2023-10-30 16:20:28 +0700

    fixed api test

--- api/src/l2aggregator/l2aggregator.service.spec.ts
+++ api/src/l2aggregator/l2aggregator.service.spec.ts
@@ -1,18 +1,19 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { L2aggregatorService } from './l2aggregator.service';
+import { Test, TestingModule } from '@nestjs/testing'
+import { L2aggregatorService } from './l2aggregator.service'
+import { PrismaService } from '../prisma.service'
 
 describe('L2aggregatorService', () => {
-  let service: L2aggregatorService;
+  let service: L2aggregatorService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [L2aggregatorService],
-    }).compile();
+      providers: [L2aggregatorService, PrismaService]
+    }).compile()
 
-    service = module.get<L2aggregatorService>(L2aggregatorService);
-  });
+    service = module.get<L2aggregatorService>(L2aggregatorService)
+  })
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+    expect(service).toBeDefined()
+  })
+})
