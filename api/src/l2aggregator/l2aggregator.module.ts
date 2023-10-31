ac0e6f0dfaf50fad9e2009de68e5ce3dd336b077
KelvinThai <quan.thai1987@gmail.com>
2023-10-31 14:48:22 +0700

    fixed e2e api test

--- api/src/l2aggregator/l2aggregator.module.ts
+++ api/src/l2aggregator/l2aggregator.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common'
 import { L2aggregatorController } from './l2aggregator.controller'
 import { L2aggregatorService } from './l2aggregator.service'
 import { ChainService } from '../chain/chain.service'
-import { PrismaService } from 'src/prisma.service'
+import { PrismaService } from '../prisma.service'
 
 @Module({
   controllers: [L2aggregatorController],
