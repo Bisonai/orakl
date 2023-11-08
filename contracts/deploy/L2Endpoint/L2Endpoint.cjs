3a0cec105f2222f39935c93366bb2fad6549273a
Kelvin <54656638+KelvinThai@users.noreply.github.com>
2023-11-08 10:49:42 +0700

    changed script deploy L2Endpoint (#892)
    * changed script deploy L2Endpoint
    
    * removed name field

--- contracts/deploy/L2Endpoint/L2Endpoint.cjs
+++ contracts/deploy/L2Endpoint/L2Endpoint.cjs
@@ -1,15 +1,28 @@
+const path = require('node:path')
+const { loadJson, loadMigration, updateMigration } = require('../../scripts/v0.1/utils.cjs')
+
 const func = async function (hre) {
   const { deployments, getNamedAccounts, network } = hre
   const { deploy } = deployments
   const { deployer } = await getNamedAccounts()
+  const migrationDirPath = `./migration/${network.name}/L2Endpoint`
+  const migrationFilesNames = await loadMigration(migrationDirPath)
+  for (const migration of migrationFilesNames) {
+    const config = await loadJson(path.join(migrationDirPath, migration))
+    // Deploy L2Endpoint ////////////////////////////////////////////////////////
+    if (config.deploy) {
+      console.log('deploy')
+      const l2EndpointDeployment = await deploy('L2Endpoint', {
+        args: [],
+        from: deployer,
+        log: true
+      })
 
-  const registryDeployment = await deploy('L2Endpoint', {
-    args: [],
-    from: deployer,
-    log: true
-  })
+      console.log('l2EndpointDeployment:', l2EndpointDeployment)
+    }
 
-  console.log('l2EndpointDeployment:', registryDeployment)
+    await updateMigration(migrationDirPath, migration)
+  }
 }
 
 func.id = 'deploy-l2Endpoint'
