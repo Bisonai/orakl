bb0be3b95c770f5b0596d3bbeee55f3e9d1d40fe
KelvinThai <quan.thai1987@gmail.com>
2023-11-08 18:00:34 +0700

    Created requestRandomWords function for VRF on L2 Endpoint

--- contracts/src/v0.1/L2endpoint.sol
+++ contracts/src/v0.1/L2endpoint.sol
@@ -6,6 +6,7 @@ import "./interfaces/IL2Aggregator.sol";
 contract L2Endpoint is Ownable {
     uint256 public aggregatorCount;
     uint256 public submitterCount;
+    uint64 sNonce;
 
     mapping(address => bool) submitters;
     mapping(address => bool) aggregators;
@@ -18,6 +19,16 @@ contract L2Endpoint is Ownable {
     event AggregatorAdded(address newAggregator);
     event AggregatorRemoved(address newAggregator);
     event Submitted(uint256 roundId, int256 submission);
+    event RandomWordsRequested(
+        bytes32 indexed keyHash,
+        uint256 requestId,
+        uint256 preSeed,
+        uint64 indexed accId,
+        uint32 callbackGasLimit,
+        uint32 numWords,
+        address indexed sender,
+        bool isDirectPayment
+    );
 
     function addAggregator(address _newAggregator) external onlyOwner {
         if (aggregators[_newAggregator]) revert InvalidAggregator(_newAggregator);
@@ -52,5 +63,57 @@ contract L2Endpoint is Ownable {
         if (!aggregators[_aggregator]) revert InvalidAggregator(_aggregator);
         IL2Aggregator(_aggregator).submit(_roundId, _submission);
         emit Submitted(_roundId, _submission);
+    }
+
+    function computeRequestId(
+        bytes32 keyHash,
+        address sender,
+        uint64 accId,
+        uint64 nonce
+    ) private pure returns (uint256, uint256) {
+        uint256 preSeed = uint256(keccak256(abi.encode(keyHash, sender, accId, nonce)));
+        uint256 requestId = uint256(keccak256(abi.encode(keyHash, preSeed)));
+        return (requestId, preSeed);
+    }
+
+    function requestRandomWords(
+        bytes32 keyHash,
+        uint64 accId,
+        uint32 callbackGasLimit,
+        uint32 numWords,
+        bool isDirectPayment
+    ) private returns (uint256) {
+        sNonce++;
+        (uint256 requestId, uint256 preSeed) = computeRequestId(keyHash, msg.sender, accId, sNonce);
+        emit RandomWordsRequested(
+            keyHash,
+            requestId,
+            preSeed,
+            accId,
+            callbackGasLimit,
+            numWords,
+            msg.sender,
+            isDirectPayment
+        );
+
+        return requestId;
+    }
+
+    function requestRandomWords(
+        bytes32 keyHash,
+        uint64 accId,
+        uint32 callbackGasLimit,
+        uint32 numWords
+    ) external returns (uint256) {
+        bool isDirectPayment = false;
+        uint256 requestId = requestRandomWords(
+            keyHash,
+            accId,
+            callbackGasLimit,
+            numWords,
+            isDirectPayment
+        );
+
+        return requestId;
     }
 }
