ffc1bf8a1fd531e192f28a51d7ba08eb87ace57a
bayram98 <bayramguwanjow@gmail.com>
2023-11-14 16:55:52 +0900

    feat: make reporter for PoR

--- core/src/por/main.ts
+++ core/src/por/main.ts
@@ -1,10 +1,16 @@
+import { buildLogger } from '../logger'
+import { hookConsoleError } from '../utils'
 import { fetchWithAggregator } from './fetcher'
+import { reportData } from './reporter'
 
 const aggregatorHash = '0x952f883b8d2fd47a790307cb569118a215ea45eb861cefd4ed3b83ae7550f8e8'
+const LOGGER = buildLogger()
 
 const main = async () => {
-  const value = await fetchWithAggregator(aggregatorHash)
-  console.log('Value:', value)
+  hookConsoleError(LOGGER)
+
+  const { value, oracleAddress } = await fetchWithAggregator(aggregatorHash)
+  await reportData({ value, oracleAddress, logger: LOGGER })
 }
 
 main().catch((error) => {
--- core/src/por/reporter.ts
+++ core/src/por/reporter.ts
@@ -1,23 +1,78 @@
-// /**
-//  * Fetch single reporter given its ID from the Orakl Network API.
-//  *
-//  * @param {string} reporter ID
-//  * @param {pino.Logger} logger
-//  * @return {IReporterConfig} reporter configuration
-//  * @exception {GetReporterRequestFailed}
-//  */
-// export async function getReporter({
-//   id,
-//   logger
-// }: {
-//   id: string
-//   logger?: Logger
-// }): Promise<IReporterConfig> {
-//   //   try {
-//   //     const endpoint = buildUrl(ORAKL_NETWORK_API_URL, `reporter/${id}`)
-//   //     return (await axios.get(endpoint))?.data
-//   //   } catch (e) {
-//   //     logger?.error({ name: 'getReporter', file: FILE_NAME, ...e }, 'error')
-//   //     throw new OraklError(OraklErrorCode.GetReporterRequestFailed)
-//   //   }
-// }
+import { Aggregator__factory } from '@bisonai/orakl-contracts'
+import { ethers } from 'ethers'
+import { Logger } from 'pino'
+import { getReporterByOracleAddress } from '../api'
+import { buildWallet, sendTransaction } from '../reporter/utils'
+import {
+  CHAIN,
+  DATA_FEED_FULFILL_GAS_MINIMUM,
+  DATA_FEED_SERVICE_NAME,
+  PROVIDER,
+  PROVIDER_URL
+} from '../settings'
+import { IReporterConfig } from '../types'
+import { buildTransaction } from '../worker/data-feed.utils'
+
+async function checkSubmissionFrequency() {
+  // TODO
+  return true
+}
+
+async function checkDeviationThreashold() {
+  // TODO
+  return true
+}
+
+async function report({
+  value,
+  oracleAddress,
+  logger
+}: {
+  value: bigint
+  oracleAddress: string
+  logger: Logger
+}) {
+  const reporter: IReporterConfig = await getReporterByOracleAddress({
+    service: DATA_FEED_SERVICE_NAME,
+    chain: CHAIN,
+    oracleAddress,
+    logger: logger
+  })
+
+  console.log('Reporter:', reporter)
+  const iface = new ethers.utils.Interface(Aggregator__factory.abi)
+  const aggregator = new ethers.Contract(oracleAddress, Aggregator__factory.abi, PROVIDER)
+  let queriedRoundId = 0
+  const state = await aggregator.oracleRoundState(reporter.address, queriedRoundId)
+  const roundId = state._roundId
+  const getLastestRoundData = await aggregator.getRoundData(roundId - 1)
+
+  const tx = buildTransaction({
+    payloadParameters: {
+      roundId,
+      submission: value
+    },
+    to: oracleAddress,
+    gasMinimum: DATA_FEED_FULFILL_GAS_MINIMUM,
+    iface,
+    logger
+  })
+
+  const wallet = await buildWallet({ privateKey: reporter.privateKey, providerUrl: PROVIDER_URL })
+  const txParams = { wallet, ...tx, logger }
+  await sendTransaction(txParams)
+}
+
+export async function reportData({
+  value,
+  oracleAddress,
+  logger
+}: {
+  value: bigint
+  oracleAddress: string
+  logger: Logger
+}) {
+  if ((await checkSubmissionFrequency()) || (await checkDeviationThreashold())) {
+    await report({ value, oracleAddress, logger })
+  }
+}
