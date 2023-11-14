17d4bc5bd5c4239032b8f6daa2c176a38a5890a7
Nick <148735107+nick-bisonai@users.noreply.github.com>
2023-11-14 11:43:56 +0900

    Use delegated fee only from `cypress` and `baobab` (#916)
    * fix: use delegated fee only from cypress and baobab
    
    * refactor: format document
    
    * fix: update based on feedbacks
    
    * fix: format document

--- core/src/reporter/data-feed.ts
+++ core/src/reporter/data-feed.ts
@@ -1,20 +1,25 @@
 import { Logger } from 'pino'
 import type { RedisClientType } from 'redis'
 import {
+  BAOBAB_CHAIN_ID,
+  CYPRESS_CHAIN_ID,
   DATA_FEED_REPORTER_STATE_NAME,
   DATA_FEED_SERVICE_NAME,
+  PROVIDER,
   REPORTER_AGGREGATOR_QUEUE_NAME
 } from '../settings'
 import { factory } from './factory'
 
 export async function buildReporter(redisClient: RedisClientType, logger: Logger) {
+  const chainId = (await PROVIDER.getNetwork()).chainId
+
   await factory({
     redisClient,
     stateName: DATA_FEED_REPORTER_STATE_NAME,
     service: DATA_FEED_SERVICE_NAME,
     reporterQueueName: REPORTER_AGGREGATOR_QUEUE_NAME,
     concurrency: 10,
-    delegatedFee: true,
+    delegatedFee: [BAOBAB_CHAIN_ID, CYPRESS_CHAIN_ID].includes(chainId) ? true : false,
     _logger: logger
   })
 }
