0a61c3ec12ee2bb1d49c1c3b702244cb8c359072
Martin Kersner <martin@bisonai.com>
2023-11-15 16:06:12 +0900

    fix: Convert env var to `Number`

--- fetcher/src/settings.ts
+++ fetcher/src/settings.ts
@@ -3,7 +3,7 @@ dotenv.config()
 
 export const FETCHER_QUEUE_NAME = 'orakl-fetcher-queue'
 
-export const WORKER_OPTS = { concurrency: process.env.CONCURRENCY || 20 }
+export const WORKER_OPTS = { concurrency: Number(process.env.CONCURRENCY) || 20 }
 
 export const FETCH_FREQUENCY = 2_000
 
