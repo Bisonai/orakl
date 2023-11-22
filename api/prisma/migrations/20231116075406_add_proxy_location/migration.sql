f8fa086bbbfa8a1c35d6d61cabdf25471815dc2d
Nick <148735107+nick-bisonai@users.noreply.github.com>
2023-11-22 17:40:29 +0900

    fix: update migration file (#966)

--- api/prisma/migrations/20231116075406_add_proxy_location/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `location` to the `proxies` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "proxies" ADD COLUMN     "location" TEXT NOT NULL;
