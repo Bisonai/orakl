50130593d98f73bff046efa4e04d9010bc62b487
Nick <148735107+nick-bisonai@users.noreply.github.com>
2023-11-22 18:14:24 +0900

    fix: minor linter fix (#965)

--- fetcher/src/tools/run-adapter.ts
+++ fetcher/src/tools/run-adapter.ts
@@ -1,22 +1,22 @@
-import { fetchData, aggregateData } from '../job/job.utils';
-import { IAdapter } from '../job/job.types';
+import { fetchData, aggregateData } from '../job/job.utils'
+import { IAdapter } from '../job/job.types'
 
 // runs job through local adapter json file
 // example: npx ts-node ./src/tools/run-adapter.ts ./ada-usdt.adapter.json
 
 const main = async () => {
-    const adapterJsonPath = process.argv.slice(2)[0];
+  const adapterJsonPath = process.argv.slice(2)[0]
 
-    const adapter: IAdapter = require(adapterJsonPath);
-    const adapterDefintions = adapter.feeds.map((feed)=> {
-      return feed.definition;
-    })
-   
-    const data = await fetchData(adapterDefintions, console);
-    console.log(data);
+  const adapter: IAdapter = await import(adapterJsonPath)
+  const adapterDefintions = adapter.feeds.map((feed) => {
+    return feed.definition
+  })
 
-    const aggregate = aggregateData(data);
-    console.log(aggregate);
+  const data = await fetchData(adapterDefintions, console)
+  console.log(data)
+
+  const aggregate = aggregateData(data)
+  console.log(aggregate)
 }
 
 main().catch((error) => {
