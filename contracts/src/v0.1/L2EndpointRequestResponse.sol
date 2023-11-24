b1cd9ce1c9736493a4dc8c6cfce745282d016c1f
KelvinThai <quan.thai1987@gmail.com>
2023-11-24 14:24:21 +0700

    Added L2 RR request and fulfill functions + testcase

---
+++ contracts/src/v0.1/L2EndpointRequestResponse.sol
@@ -0,0 +1,150 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+import "./L2EndpointBase.sol";
+import "./libraries/Orakl.sol";
+import "./RequestResponseConsumerFulfill.sol";
+
+abstract contract L2EndpointRequestResponse is L2EndpointBase {
+    using Orakl for Orakl.Request;
+    uint64 private sNonce;
+
+    event DataRequested(
+        uint256 indexed requestId,
+        bytes32 jobId,
+        uint64 indexed accId,
+        uint32 callbackGasLimit,
+        address indexed sender,
+        uint8 numSubmission,
+        bytes data
+    );
+
+    event DataRequestFulfilledUint128(uint256 indexed requestId, uint256 response, bool success);
+    event DataRequestFulfilledInt256(uint256 indexed requestId, int256 response, bool success);
+    event DataRequestFulfilledBool(uint256 indexed requestId, bool response, bool success);
+    event DataRequestFulfilledString(uint256 indexed requestId, string response, bool success);
+    event DataRequestFulfilledBytes32(uint256 indexed requestId, bytes32 response, bool success);
+    event DataRequestFulfilledBytes(uint256 indexed requestId, bytes response, bool success);
+
+    function computeRequestId(
+        address sender,
+        uint64 accId,
+        uint64 nonce
+    ) private pure returns (uint256) {
+        return uint256(keccak256(abi.encode(sender, accId, nonce)));
+    }
+
+    function requestData(
+        Orakl.Request memory req,
+        uint32 callbackGasLimit,
+        uint64 accId,
+        uint8 numSubmission
+    ) external nonReentrant returns (uint256) {
+        sNonce++;
+        uint256 requestId = computeRequestId(msg.sender, accId, sNonce);
+        sRequestDetail[requestId] = RequestInfo({
+            owner: msg.sender,
+            callbackGasLimit: callbackGasLimit
+        });
+        emit DataRequested(
+            requestId,
+            req.id,
+            accId,
+            callbackGasLimit,
+            msg.sender,
+            numSubmission,
+            req.buf.buf
+        );
+
+        return requestId;
+    }
+
+    function fulfillDataRequestUint128(uint256 requestId, uint128 response) external nonReentrant {
+        if (!sSubmitters[msg.sender]) revert InvalidSubmitter(msg.sender);
+        RequestInfo memory detail = sRequestDetail[requestId];
+        bytes memory resp = abi.encodeWithSelector(
+            RequestResponseConsumerFulfillUint128.rawFulfillDataRequest.selector,
+            requestId,
+            response
+        );
+        setReentrancy(true);
+        bool success = callWithExactGas(detail.callbackGasLimit, detail.owner, resp);
+        setReentrancy(false);
+        emit DataRequestFulfilledUint128(requestId, response, success);
+    }
+
+    function fulfillDataRequestInt256(uint256 requestId, int256 response) external nonReentrant {
+        if (!sSubmitters[msg.sender]) revert InvalidSubmitter(msg.sender);
+        RequestInfo memory detail = sRequestDetail[requestId];
+        bytes memory resp = abi.encodeWithSelector(
+            RequestResponseConsumerFulfillInt256.rawFulfillDataRequest.selector,
+            requestId,
+            response
+        );
+        setReentrancy(true);
+        bool success = callWithExactGas(detail.callbackGasLimit, detail.owner, resp);
+        setReentrancy(false);
+        emit DataRequestFulfilledInt256(requestId, response, success);
+    }
+
+    function fulfillDataRequestBool(uint256 requestId, bool response) external nonReentrant {
+        if (!sSubmitters[msg.sender]) revert InvalidSubmitter(msg.sender);
+        RequestInfo memory detail = sRequestDetail[requestId];
+        bytes memory resp = abi.encodeWithSelector(
+            RequestResponseConsumerFulfillBool.rawFulfillDataRequest.selector,
+            requestId,
+            response
+        );
+        setReentrancy(true);
+        bool success = callWithExactGas(detail.callbackGasLimit, detail.owner, resp);
+        setReentrancy(false);
+        emit DataRequestFulfilledBool(requestId, response, success);
+    }
+
+    function fulfillDataRequestString(
+        uint256 requestId,
+        string memory response
+    ) external nonReentrant {
+        if (!sSubmitters[msg.sender]) revert InvalidSubmitter(msg.sender);
+        RequestInfo memory detail = sRequestDetail[requestId];
+        bytes memory resp = abi.encodeWithSelector(
+            RequestResponseConsumerFulfillString.rawFulfillDataRequest.selector,
+            requestId,
+            response
+        );
+        setReentrancy(true);
+        bool success = callWithExactGas(detail.callbackGasLimit, detail.owner, resp);
+        setReentrancy(false);
+        emit DataRequestFulfilledString(requestId, response, success);
+    }
+
+    function fulfillDataRequestBytes32(uint256 requestId, bytes32 response) external nonReentrant {
+        if (!sSubmitters[msg.sender]) revert InvalidSubmitter(msg.sender);
+        RequestInfo memory detail = sRequestDetail[requestId];
+        bytes memory resp = abi.encodeWithSelector(
+            RequestResponseConsumerFulfillBytes32.rawFulfillDataRequest.selector,
+            requestId,
+            response
+        );
+        setReentrancy(true);
+        bool success = callWithExactGas(detail.callbackGasLimit, detail.owner, resp);
+        setReentrancy(false);
+        emit DataRequestFulfilledBytes32(requestId, response, success);
+    }
+
+    function fulfillDataRequestBytes(
+        uint256 requestId,
+        bytes memory response
+    ) external nonReentrant {
+        if (!sSubmitters[msg.sender]) revert InvalidSubmitter(msg.sender);
+        RequestInfo memory detail = sRequestDetail[requestId];
+        bytes memory resp = abi.encodeWithSelector(
+            RequestResponseConsumerFulfillBytes.rawFulfillDataRequest.selector,
+            requestId,
+            response
+        );
+        setReentrancy(true);
+        bool success = callWithExactGas(detail.callbackGasLimit, detail.owner, resp);
+        setReentrancy(false);
+        emit DataRequestFulfilledBytes(requestId, response, success);
+    }
+}
---
+++ contracts/test/v0.1/non-vrf/L2RequestResponseConsumer.test.cjs
@@ -0,0 +1,118 @@
+const { expect } = require('chai')
+const { ethers } = require('hardhat')
+const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers')
+
+const { createSigners } = require('../utils.cjs')
+const { requestResponseConfig } = require('./RequestResponse.config.cjs')
+
+async function deploy() {
+  const {
+    account0: deployerSigner,
+    account1: consumerSigner,
+    account2,
+    account3,
+    account4,
+    account5
+  } = await createSigners()
+
+  // L2 endpoint
+  let l2EndpointContract = await ethers.getContractFactory('L2Endpoint', { signer: deployerSigner })
+  l2EndpointContract = await l2EndpointContract.deploy()
+  await l2EndpointContract.deployed()
+
+  const endpoint = {
+    contract: l2EndpointContract,
+    signer: deployerSigner
+  }
+
+  // L2 consumer
+  let l2RRConsumerMock = await ethers.getContractFactory('L2RequestResponseConsumerMock', {
+    signer: deployerSigner
+  })
+  l2RRConsumerMock = await l2RRConsumerMock.deploy(l2EndpointContract.address)
+  await l2RRConsumerMock.deployed()
+
+  const consumer = {
+    contract: l2RRConsumerMock,
+    signer: deployerSigner
+  }
+
+  return {
+    endpoint,
+    consumer,
+    account2,
+    account3,
+    account4,
+    account5
+  }
+}
+
+async function requestAndFulfill(requestFn, fulfillFn, dataResponseFn, dataResponse) {
+  const { consumer, endpoint } = await loadFixture(deploy)
+  const { maxGasLimit: callbackGasLimit } = requestResponseConfig()
+  const accMock = 1
+  const numSubmission = 1
+  const txRequestData = await (
+    await consumer.contract[requestFn](accMock, callbackGasLimit, numSubmission)
+  ).wait()
+
+  const event = endpoint.contract.interface.parseLog(txRequestData.events[0])
+  expect(event.name).to.be.equal('DataRequested')
+  const { requestId } = event.args
+
+  await expect(endpoint.contract[fulfillFn](requestId, dataResponse)).revertedWithCustomError(
+    endpoint.contract,
+    'InvalidSubmitter'
+  )
+
+  await (await endpoint.contract.addSubmitter(endpoint.signer.address)).wait()
+  await (await endpoint.contract[fulfillFn](requestId, dataResponse)).wait()
+  const result = await consumer.contract[dataResponseFn]()
+  expect(result).to.be.equal(dataResponse)
+}
+
+describe('L2 Request-Response', function () {
+  it('Request & Fulfill Uint128', async function () {
+    await requestAndFulfill(
+      'requestDataUint128',
+      'fulfillDataRequestUint128',
+      'sResponseUint128',
+      1
+    )
+  })
+
+  it('Request & Fulfill Int256', async function () {
+    await requestAndFulfill('requestDataInt256', 'fulfillDataRequestInt256', 'sResponseInt256', 1)
+  })
+
+  it('Request & Fulfill Bool', async function () {
+    await requestAndFulfill('requestDataBool', 'fulfillDataRequestBool', 'sResponseBool', true)
+  })
+
+  it('Request & Fulfill String', async function () {
+    await requestAndFulfill(
+      'requestDataString',
+      'fulfillDataRequestString',
+      'sResponseString',
+      'hello'
+    )
+  })
+
+  it('Request & Fulfill Bytes32', async function () {
+    await requestAndFulfill(
+      'requestDataBytes32',
+      'fulfillDataRequestBytes32',
+      'sResponseBytes32',
+      ethers.utils.formatBytes32String('hello')
+    )
+  })
+
+  it('Request & Fulfill Bytes', async function () {
+    await requestAndFulfill(
+      'requestDataBytes',
+      'fulfillDataRequestBytes',
+      'sResponseBytes',
+      ethers.utils.formatBytes32String('hello')
+    )
+  })
+})
