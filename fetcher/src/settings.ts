3a0638119922b2c932a6c4c3abfe093921495b22
bayram98 <bayramguwanjow@gmail.com>
2023-11-27 14:28:06 +0900

    feat: export PROVIDER variable

--- fetcher/src/settings.ts
+++ fetcher/src/settings.ts
@@ -1,4 +1,5 @@
 import * as dotenv from 'dotenv'
+import { ethers } from 'ethers'
 dotenv.config()
 
 export const FETCHER_QUEUE_NAME = 'orakl-fetcher-queue'
@@ -10,3 +11,11 @@ export const FETCH_FREQUENCY = 2_000
 export const DEVIATION_QUEUE_NAME = 'orakl-deviation-queue'
 
 export const FETCHER_TYPE = process.env.FETCHER_TYPE || 0
+
+export const PROVIDER_URL = process.env.PROVIDER_URL || 'http://127.0.0.1:8545'
+
+function createJsonRpcProvider(providerUrl: string = PROVIDER_URL) {
+  return new ethers.JsonRpcProvider(providerUrl)
+}
+
+export const PROVIDER = createJsonRpcProvider()
