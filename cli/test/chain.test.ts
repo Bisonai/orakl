38f8ecbf6cb33261b6bb5c994dfe9e2833f537e9
Nick <nick@bisonai.com>
2023-12-06 19:36:06 +0900

    test: add github action yaml
    test: add test codes
    
    test: fix typo
    
    test: update yaml for test purpose
    
    fix: fix typo
    
    fix: remove test.txt
    
    fix: use single db with different schemas
    
    fix: fix port
    
    test
    
    fix: update test yaml
    
    test
    
    test: remove sign test
    
    fix: lint test codes
    
    fix: add api app port
    
    fix: add cli build process into yaml
    
    test
    
    fix: update test
    
    remove delegator test
    
    test
    
    remove delegator test
    
    test
    
    test
    
    update yaml for easier test
    
    test
    
    test
    
    test: seperate migration steps
    
    fix: remove multiple db script
    
    wip
    
    nohoist setup
    
    fix typo
    
    rollback nohoist
    
    fix: no hoist required for separated prisma client

--- cli/test/chain.test.ts
+++ cli/test/chain.test.ts
@@ -2,28 +2,41 @@ import { describe, expect, test } from '@jest/globals'
 import { insertHandler, listHandler, removeHandler } from '../src/chain'
 
 describe('CLI Chain', function () {
-  test.skip('Should list chain', async function () {
+  
+  let initalChainId
+  beforeAll(async () => {
+    const insertResult = await insertHandler()({ name: 'boabab' })
+    initalChainId = insertResult.id
+  })
+
+  afterAll(async () => {
+    const chains = await listHandler()()
+    for (const chain of chains) {
+      await removeHandler()({ id: chain.id })
+    }
+  })
+
+  test('Should list chain', async function () {
     const chain = await listHandler()()
     expect(chain.length).toBeGreaterThan(0)
   })
 
-  test.skip('Should insert new chain', async function () {
+  test('Should insert new chain', async function () {
     const chainBefore = await listHandler()()
     await insertHandler()({ name: 'ethereum' })
     const chainAfter = await listHandler()()
     expect(chainAfter.length).toEqual(chainBefore.length + 1)
   })
 
-  test.skip('Should not allow to insert the same chain more than once', async function () {
+  test('Should not allow to insert the same chain more than once', async function () {
     await insertHandler()({ name: 'ethereum' })
-    await expect(async () => {
-      await insertHandler()({ name: 'ethereum' })
-    }).rejects.toThrow()
+    const msg = await insertHandler()({ name: 'ethereum' })
+    expect(msg).toEqual('Internal server error')
   })
 
-  test.skip('Should delete chain based on id', async function () {
+  test('Should delete chain based on id', async function () {
     const chainBefore = await listHandler()()
-    await removeHandler()({ id: 1 })
+    await removeHandler()({ id: initalChainId })
     const chainAfter = await listHandler()()
     expect(chainAfter.length).toEqual(chainBefore.length - 1)
   })
