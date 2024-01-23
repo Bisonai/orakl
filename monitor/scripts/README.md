3af2510f77951e43c749aba681bfafe9494b65d7
Martin Kersner <martin@bisonai.com>
2024-01-23 13:53:15 +0900

    feat: Finalize ECR+Helm Chart Check

---
+++ monitor/scripts/README.md
@@ -0,0 +1,35 @@
+# Monitoring scripts
+
+## ECR + Helm Chart Check
+
+```
+./ecr-check.sh
+```
+
+```
+orakl-core: count FAIL (7 tags)
+(cypress) orakl-core: vrf FAIL (v0.3.1.20231103.0653.19ca105)
+(cypress) orakl-core: request-response FAIL (v0.3.1.20231016.1000.749dac8)
+(cypress) orakl-core: aggregator OK (v0.5.1.20240122.0131.44014e7)
+(baobab) orakl-core: vrf OK (v0.5.1.20240111.1440.dacb2ef)
+(baobab) orakl-core: vrf OK (v0.5.1.20240111.1440.dacb2ef)
+(baobab) orakl-core: vrf OK (v0.5.1.20240111.1440.dacb2ef)
+(baobab) orakl-core: request-response OK (v0.5.1.20240111.1440.dacb2ef)
+(baobab) orakl-core: request-response OK (v0.5.1.20240122.0240.1672788)
+(baobab) orakl-core: request-response OK (v0.5.1.20240111.1440.dacb2ef)
+(baobab) orakl-core: aggregator OK (v0.5.1.20231221.0205.5ff1278)
+(baobab) orakl-core: aggregator OK (v0.5.1.20240122.0240.1672788)
+(baobab) orakl-core: aggregator OK (v0.5.1.20240122.0131.44014e7)
+orakl-api: count OK
+(cypress) orakl-api: api OK (v0.1.0.20231214.0553.3dd6e26)
+(baobab) orakl-api: api OK (v0.1.0.20231214.0451.cb4bd3a)
+orakl-cli: count OK
+(cypress) orakl-cli: cli FAIL (v0.6.0.20230920.0750.99c4cdd)
+(baobab) orakl-cli: cli OK (v0.6.0.20231229.0312.9433d53)
+orakl-fetcher: count FAIL (6 tags)
+(cypress) orakl-fetcher: fetcher OK (v0.0.1.20231221.0248.5ff1278)
+(baobab) orakl-fetcher: fetcher OK (v0.0.1.20240122.0240.1672788)
+orakl-delegator: count OK
+(cypress) orakl-delegator: delegator FAIL (v0.0.1.20230707.0137.513e9f9)
+(baobab) orakl-delegator: delegator OK (v0.0.1.20231211.0735.867d885)
+```
--- monitor/scripts/ecr-check.sh
+++ monitor/scripts/ecr-check.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
 # Dependencies: curl, jq, yq
 
 # Check the number of tags for a repository
 check_repository_tags() {
     repository_name="$1"
-    service_name="$2"
+    max_tag_count="$2"
+    service_names="${@:3}"
 
     # Make the API call
     response=$(curl -s -X POST \
@@ -17,51 +18,77 @@ check_repository_tags() {
     available_tags=$(echo "$response" | jq '.imageTagDetails |= sort_by(.createdAt)')
     tag_count=$(echo "$available_tags" | jq '.imageTagDetails[].imageTag' | wc -l | tr -d '[:space:]')
 
-    # # Check if the tag count exceeds the maximum allowed
-    if [ "${tag_count}" -gt "${orakl_max_tag_count}" ]; then
-        echo -e "$repository_name: count FAIL (${tag_count} tags)"
+    # Check if the tag count exceeds the maximum allowed
+    if [ "${tag_count}" -gt "${max_tag_count}" ]; then
+        echo "$repository_name: count FAIL (${tag_count} tags)"
     else
-        echo -e "$repository_name: count OK"
+        echo "$repository_name: count OK"
     fi
 
-    # cypress
-    check_used_tags "$available_tags" "cypress"
-    check_used_tags "$available_tags" "baobab"
+    check_used_tags "cypress"
+    check_used_tags "baobab"
 }
 
 check_used_tags() {
-    available_tags="$1"
-    chain="$2"
+    chain="$1"
 
-    helm_chart=$(curl -s https://raw.githubusercontent.com/Bisonai/orakl-helm-charts/gcp-${chain}-prod/${service_name}/values.yaml)
-    tag=$(echo ${helm_chart} | yq eval '.global.image.tag')
-    # listener_tag=$(echo ${helm_chart} | yq eval '.global.image.listenerTag')
-    # worker_tag=$(echo ${helm_chart} | yq eval '.global.image.workerTag')
-    # reporter_tag=$(echo ${helm_chart} | yq eval '.global.image.reporterTag')
+    for service_name in ${service_names}; do
+        helm_chart=$(curl -s --raw https://raw.githubusercontent.com/Bisonai/orakl-helm-charts/gcp-${chain}-prod/${service_name}/values.yaml)
+        tag=$(echo "${helm_chart}" | yq eval '.global.image.tag')
 
-    cypress_found_tag=false
+        # Sometimes we split tags into listener, worker, and reporter.
+        if [ "$tag" = "null" ]; then
+            listener_tag=$(echo "${helm_chart}" | yq eval '.global.image.listenerTag')
+            worker_tag=$(echo "${helm_chart}" | yq eval '.global.image.workerTag')
+            reporter_tag=$(echo "${helm_chart}" | yq eval '.global.image.reporterTag')
+
+            check_tag ${listener_tag}
+            check_tag ${worker_tag}
+            check_tag ${reporter_tag}
+
+            continue
+        fi
+
+        # Sometimes, there is just single tag.
+        found_tag=false
+        for i in $(seq 0 $((tag_count - 1))); do
+            cur_image_tag=$(echo "$available_tags" | jq -r ".imageTagDetails[$i].imageTag")
+            if [ "$cur_image_tag" = "$tag" ]; then
+                found_tag=true
+            fi
+        done
+
+        if [ "$found_tag" = false ]; then
+            echo "(${chain}) ${repository_name}: ${service_name} FAIL ($tag)"
+        else
+            echo "(${chain}) ${repository_name}: ${service_name} OK ($tag)"
+        fi
+    done
+}
+
+check_tag() {
+    tag="$1"
+
+    found_tag=false
     for i in $(seq 0 $((tag_count - 1))); do
         cur_image_tag=$(echo "$available_tags" | jq -r ".imageTagDetails[$i].imageTag")
         if [ "$cur_image_tag" = "$tag" ]; then
-            cypress_found_tag=true
+            found_tag=true
         fi
     done
 
-    if [ "$cypress_found_tag" = false ]; then
-        echo "${repository_name}: ${chain} image FAIL (not found)"
+    if [ "$found_tag" = false ]; then
+        echo "(${chain}) ${repository_name}: ${service_name} FAIL ($tag)"
     else
-        echo "${repository_name}: ${chain} $tag"
+        echo "(${chain}) ${repository_name}: ${service_name} OK ($tag)"
     fi
 }
 
-# Set the maximum tag count
-orakl_max_tag_count=5
-
 # orakl-general has quite mixed set of tags, so we skip it
 # check_repository_tags "orakl-general"
-check_repository_tags "orakl-core" "core"
-check_repository_tags "orakl-api" "api"
-check_repository_tags "orakl-cli" "cli"
-check_repository_tags "orakl-fetcher" "fetcher"
-check_repository_tags "orakl-delegator" "delegator"
+check_repository_tags "orakl-core" 5 "vrf" "request-response" "aggregator"
+check_repository_tags "orakl-api" 5 "api"
+check_repository_tags "orakl-cli" 5 "cli"
+check_repository_tags "orakl-fetcher" 5 "fetcher"
+check_repository_tags "orakl-delegator" 5 "delegator"
 # check_repository_tags "orakl-goapi"
