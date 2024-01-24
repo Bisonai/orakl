9e3cb08177ba808ae2d773da262ee9fd33d63189
nick <nick@bisonai.com>
2024-01-24 14:26:47 +0900

    feat: update main

--- fetcher-v2/main.go
+++ fetcher-v2/main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"context"
+	"log"
+
+	"bisonai.com/fetcher/v2/utils"
+)
+
+func setup(ctx context.Context) {
+	host, err := utils.MakeHost()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	discoverString := "orakl-rendezvous"
+	//discover 1 peer for gossip protocol networking
+	utils.DiscoverPeers(ctx, host, discoverString)
+}
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	setup(ctx)
+}
