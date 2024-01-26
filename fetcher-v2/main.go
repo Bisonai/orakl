f4cc440bb68fc448d693561b030c8dcc3f4c2c3d
nick <nick@bisonai.com>
2024-01-26 13:50:30 +0900

    fix: remove comments and unused codes

--- fetcher-v2/main.go
+++ fetcher-v2/main.go
@@ -13,38 +13,6 @@ import (
 	"bisonai.com/fetcher/v2/utils"
 )
 
-// func setup(appPort string, wg *sync.WaitGroup) {
-// 	h, err := utils.MakeHost()
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	//
-// 	nodes := make(map[string]*utils.Node)
-// 	// will be tracked to track connected peers
-// 	discoveredPeers := make(map[peer.ID]peer.AddrInfo)
-// 	discoverString := "orakl-rendezvous"
-// 	appContext := admin.AppContext{
-// 		DiscoverString: discoverString,
-// 		Host:           &h,
-// 		Nodes:          nodes,
-// 		Peers:          discoveredPeers,
-// 	}
-
-// 	utils.DiscoverPeers(context.Background(), h, discoverString)
-
-// 	//discover 1 peer for gossip protocol networking
-// 	wg.Add(1)
-// 	// go func() {
-// 	// 	defer wg.Done()
-// 	// 	utils.DiscoverPeers(context.Background(), h, discoverString)
-// 	// }()
-// 	go func() {
-// 		defer wg.Done()
-// 		admin.Run(appPort, appContext)
-// 	}()
-// }
-
 func main() {
 	discoverString := "orakl-test-discover-2024"
 	port := flag.Int("p", 0, "app port")
@@ -52,9 +20,7 @@ func main() {
 	if *port == 0 {
 		log.Fatal("app port is required")
 	}
-	// var wg sync.WaitGroup
-	// setup(strconv.Itoa(*port), &wg)
-	// wg.Wait()
+
 	h, err := utils.MakeHost(*port + 7000)
 	if err != nil {
 		log.Fatal(err)
