b6c40e9023dfbb9e991c93b105e34f796ac6e7bd
KelvinThai <quan.thai1987@gmail.com>
2024-01-26 21:18:47 +0700

    added more test case for aggregator on foundry

--- foundryContract/test/Aggregator.t.sol
+++ foundryContract/test/Aggregator.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test, console2} from "forge-std/Test.sol";
+import {Test, console2, console} from "forge-std/Test.sol";
 import {Aggregator} from "../src/Aggregator.sol";
 
 contract AggregatorTest is Test {
@@ -15,7 +15,6 @@ contract AggregatorTest is Test {
     address[] oracleAdd;
 
     uint256 timestamp = 1706170779;
-
     event AnswerUpdated(
         int256 indexed current,
         uint256 indexed roundId,
@@ -118,5 +117,121 @@ contract AggregatorTest is Test {
 
         (, int256 answer, , , ) = aggregator.latestRoundData();
         assertEq(answer, 11);
+    }
+
+    function test_RevertWith_TooManyOracles() public {
+        uint256 maxOracle = aggregator.MAX_ORACLE_COUNT();
+        for (uint32 i = 1; i < maxOracle; i++) {
+            address add = address(bytes20(keccak256(abi.encodePacked(i))));
+            oracleAdd.push(add);
+            aggregator.changeOracles(oracleRemove, oracleAdd, i, i, 0);
+            oracleAdd.pop();
+        }
+        vm.expectRevert(Aggregator.TooManyOracles.selector);
+        oracleAdd.push(address(0));
+        aggregator.changeOracles(
+            oracleRemove,
+            oracleAdd,
+            uint32(maxOracle),
+            uint32(maxOracle),
+            0
+        );
+    }
+
+    function test_RevertWith_MinSubmissionGtMaxSubmission() public {
+        vm.expectRevert(Aggregator.MinSubmissionGtMaxSubmission.selector);
+        aggregator.changeOracles(oracleRemove, oracleAdd, 1, 0, 0);
+    }
+
+    function test_RevertWith_MaxSubmissionGtOracleNum() public {
+        vm.expectRevert(Aggregator.MaxSubmissionGtOracleNum.selector);
+        aggregator.changeOracles(oracleRemove, oracleAdd, 0, 1, 0);
+    }
+
+    function test_RevertWith_RestartDelayExceedOracleNum() public {
+        uint32 minSubmissionCount = 0;
+        uint32 maxSubmissionCount = 1;
+        uint32 restartDelay = 1;
+        vm.expectRevert(Aggregator.RestartDelayExceedOracleNum.selector);
+        oracleAdd.push(address(0));
+        aggregator.changeOracles(
+            oracleRemove,
+            oracleAdd,
+            minSubmissionCount,
+            maxSubmissionCount,
+            restartDelay
+        );
+    }
+
+    function test_RevertWith_MinSubmissionZero() public {
+        uint32 minSubmissionCount = 0;
+        uint32 maxSubmissionCount = 1;
+        uint32 restartDelay = 0;
+        vm.expectRevert(Aggregator.MinSubmissionZero.selector);
+        oracleAdd.push(address(0));
+        aggregator.changeOracles(
+            oracleRemove,
+            oracleAdd,
+            minSubmissionCount,
+            maxSubmissionCount,
+            restartDelay
+        );
+    }
+
+    function test_RevertWith_PrevRoundNotSupersedable() public {
+        bool authorized = true;
+        uint32 deplay = 0;
+        aggregator.setRequesterPermissions(address(0), authorized, deplay);
+        oracleAdd.push(address(1));
+        oracleAdd.push(address(2));
+        oracleAdd.push(address(3));
+        aggregator.changeOracles(oracleRemove, oracleAdd, 2, 3, 0);
+        vm.prank(address(1));
+        aggregator.submit(1, 321);
+        vm.expectRevert(Aggregator.PrevRoundNotSupersedable.selector);
+        vm.prank(address(0));
+        aggregator.requestNewRound();
+    }
+
+    function test_currentRoundStartedAt() public {
+        oracleAdd.push(address(0));
+        changeOracles();
+        for (uint i = 1; i <= 2; i++) {
+            vm.warp(timestamp + i);
+            vm.prank(address(0));
+            aggregator.submit(i, 321);
+            uint256 startedAt = aggregator.currentRoundStartedAt();
+            assertEq(startedAt, timestamp + i);
+        }
+    }
+
+    function test_validateOracleRound() public {
+        vm.expectRevert("not enabled oracle");
+        uint256 roundId = 1;
+        aggregator.submit(roundId, 321);
+        oracleAdd.push(address(0));
+        changeOracles();
+
+        roundId = 2;
+        vm.expectRevert("invalid round to report");
+        vm.prank(address(0));
+        aggregator.submit(roundId, 321);
+
+        roundId = 1;
+        vm.prank(address(0));
+        aggregator.submit(roundId, 321);
+        vm.expectRevert("cannot report on previous rounds");
+        vm.prank(address(0));
+        aggregator.submit(roundId, 321);
+
+        roundId = 2;
+        oracleAdd.pop();
+        oracleAdd.push(address(1));
+        aggregator.changeOracles(oracleRemove, oracleAdd, 2, 2, 0);
+        vm.prank(address(0));
+        aggregator.submit(roundId, 321);
+        vm.expectRevert("previous round not supersedable");
+        vm.prank(address(0));
+        aggregator.submit(roundId + 1, 321);
     }
 }
