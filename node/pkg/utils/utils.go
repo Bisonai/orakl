8d6a6c3811c45d361aacf9395796751dcfa0a465
nick <nick@bisonai.com>
2024-02-14 20:17:39 +0900

    fix: remove timestamp based id generator code

--- node/pkg/utils/utils.go
+++ node/pkg/utils/utils.go
@@ -1,26 +1,12 @@
 package utils
 
 import (
-	"fmt"
 	"math/rand"
 	"sort"
-	"time"
 )
 
 func RandomNumberGenerator() int {
 	return rand.Intn(20) + 1
-}
-
-func GetIDFromTimestamp(rangeSize int64, t time.Time) string {
-	seconds := t.Unix()
-	rangeID := seconds / rangeSize
-	str := fmt.Sprintf("%d", rangeID)
-
-	if len(str) > 6 {
-		str = str[len(str)-6:]
-	}
-
-	return str
 }
 
 func FindMedian(nums []int) int {
