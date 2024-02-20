b878a224a8a302df671cda32d38d114a139e7358
nick <nick@bisonai.com>
2024-02-20 11:52:36 +0900

    feat: add raw reducer code, fetching code

--- node/pkg/fetcher/fetcher.go
+++ node/pkg/fetcher/fetcher.go
@@ -4,15 +4,29 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"math"
 
 	"bisonai.com/orakl/node/pkg/bus"
 	"bisonai.com/orakl/node/pkg/db"
+	"bisonai.com/orakl/node/pkg/utils"
 )
 
 const (
-	GetAdapters         = `GET * FROM adapters;`
-	GetFeedsByAdapterId = `GET * FROM feeds WHERE adapter_id = @id;`
+	loadActiveAdaptersQuery   = `SELECT * FROM adapters WHERE active = true`
+	loadFeedsByAdapterIdQuery = `SELECT * FROM feeds WHERE adapter_id = @adapterId`
 )
+
+type Definition struct {
+	Url      string            `json:"url"`
+	Headers  map[string]string `json:"headers"`
+	Method   string            `json:"method"`
+	Reducers []Reducer         `json:"reducers"`
+}
+
+type Reducer struct {
+	Function string      `json:"function"`
+	Args     interface{} `json:"args"`
+}
 
 type Adapter struct {
 	ID     int64  `db:"id"`
@@ -54,7 +68,112 @@ func (f *Fetcher) Stop() error {
 }
 
 func (f *Fetcher) StartAdapter(adapterName string) error {
+	for _, adapter := range f.Adapters {
+		f.fetch(adapter)
+	}
 	return nil
+}
+
+func (f *Fetcher) fetch(adapter AdapterDetail) ([]float64, error) {
+	adapterFeeds := adapter.Feeds
+
+	data := []float64{}
+
+	for _, feed := range adapterFeeds {
+		definition := new(Definition)
+		err := json.Unmarshal(feed.Definition, &definition)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		res, err := utils.GetRequest[interface{}](definition.Url, nil, definition.Headers)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		result, err := f.reduceAll(res, definition.Reducers)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		data = append(data, result)
+	}
+	if len(data) < 1 {
+		return nil, fmt.Errorf("no data fetched")
+	}
+	return data, nil
+}
+
+func (f *Fetcher) reduceAll(raw interface{}, reducers []Reducer) (float64, error) {
+	var result float64
+	for _, reducer := range reducers {
+		var err error
+		raw, err = f.reduce(raw, reducer)
+		if err != nil {
+			return 0, err
+		}
+	}
+	result, ok := raw.(float64)
+	if !ok {
+		return 0, fmt.Errorf("cannot cast raw data to float")
+	}
+	return result, nil
+}
+
+func (f *Fetcher) reduce(raw interface{}, reducer Reducer) (interface{}, error) {
+	switch reducer.Function {
+	case "INDEX":
+		castedRaw, ok := raw.([]interface{})
+		if !ok {
+			return nil, fmt.Errorf("cannot cast raw data to []interface{}")
+		}
+		return castedRaw[reducer.Args.(int)], nil
+	case "PARSE", "PATH":
+		castedRaw, ok := raw.(map[string]interface{})
+		if !ok {
+			return nil, fmt.Errorf("cannot cast raw data to map[string]interface{}")
+		}
+		args := reducer.Args.([]string)
+		for _, arg := range args {
+			castedRaw = castedRaw[arg].(map[string]interface{})
+		}
+		return castedRaw, nil
+	case "MUL":
+		castedRaw, ok := raw.(float64)
+		if !ok {
+			return nil, fmt.Errorf("cannot cast raw data to float")
+		}
+		return castedRaw * reducer.Args.(float64), nil
+	case "POW10":
+		castedRaw, ok := raw.(float64)
+		if !ok {
+			return nil, fmt.Errorf("cannot cast raw data to float")
+		}
+		return float64(math.Pow10(reducer.Args.(int))) * castedRaw, nil
+	case "ROUND":
+		castedRaw, ok := raw.(float64)
+		if !ok {
+			return nil, fmt.Errorf("cannot cast raw data to float")
+		}
+		return math.Round(castedRaw), nil
+	case "DIV":
+		castedRaw, ok := raw.(float64)
+		if !ok {
+			return nil, fmt.Errorf("cannot cast raw data to float")
+		}
+		return castedRaw / reducer.Args.(float64), nil
+	case "DIVFROM":
+		castedRaw, ok := raw.(float64)
+		if !ok {
+			return nil, fmt.Errorf("cannot cast raw data to float")
+		}
+		return reducer.Args.(float64) / castedRaw, nil
+	default:
+		return nil, fmt.Errorf("unknown reducer function: %s", reducer.Function)
+	}
+
 }
 
 func (f *Fetcher) StopAdapter(adapterName string) error {
@@ -74,7 +193,7 @@ func (f *Fetcher) loadActiveAdapters() error {
 }
 
 func (f *Fetcher) getAdapters(ctx context.Context) ([]Adapter, error) {
-	adapters, err := db.QueryRows[Adapter](ctx, GetAdapters, nil)
+	adapters, err := db.QueryRows[Adapter](ctx, loadActiveAdaptersQuery, nil)
 	if err != nil {
 		return nil, err
 	}
@@ -82,7 +201,7 @@ func (f *Fetcher) getAdapters(ctx context.Context) ([]Adapter, error) {
 }
 
 func (f *Fetcher) getFeeds(ctx context.Context, adapterId int64) ([]Feed, error) {
-	feeds, err := db.QueryRows[Feed](ctx, GetFeedsByAdapterId, map[string]any{"id": adapterId})
+	feeds, err := db.QueryRows[Feed](ctx, loadFeedsByAdapterIdQuery, map[string]any{"id": adapterId})
 	if err != nil {
 		return nil, err
 	}
--- node/pkg/fetcher/queries.go
+++
@@ -1,6 +0,0 @@
-package fetcher
-
-const (
-	loadActiveAdaptersQuery   = `SELECT * FROM adapters WHERE active = true`
-	loadFeedsByAdapterIdQuery = `SELECT * FROM feeds WHERE adapter_id = @adapterId`
-)
