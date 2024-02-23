daae1e5b745a1a9a7cf491b45fd5a7dbe3d8f0ba
nick <nick@bisonai.com>
2024-02-23 17:31:24 +0900

    fix: add comment for const type for bus

--- node/pkg/bus/types.go
+++ node/pkg/bus/types.go
@@ -6,13 +6,12 @@ const (
 	// 2. fetcher: fetches data from different sources and stores in db, sends message to aggregator in case of deviation
 	// 3. aggregator: aggregates data through data sent from other nodes and stores global aggregates into db
 	// 4. submitter: submits price into blockchain in regular basis
-
 	ADMIN      = "admin"
 	FETCHER    = "fetcher"
 	AGGREGATOR = "aggregator"
 	SUBMITTER  = "submitter"
 
-	// Modular Monolith pkg commands
+	// Modular Monolith pkg commands, please follow {verb}_{noun} pattern for both variable name and value
 	START_FETCHER   = "start_fetcher"
 	STOP_FETCHER    = "stop_fetcher"
 	REFRESH_FETCHER = "refresh_fetcher"
