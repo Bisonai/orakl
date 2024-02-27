37ae0e413af990263b9e697e240c2af13bd017da
Nick <148735107+nick-bisonai@users.noreply.github.com>
2024-02-27 14:17:57 +0900

    (OraklNode) Utils proxy request and test codes (#1174)
    * feat: proxy request functionality and testcodes
    
    * fix: no lint testing
    
    * fix: add missing test task
    
    * fix: replace fmt into zerologs
    
    * fix: add missing error handling
    
    * fix: update based on feedback

--- node/pkg/utils/request.go
+++ node/pkg/utils/request.go
@@ -13,14 +13,46 @@ import (
 )
 
 func GetRequest[T any](urlEndpoint string, requestBody interface{}, headers map[string]string) (T, error) {
-	return UrlRequest[T](urlEndpoint, "GET", requestBody, headers)
+	return UrlRequest[T](urlEndpoint, "GET", requestBody, headers, "")
 }
 
 func GetRequestRaw(urlEndpoint string, requestBody interface{}, headers map[string]string) (*http.Response, error) {
-	return UrlRequestRaw(urlEndpoint, "GET", requestBody, headers)
+	return UrlRequestRaw(urlEndpoint, "GET", requestBody, headers, "")
 }
 
-func UrlRequestRaw(urlEndpoint string, method string, requestBody interface{}, headers map[string]string) (*http.Response, error) {
+func UrlRequest[T any](urlEndpoint string, method string, requestBody interface{}, headers map[string]string, proxy string) (T, error) {
+	var result T
+	response, err := UrlRequestRaw(urlEndpoint, method, requestBody, headers, proxy)
+	if err != nil {
+		log.Error().Err(err).Msg("failed to make request")
+		return result, err
+	}
+
+	if response.StatusCode != http.StatusOK {
+		log.Info().
+			Int("status", response.StatusCode).
+			Str("url", urlEndpoint).
+			Msg("failed to make request")
+		return result, errors.New("failed to make request")
+	}
+
+	resultBody, err := io.ReadAll(response.Body)
+	if err != nil {
+		log.Error().Err(err).Msg("failed to read response body")
+
+		return result, err
+	}
+
+	err = json.Unmarshal(resultBody, &result)
+	if err != nil {
+		log.Error().Err(err).Str("resultBody", string(resultBody)).Msg("failed to unmarshal response body")
+		return result, err
+	}
+
+	return result, nil
+}
+
+func UrlRequestRaw(urlEndpoint string, method string, requestBody interface{}, headers map[string]string, proxy string) (*http.Response, error) {
 	var body io.Reader
 
 	if requestBody != nil {
@@ -34,7 +66,7 @@ func UrlRequestRaw(urlEndpoint string, method string, requestBody interface{}, h
 
 	url, err := url.Parse(urlEndpoint)
 	if err != nil {
-		log.Error().Err(err).Msg("failed to parse url")
+		log.Error().Err(err).Str("url", urlEndpoint).Msg("failed to parse url")
 		return nil, err
 	}
 
@@ -59,40 +91,21 @@ func UrlRequestRaw(urlEndpoint string, method string, requestBody interface{}, h
 		Timeout: time.Second, // Set the timeout to 1 second
 	}
 
+	if proxy != "" {
+		proxyUrl, err := url.Parse(proxy)
+		if err != nil {
+			log.Error().Err(err).Str("proxy", proxy).Msg("failed to parse proxy")
+			return nil, err
+		}
+
+		client.Transport = &http.Transport{
+			Proxy: http.ProxyURL(proxyUrl),
+		}
+	}
+
 	return client.Do(req)
 }
 
-func UrlRequest[T any](urlEndpoint string, method string, requestBody interface{}, headers map[string]string) (T, error) {
-	var result T
-	response, err := UrlRequestRaw(urlEndpoint, method, requestBody, headers)
-	if err != nil {
-		log.Error().Err(err).Msg("failed to make request")
-		return result, err
-	}
-
-	if response.StatusCode != http.StatusOK {
-		log.Info().
-			Int("status", response.StatusCode).
-			Str("url", urlEndpoint).
-			Msg("failed to make request")
-		return result, errors.New("failed to make request")
-	}
-
-	resultBody, err := io.ReadAll(response.Body)
-	if err != nil {
-		log.Error().Err(err).Msg("failed to read response body")
-		return result, err
-	}
-
-	err = json.Unmarshal(resultBody, &result)
-	if err != nil {
-		log.Info().
-			Err(err).
-			Str("response", string(resultBody)).
-			Msg("failed to unmarshal response body")
-
-		return result, err
-	}
-
-	return result, nil
+func GetRequestProxy[T any](urlEndpoint string, requestBody interface{}, headers map[string]string, proxy string) (T, error) {
+	return UrlRequest[T](urlEndpoint, "GET", requestBody, headers, proxy)
 }
