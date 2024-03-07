34f5e153448ba05f7791f34847bc459a7bedf633
Nick <148735107+nick-bisonai@users.noreply.github.com>
2024-03-07 18:00:59 +0900

    feat: script to measure gossipsub connection time (#1205)

---
+++ node/cmd/test_connection/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"time"
+
+	"bisonai.com/orakl/node/pkg/libp2p"
+	"github.com/rs/zerolog/log"
+)
+
+func main() {
+	ctx := context.Background()
+	topicString := "orakl-test-discover-connection-time"
+	port := flag.Int("p", 0, "libp2p port")
+	bootnode := flag.String("b", "", "bootnode")
+	flag.Parse()
+	if *port == 0 {
+		log.Fatal().Msg("Please provide a port to bind on with -p")
+	}
+
+	startTime := time.Now()
+	_, ps, err := libp2p.Setup(ctx, *bootnode, *port)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to setup libp2p")
+	}
+
+	topic, err := ps.Join(topicString)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to join topic")
+	}
+
+	_, err = topic.Subscribe()
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to subscribe")
+	}
+
+	for {
+		if len(ps.ListPeers(topicString)) > 0 {
+			log.Debug().Str("connection time", time.Since(startTime).String()).Msg("Connected to peers")
+			break
+		}
+	}
+
+}
