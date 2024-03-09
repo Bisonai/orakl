934897994eab723e2a6ea8c699e1b2e0b9f80b83
Martin Kersner <martin@bisonai.com>
2024-03-09 19:35:02 +0900

    feat: refactor submission proxy test

--- contracts/v0.2/test/SubmissionProxy.t.sol
+++ contracts/v0.2/test/SubmissionProxy.t.sol
@@ -6,7 +6,7 @@ import {SubmissionProxy} from "../src/SubmissionProxy.sol";
 import {Aggregator} from "../src/Aggregator.sol";
 
 contract SubmissionProxyTest is Test {
-    SubmissionProxy batchSubmission;
+    SubmissionProxy submissionProxy;
     uint32 timeout = 10;
     address validator = address(0);
     uint8 decimals = 18;
@@ -19,46 +19,56 @@ contract SubmissionProxyTest is Test {
 
     function setUp() public {
         vm.warp(timestamp);
-        batchSubmission = new SubmissionProxy();
+        submissionProxy = new SubmissionProxy();
     }
 
     function test_AddAndRemoveOracle() public {
-        batchSubmission.addOracle(address(0));
-        uint256 oracleLength = batchSubmission.getOracles().length;
+        submissionProxy.addOracle(address(0));
+        uint256 oracleLength = submissionProxy.getOracles().length;
         assertEq(oracleLength, 1);
 
-        batchSubmission.removeOracle(address(0));
-        oracleLength = batchSubmission.getOracles().length;
+        submissionProxy.removeOracle(address(0));
+        oracleLength = submissionProxy.getOracles().length;
         assertEq(oracleLength, 0);
     }
 
     function test_BatchSubmission() public {
-        batchSubmission.addOracle(address(0));
+	address offChainSubmissionProxyReporter = address(0);
+	address offChainAggregatorReporter = address(1);
+
+        submissionProxy.addOracle(offChainSubmissionProxyReporter);
+
         address[] memory oracleRemove;
         address[] memory oracleAdd = new address[](2);
-        uint256 submitGas;
+        uint256 singleSubmitGas;
         uint256 batchSubmitGas;
-        address[] memory aggregatorForBathSubmit = new address[](50);
+        address[] memory aggregators = new address[](50);
         int256[] memory submissions = new int256[](50);
         uint256 startGas;
 
+	// multiple single submissions
         for (uint256 i = 0; i < 50; i++) {
             Aggregator aggregator = new Aggregator(timeout, decimals, description);
-            oracleAdd[0] = address(batchSubmission);
-            oracleAdd[1] = address(0);
+
+            oracleAdd[0] = address(submissionProxy);
+            oracleAdd[1] = offChainAggregatorReporter;
             aggregator.changeOracles(oracleRemove, oracleAdd, 1, 1, 0);
-            aggregatorForBathSubmit[i] = address(aggregator);
+
+            aggregators[i] = address(aggregator);
             submissions[i] = 10;
+
             startGas = gasleft();
-            vm.prank(address(0));
+            vm.prank(offChainAggregatorReporter);
             aggregator.submit(10);
-            submitGas += estimateGasCost(startGas);
+            singleSubmitGas += estimateGasCost(startGas);
         }
+
+	// single batch submission
         startGas = gasleft();
-        vm.prank(address(0));
-        batchSubmission.submit(aggregatorForBathSubmit, submissions);
+        vm.prank(offChainSubmissionProxyReporter);
+        submissionProxy.submit(aggregators, submissions);
         batchSubmitGas = estimateGasCost(startGas);
 
-        console.log("submit", submitGas, "batch submit", batchSubmitGas);
+        console.log("single submit", singleSubmitGas, "batch submit", batchSubmitGas);
     }
 }
