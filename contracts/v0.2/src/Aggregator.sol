67817c9bb23eeebfd4b9c728f8e0503fa8a60cee
Martin Kersner <martin@bisonai.com>
2024-03-11 14:42:24 +0900

    fix: add caret to version

--- contracts/v0.2/src/Aggregator.sol
+++ contracts/v0.2/src/Aggregator.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.24;
+pragma solidity ^0.8.24;
 
 // https://github.com/smartcontractkit/chainlink/blob/develop/contracts/src/v0.6/FluxAggregator.sol
 
--- contracts/v0.2/src/SubmissionProxy.sol
+++ contracts/v0.2/src/SubmissionProxy.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.24;
+pragma solidity ^0.8.24;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {IAggregator} from "./interfaces/IAggregatorSubmit.sol";
