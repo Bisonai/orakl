41ccd2db9cb6971ebe7036bee1dacca679e1cbf5
Martin Kersner <martin@bisonai.com>
2024-03-20 15:36:15 +0900

    feat: FeedProxy comments

--- contracts/v0.2/src/FeedProxy.sol
+++ contracts/v0.2/src/FeedProxy.sol
@@ -6,74 +6,113 @@ import {IFeedProxy} from "./interfaces/IFeedProxy.sol";
 import {IFeed} from "./interfaces/IFeed.sol";
 
 /**
- * @title A trusted proxy for updating where current answers are read from
- * @notice This contract provides a consistent address for the
- * CurrentAnswerInterface but delegates where it reads from to the owner, who is
- * trusted to update it.
+ * @title Proxy feed contract
+ * @author Bisonai Labs
+ * @notice A contract that acts as a proxy for a `Feed` contract. It
+ * allows the owner to propose and confirm a new feed.
+ * @dev The current and proposed contracts are stored in the `feed`
+ * and `proposedFeed` variables.
  */
 contract FeedProxy is Ownable, IFeedProxy {
     IFeed public feed;
     IFeed private proposedFeed;
 
-    error InvalidProposedFeed();
-
     event FeedProposed(address indexed current, address indexed proposed);
-    event FeedConfirmed(address indexed previous, address indexed latest);
+    event FeedConfirmed(address indexed previous, address indexed current);
+
+    error InvalidProposedFeed();
 
     modifier hasProposal() {
         require(address(proposedFeed) != address(0), "No proposed feed present");
         _;
     }
 
+    /**
+     * @notice Construct a new `FeedProxy` contract.
+     * @dev The deployer of the contract will become the owner.
+     * @param _feed The address of the initial feed
+     */
     constructor(address _feed) Ownable(msg.sender) {
         setFeed(_feed);
     }
 
-    function getRoundData(uint64 roundId)
-        public
-        view
-        virtual
-        override
-        returns (uint64 id, int256 answer, uint256 updatedAt)
-    {
-        return feed.getRoundData(roundId);
+    /**
+     * @notice Return the round data given a round ID.
+     * @param _roundId The round ID.
+     * @return id The round ID.
+     * @return answer The oracle answer.
+     * @return updatedAt Timestamp of the last update.
+     */
+    function getRoundData(uint64 _roundId) external view returns (uint64 id, int256 answer, uint256 updatedAt) {
+        return feed.getRoundData(_roundId);
     }
 
-    function latestRoundData() public view virtual override returns (uint64 id, int256 answer, uint256 updatedAt) {
+    /**
+     * @notice Return the timestamp of the latest round update.
+     * @return The timestamp of the latest round update
+     */
+    function latestRoundUpdatedAt() external view returns (uint256) {
+        return feed.latestRoundUpdatedAt();
+    }
+
+    /**
+     * @notice Return the latest round data of the feed.
+     * @return id The round ID.
+     * @return answer The oracle answer.
+     * @return updatedAt Timestamp of the last update.
+     */
+    function latestRoundData() external view returns (uint64 id, int256 answer, uint256 updatedAt) {
         return feed.latestRoundData();
     }
 
-    function proposedGetRoundData(uint64 roundId)
+    /**
+     * @inheritdoc IFeedProxy
+     */
+    function proposedGetRoundData(uint64 _roundId)
         external
         view
-        virtual
-        override
         hasProposal
         returns (uint64 id, int256 answer, uint256 updatedAt)
     {
-        return proposedFeed.getRoundData(roundId);
+        return proposedFeed.getRoundData(_roundId);
     }
 
+    /**
+     * @inheritdoc IFeedProxy
+     */
     function proposedLatestRoundData()
         external
         view
-        virtual
-        override
         hasProposal
         returns (uint64 id, int256 answer, uint256 updatedAt)
     {
         return proposedFeed.latestRoundData();
     }
 
-    function getFeed() external view override returns (address) {
+    /**
+     * @inheritdoc IFeedProxy
+     */
+    function getFeed() external view returns (address) {
         return address(feed);
     }
 
-    function decimals() external view override returns (uint8) {
+    /**
+     * @inheritdoc IFeedProxy
+     */
+    function getProposedFeed() external view returns (address) {
+        return address(proposedFeed);
+    }
+
+    /**
+     * @notice Return the decimals of the feed.
+     * @return decimals The decimals of the feed.
+     */
+    function decimals() external view returns (uint8) {
         return feed.decimals();
     }
 
     /**
+     * @inheritdoc IFeed
      */
     function typeAndVersion() external view returns (string memory) {
         return feed.typeAndVersion();
@@ -82,17 +121,19 @@ contract FeedProxy is Ownable, IFeedProxy {
     /**
      * @notice returns the description of the feed the proxy points to.
      */
-    function description() external view override returns (string memory) {
+    function description() external view returns (string memory) {
         return feed.description();
     }
 
     /**
-     * @notice returns the current proposed feed
+     * @notice Propose a new feed to update to. This is the first step
+     * of feed update process. The second step (`confirmFeed`) is to
+     * confirm the feed.
+     * @dev Only the owner can propose a new feed. The feed must not
+     * be the zero address. When a new feed is proposed, it emits a
+     * `FeedProposed` event.
+     * @param _feed The address of the new feed
      */
-    function getProposedFeed() external view override returns (address) {
-        return address(proposedFeed);
-    }
-
     function proposeFeed(address _feed) external onlyOwner {
         if (_feed == address(0)) {
             revert InvalidProposedFeed();
@@ -101,6 +142,14 @@ contract FeedProxy is Ownable, IFeedProxy {
         emit FeedProposed(address(feed), _feed);
     }
 
+    /**
+     * @notice Confirm the proposed feed. This is the second step of
+     * feed update process.
+     * @dev Only the owner can confirm the feed. The proposed feed
+     * must not be the zero address. When a new feed is confirmed, it
+     * emits a `FeedConfirmed` event.
+     * @param _feed The address of the proposed feed
+     */
     function confirmFeed(address _feed) external onlyOwner {
         if (_feed != address(proposedFeed)) {
             revert InvalidProposedFeed();
@@ -111,6 +160,12 @@ contract FeedProxy is Ownable, IFeedProxy {
         emit FeedConfirmed(previousFeed, _feed);
     }
 
+    /**
+     * @notice Set the feed to a new address. This function is
+     * internal and should be called by the constructor, or by the
+     * `confirmFeed` function.
+     * @param _feed The address of the new feed
+     */
     function setFeed(address _feed) internal {
         feed = IFeedProxy(_feed);
     }
--- contracts/v0.2/src/interfaces/IFeedProxy.sol
+++ contracts/v0.2/src/interfaces/IFeedProxy.sol
@@ -5,13 +5,34 @@ import {IFeed} from "./IFeed.sol";
 
 interface IFeedProxy is IFeed {
     /**
-     * @notice return the address of the proposed feed.
+     * @notice Get round data from the proposed feed given a round ID.
+     * @param _roundId The round ID.
+     * @return id The round ID.
+     * @return answer The oracle answer.
+     * @return updatedAt Timestamp of the last update.
      */
-    function getProposedFeed() external view returns (address);
+    function proposedGetRoundData(uint64 _roundId)
+        external
+        view
+        returns (uint64 id, int256 answer, uint256 updatedAt);
 
-    function proposedGetRoundData(uint64 roundId) external view returns (uint64 id, int256 answer, uint256 updatedAt);
-
+    /**
+     * @notice Get the latest round data from the proposed feed.
+     * @return id The round ID.
+     * @return answer The oracle answer.
+     * @return updatedAt Timestamp of the last update.
+     */
     function proposedLatestRoundData() external view returns (uint64 id, int256 answer, uint256 updatedAt);
 
+    /**
+     * @notice Return the address of the feed.
+     * @return The address of the feed.
+     */
     function getFeed() external view returns (address);
+
+    /**
+     * @notice Return the address of the proposed feed.
+     * @return The address of the proposed feed.
+     */
+    function getProposedFeed() external view returns (address);
 }
