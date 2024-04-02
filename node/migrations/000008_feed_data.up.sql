493d53d72514828b20709b5d5eb52324f5660d6e
nick <nick@bisonai.com>
2024-04-02 17:36:18 +0900

    fix: fix typo for migration file

--- node/migrations/000008_feed_data.up.sql
+++ node/migrations/000008_feed_data.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS feed_data (
     adapter_id INT8 NOT NULL,
     name TEXT NOT NULL,
     value INT8 NOT NULL,
-    timestamp TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL
+    timestamp TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
     CONSTRAINT feed_data_adapter_id_fkey
         FOREIGN KEY(adapter_id)
         REFERENCES adapters(id)
