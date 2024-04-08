543a480a2f989e851087409b50495e1066b45bd2
Nick <148735107+nick-bisonai@users.noreply.github.com>
2024-04-08 16:33:04 +0900

    feat: minor updates for app control (#1350)

--- node/pkg/admin/admin.go
+++ node/pkg/admin/admin.go
@@ -8,6 +8,7 @@ import (
 	"bisonai.com/orakl/node/pkg/admin/aggregator"
 	"bisonai.com/orakl/node/pkg/admin/feed"
 	"bisonai.com/orakl/node/pkg/admin/fetcher"
+	"bisonai.com/orakl/node/pkg/admin/providerUrl"
 	"bisonai.com/orakl/node/pkg/admin/proxy"
 	"bisonai.com/orakl/node/pkg/admin/reporter"
 	"bisonai.com/orakl/node/pkg/admin/submissionAddress"
@@ -44,6 +45,7 @@ func Run(bus *bus.MessageBus) error {
 	reporter.Routes(v1)
 	wallet.Routes(v1)
 	submissionAddress.Routes(v1)
+	providerUrl.Routes(v1)
 
 	port := os.Getenv("APP_PORT")
 	if port == "" {
