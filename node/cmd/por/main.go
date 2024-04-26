51e48b15020a183b3c1849923814ac3c846b504c
nick <nick@bisonai.com>
2024-04-26 14:53:11 +0900

    feat: customizable por log level

--- node/cmd/por/main.go
+++ node/cmd/por/main.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"context"
+	"os"
+	"strings"
 
 	"bisonai.com/orakl/node/pkg/por"
+	"github.com/rs/zerolog"
 )
 
 func main() {
+	logLevel := os.Getenv("LOG_LEVEL")
+	if logLevel == "" {
+		logLevel = "info"
+	}
+	zerolog.SetGlobalLevel(getLogLevel(logLevel))
+
 	ctx := context.Background()
 	app, err := por.New(ctx)
 	if err != nil {
@@ -15,5 +24,24 @@ func main() {
 	err = app.Run(ctx)
 	if err != nil {
 		panic(err)
+	}
+}
+
+func getLogLevel(input string) zerolog.Level {
+	switch strings.ToLower(input) {
+	case "debug":
+		return zerolog.DebugLevel
+	case "info":
+		return zerolog.InfoLevel
+	case "warn":
+		return zerolog.WarnLevel
+	case "error":
+		return zerolog.ErrorLevel
+	case "fatal":
+		return zerolog.FatalLevel
+	case "panic":
+		return zerolog.PanicLevel
+	default:
+		return zerolog.InfoLevel
 	}
 }
