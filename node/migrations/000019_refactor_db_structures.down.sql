dd96bf5a361780991bde964cc0b0cf302310b7fd
nick <nick@bisonai.com>
2024-04-27 10:42:52 +0900

    fix: fix migration down file

--- node/migrations/000019_refactor_db_structures.down.sql
+++ node/migrations/000019_refactor_db_structures.down.sql
@@ -3,40 +3,39 @@ BEGIN
     -- 1. Add back constraint columns
 
     -- feeds
+    ALTER TABLE feeds DROP CONSTRAINT IF EXISTS feeds_config_id_fkey;
     IF NOT EXISTS(SELECT 1 FROM information_schema.columns WHERE table_name = 'feeds' AND column_name = 'adapter_id') THEN
-        ALTER TABLE feeds DROP CONSTRAINT feeds_config_id_fkey;
-        ALTER TABLE feeds DROP COLUMN config_id;
+        ALTER TABLE feeds DROP COLUMN IF EXISTS config_id;
         ALTER TABLE feeds ADD COLUMN adapter_id INT8 NOT NULL;
     END IF;
 
     -- feed_data
+    ALTER TABLE feed_data DROP CONSTRAINT IF EXISTS feed_data_config_id_fkey;
     IF NOT EXISTS(SELECT 1 FROM information_schema.columns WHERE table_name = 'feed_data' AND column_name = 'name') THEN
-        ALTER TABLE feed_data DROP CONSTRAINT feed_data_config_id_fkey;
-        ALTER TABLE feed_data DROP COLUMN config_id;
+        ALTER TABLE feed_data DROP COLUMN IF EXISTS config_id;
         ALTER TABLE feed_data ADD COLUMN name TEXT NOT NULL;
     END IF;
 
     -- local_aggregates
+    ALTER TABLE local_aggregates DROP CONSTRAINT IF EXISTS local_aggregates_config_id_fkey;
     IF NOT EXISTS(SELECT 1 FROM information_schema.columns WHERE table_name = 'local_aggregates' AND column_name = 'name') THEN
-        ALTER TABLE local_aggregates DROP CONSTRAINT local_aggregates_config_id_fkey;
-        ALTER TABLE local_aggregates DROP COLUMN config_id;
+        ALTER TABLE local_aggregates DROP COLUMN IF EXISTS config_id;
         ALTER TABLE local_aggregates ADD COLUMN name TEXT NOT NULL;
     END IF;
 
     -- global_aggregates
+    ALTER TABLE global_aggregates DROP CONSTRAINT IF EXISTS global_aggregates_config_id_fkey;
     IF NOT EXISTS(SELECT 1 FROM information_schema.columns WHERE table_name = 'global_aggregates' AND column_name = 'name') THEN
-        ALTER TABLE global_aggregates DROP CONSTRAINT global_aggregates_config_id_fkey;
-        ALTER TABLE global_aggregates DROP COLUMN config_id;
+        ALTER TABLE global_aggregates DROP COLUMN IF EXISTS config_id;
         ALTER TABLE global_aggregates ADD COLUMN name TEXT NOT NULL;
     END IF;
 
     -- proofs
+    ALTER TABLE proofs DROP CONSTRAINT IF EXISTS proofs_config_id_fkey;
     IF NOT EXISTS(SELECT 1 FROM information_schema.columns WHERE table_name = 'proofs' AND column_name = 'name') THEN
-        ALTER TABLE proofs DROP CONSTRAINT proofs_config_id_fkey;
-        ALTER TABLE proofs DROP COLUMN config_id;
+        ALTER TABLE proofs DROP COLUMN IF EXISTS config_id;
         ALTER TABLE proofs ADD COLUMN name TEXT NOT NULL;
     END IF;
-
     -- 2. Recreate dropped tables
 
     CREATE TABLE IF NOT EXISTS adapters (
