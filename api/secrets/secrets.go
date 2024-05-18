6ba6051366be0d0d9ecbad3d27ec888e48ee7aff
nick <nick@bisonai.com>
2024-05-18 20:33:48 +0900

    fix: update based on feedback

--- api/secrets/secrets.go
+++ api/secrets/secrets.go
@@ -23,42 +23,46 @@ func init() {
 		vaultSecretPath := os.Getenv("VAULT_SECRET_PATH")
 		vaultKeyName := os.Getenv("VAULT_KEY_NAME")
 
-		if vaultRole != "" && jwtPath != "" && vaultSecretPath != "" && vaultKeyName != "" {
-			config := vault.DefaultConfig()
-			client, err := vault.NewClient(config)
-			if err != nil {
-				log.Error().Err(err).Msg("unable to initialize Vault client")
-				return
-			}
-
-			k8sAuth, err := auth.NewKubernetesAuth(
-				vaultRole,
-				auth.WithServiceAccountTokenPath(jwtPath),
-			)
-			if err != nil {
-				log.Error().Err(err).Msg("unable to initialize Kubernetes auth method")
-				return
-			}
-
-			authInfo, err := client.Auth().Login(ctx, k8sAuth)
-			if err != nil {
-				log.Error().Err(err).Msg("unable to log in with Kubernetes auth")
-				return
-			}
-			if authInfo == nil {
-				log.Error().Err(err).Msg("no auth info was returned after login")
-				return
-			}
-
-			secrets, err := client.KVv2(vaultSecretPath).Get(ctx, vaultKeyName)
-			if err != nil {
-				log.Error().Err(err).Msg("unable to read secret")
-				return
-			}
-
-			secretData = secrets.Data
-			initialized = true
+		if vaultRole == "" || jwtPath == "" || vaultSecretPath == "" || vaultKeyName == "" {
+			log.Error().Msg("Missing required environment variables for Vault initialization")
+			return
 		}
+
+		config := vault.DefaultConfig()
+		client, err := vault.NewClient(config)
+		if err != nil {
+			log.Error().Err(err).Msg("unable to initialize Vault client")
+			return
+		}
+
+		k8sAuth, err := auth.NewKubernetesAuth(
+			vaultRole,
+			auth.WithServiceAccountTokenPath(jwtPath),
+		)
+		if err != nil {
+			log.Error().Err(err).Msg("unable to initialize Kubernetes auth method")
+			return
+		}
+
+		authInfo, err := client.Auth().Login(ctx, k8sAuth)
+		if err != nil {
+			log.Error().Err(err).Msg("unable to log in with Kubernetes auth")
+			return
+		}
+		if authInfo == nil {
+			log.Error().Err(err).Msg("no auth info was returned after login")
+			return
+		}
+
+		secrets, err := client.KVv2(vaultSecretPath).Get(ctx, vaultKeyName)
+		if err != nil {
+			log.Error().Err(err).Msg("unable to read secret")
+			return
+		}
+
+		secretData = secrets.Data
+		initialized = true
+
 	})
 }
 
