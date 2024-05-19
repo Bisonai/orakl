d5da3d43a49b4a727442da4ee7d81c6516135fdc
nick <nick@bisonai.com>
2024-05-19 22:41:24 +0900

    feat: event checker intended for contractV2

---
+++ sentry/pkg/checker/event/app.go
@@ -0,0 +1,121 @@
+package event
+
+import (
+	"context"
+	"os"
+	"time"
+
+	"bisonai.com/orakl/sentry/pkg/alert"
+	"bisonai.com/orakl/sentry/pkg/db"
+	"bisonai.com/orakl/sentry/pkg/request"
+	"github.com/rs/zerolog/log"
+)
+
+var FeedsToCheck = []FeedToCheck{}
+var EventCheckInterval time.Duration
+
+func setUp(ctx context.Context) error {
+	EventCheckInterval = 60 * time.Second
+
+	interval := os.Getenv("EVENT_CHECK_INTERVAL")
+	parsedInterval, err := time.ParseDuration(interval)
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to parse HEALTH_CHECK_INTERVAL, using default 60s")
+	} else {
+		EventCheckInterval = parsedInterval
+	}
+
+	configs, err := loadExpectedEventIntervals()
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to load expected event intervals")
+		return err
+	}
+	subgraphInfoMap, err := loadSubgraphInfoMap(ctx)
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to load subgraph info")
+		return err
+	}
+
+	for _, config := range configs {
+		subgraphInfo, ok := subgraphInfoMap[config.Name]
+		if !ok {
+			continue
+		}
+
+		if subgraphInfo.Status != "current" {
+			continue
+		}
+
+		FeedsToCheck = append(FeedsToCheck, FeedToCheck{
+			SchemaName:       subgraphInfo.SchemaName,
+			FeedName:         config.Name,
+			ExpectedInterval: config.SubmitInterval,
+		})
+	}
+
+	return nil
+}
+
+func Start(ctx context.Context) {
+	log.Info().Msg("Starting event checker")
+	setUp(ctx)
+	ticker := time.NewTicker(EventCheckInterval)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		check(ctx)
+	}
+}
+
+func check(ctx context.Context) {
+	msg := ""
+	for _, feed := range FeedsToCheck {
+		delayedTime, err := timeSinceLastEvent(ctx, feed)
+		if err != nil {
+			continue
+		}
+		if delayedTime > time.Duration(feed.ExpectedInterval)*time.Millisecond {
+			msg += feed.FeedName + " delayed by " + delayedTime.String() + "\n"
+		}
+	}
+	if msg != "" {
+		alert.SlackAlert(msg)
+	}
+}
+
+func timeSinceLastEvent(ctx context.Context, feed FeedToCheck) (time.Duration, error) {
+	type QueriedTime struct {
+		UnixTime int64 `db:"time"`
+	}
+	query := feedEventQuery(feed.SchemaName)
+	queriedTime, err := db.QueryRow[QueriedTime](ctx, query, nil)
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to query last event time")
+		return 0, err
+	}
+	lastEventTime := time.Unix(queriedTime.UnixTime, 0)
+	return time.Since(lastEventTime), nil
+}
+
+func loadExpectedEventIntervals() ([]Config, error) {
+	chain := os.Getenv("CHAIN")
+	if chain == "" {
+		chain = "baobab"
+	}
+	url := loadOraklConfigUrl(chain)
+	return request.GetRequest[[]Config](url, nil, nil)
+}
+
+func loadSubgraphInfoMap(ctx context.Context) (map[string]SubgraphInfo, error) {
+	subgraphInfos, err := db.QueryRows[SubgraphInfo](ctx, SubgraphInfoQuery, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	subgraphInfoMap := make(map[string]SubgraphInfo)
+	for _, subgraphInfo := range subgraphInfos {
+		subgraphInfoMap[subgraphInfo.Name] = subgraphInfo
+	}
+
+	return subgraphInfoMap, nil
+}
---
+++ sentry/pkg/checker/event/types.go
@@ -0,0 +1,55 @@
+package event
+
+import (
+	"fmt"
+)
+
+const (
+	SubgraphInfoQuery = `SELECT ds.id AS schema_id,
+    ds.name AS schema_name,
+    ds.subgraph,
+    ds.version,
+    s.name,
+        CASE
+            WHEN s.pending_version = v.id THEN 'pending'::text
+            WHEN s.current_version = v.id THEN 'current'::text
+            ELSE 'unused'::text
+        END AS status,
+    d.failed,
+    d.synced
+   FROM deployment_schemas ds,
+    subgraphs.subgraph_deployment d,
+    subgraphs.subgraph_version v,
+    subgraphs.subgraph s
+  WHERE d.deployment = ds.subgraph::text AND v.deployment = d.deployment AND v.subgraph = s.id;`
+)
+
+type SubgraphInfo struct {
+	SchemaId   int    `db:"schema_id"`
+	SchemaName string `db:"schema_name"`
+	Subgraph   string `db:"subgraph"`
+	Version    int    `db:"version"`
+	Name       string `db:"name"`
+	Status     string `db:"status"`
+	Failed     bool   `db:"failed"`
+	Synced     bool   `db:"synced"`
+}
+
+type Config struct {
+	Name           string `json:"name"`
+	SubmitInterval int    `json:"submitInterval"`
+}
+
+type FeedToCheck struct {
+	SchemaName       string
+	FeedName         string
+	ExpectedInterval int
+}
+
+func feedEventQuery(schemaName string) string {
+	return fmt.Sprintf(`SELECT time FROM %s.feed_feed_updated ORDER BY time DESC LIMIT 1;`, schemaName)
+}
+
+func loadOraklConfigUrl(chain string) string {
+	return fmt.Sprintf("https://raw.githubusercontent.com/Bisonai/orakl-config/master/%s_configs.json", chain)
+}
