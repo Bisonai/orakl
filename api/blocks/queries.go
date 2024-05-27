38d2f6d640f54e4d8217a84fc37fff0824ced182
Intizar <intizar.tashov22@gmail.com>
2024-05-27 14:29:08 +0900

    write queries

--- api/blocks/queries.go
+++ api/blocks/queries.go
@@ -1,2 +1,29 @@
 package blocks
 
+const (
+	// upsert to observed_blocks given service and block_number
+	UpsertObservedBlock = `
+		INSERT INTO observed_blocks (service_id, block_number)
+		VALUES (@service, @block_number)
+		ON CONFLICT (service) DO UPDATE SET block_number = @block_number
+		RETURNING *;
+	`
+
+	// upsert to unprocessed_blocks given service and block_number
+	UpsertUnprocessedBlock = `
+		INSERT INTO unprocessed_blocks (service, block_number)
+		VALUES (@service, @block_number)
+		ON CONFLICT (service, block_number) DO NOTHING
+		RETURNING *;
+	`
+
+	// get all unprocessed blocks given service
+	GetUnprocessedBlocks = `
+		SELECT * FROM unprocessed_blocks WHERE service = @service
+	`
+
+	// delete unprocessed block given service and block_number
+	DeleteUnprocessedBlock = `
+		DELETE FROM unprocessed_blocks WHERE service = @service AND block_number = @block_number
+	`
+)
