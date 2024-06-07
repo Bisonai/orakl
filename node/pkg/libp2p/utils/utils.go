b7f1cd0a101211e4817ad7ea1a43a13cb502e1c9
nick <nick@bisonai.com>
2024-06-07 11:12:23 +0900

    feat: add logs

--- node/pkg/libp2p/utils/utils.go
+++ node/pkg/libp2p/utils/utils.go
@@ -56,6 +56,7 @@ func IsHostAlive(ctx context.Context, h host.Host, addr string) (bool, error) {
 
 	lastErr := retrier.Retry(
 		func() error {
+			log.Info().Str("addr", addr).Str("peer", info.ID.String()).Msg("checking peer alive")
 			return h.Connect(ctx, *info)
 		},
 		3,
