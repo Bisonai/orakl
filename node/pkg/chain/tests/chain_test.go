6a75e845cad251b5c5634986dfd7810e6f656977
Martin Kersner <martin@bisonai.com>
2024-06-12 14:26:39 +0900

    chore: update kairos json-rpc

--- node/pkg/chain/tests/chain_test.go
+++ node/pkg/chain/tests/chain_test.go
@@ -35,7 +35,7 @@ func TestNewKlaytnHelper(t *testing.T) {
 	}
 	err = db.QueryWithoutResult(ctx, InsertProviderUrlQuery, map[string]any{
 		"chain_id": 1001,
-		"url":      "https://klaytn-baobab-rpc.allthatnode.com:8551",
+		"url":      "https://public-en.kairos.node.kaia.io",
 		"priority": 2,
 	})
 	if err != nil {
