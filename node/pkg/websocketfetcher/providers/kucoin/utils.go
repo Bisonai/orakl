69d0c533c68b138e9a51921879e503063cf6c9cd
nick <nick@bisonai.com>
2024-06-17 10:14:24 +0900

    fix: fix linter err

--- node/pkg/websocketfetcher/providers/kucoin/utils.go
+++ node/pkg/websocketfetcher/providers/kucoin/utils.go
@@ -9,7 +9,7 @@ import (
 func RawDataToFeedData(raw SymbolSnapshotRaw, feedMap map[string]int32) *common.FeedData {
 	snapshot := raw.Data.Data
 	symbol := snapshot.Symbol
-	id, _ := feedMap[symbol]
+	id := feedMap[symbol]
 
 	timestamp := time.Unix(snapshot.Time/1000, 0)
 	value := common.FormatFloat64Price(snapshot.Price)
