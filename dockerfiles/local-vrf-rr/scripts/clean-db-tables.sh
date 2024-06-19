f370c8e7249e9fe50c0c0ac0967487cbae7960c6
Intizar Tashov <intizar.tashov22@gmail.com>
2024-06-19 18:51:16 +0900

    replace hardcoded values with variables in vrf rr docker setup (#1645)
    * replace hardcoded values with variables in vrf rr docker setup
    
    * update readme

--- dockerfiles/local-vrf-rr/scripts/clean-db-tables.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-psql -h postgres -U "${POSTGRES_USER}" -d "${POSTGRES_DB}" <<EOF
-DELETE FROM vrf_keys CASCADE;
-DELETE FROM listeners CASCADE;
-DELETE FROM reporters CASCADE;
-DELETE FROM chains CASCADE;
-DELETE FROM services CASCADE;
-EOF
--- dockerfiles/local-vrf-rr/scripts/update-vrf-migration.js
+++ dockerfiles/local-vrf-rr/scripts/update-vrf-migration.js
@@ -4,6 +4,7 @@ const path = require("node:path");
 const dir = "contracts/v0.1/migration/localhost/VRF";
 const pkX = process.argv[2];
 const pkY = process.argv[3];
+const address = process.argv[4];
 
 const files = fs.readdirSync(dir);
 const jsonFile = files.find((file) => path.extname(file) === ".json");
@@ -11,7 +12,7 @@ const filePath = path.join(dir, jsonFile);
 const migrationData = JSON.parse(fs.readFileSync(filePath, "utf8"));
 migrationData["registerOracle"] = [
   {
-    address: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+    address: address,
     publicProvingKey: [pkX, pkY],
   },
 ];
