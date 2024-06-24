352ce74a8195e07f6b173661d8eb114ff690f319
Intizar <intizar.tashov22@gmail.com>
2024-06-24 17:36:42 +0900

    update dockerfile and setup-rr.sh

--- dockerfiles/local-vrf-rr/scripts/setup-rr.sh
+++ dockerfiles/local-vrf-rr/scripts/setup-rr.sh
@@ -15,22 +15,20 @@ cd cli || exit
 yarn cli chain insert --name "$CHAIN"
 yarn cli service insert --name REQUEST_RESPONSE
 
-cd .. || exit
-node update-rr-migration.js "$ADDRESS"
-node update-hardhat-network.js "$PROVIDER_URL"
 
 if [ "$CHAIN" == "localhost" ]; then
+  cd .. || exit
+  node update-rr-migration.js "$ADDRESS"
+  node update-hardhat-network.js "$PROVIDER_URL"
   cd contracts/v0.1 || exit
-  yarn deploy:"$CHAIN":prepayment
-  yarn deploy:"$CHAIN":rr
+  yarn deploy:localhost:prepayment
+  yarn deploy:localhost:rr
   cd ../../cli || exit
-else
-  cd cli || exit
 fi
 
 ORACLE_ADDRESS=0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512
 if [ "$CHAIN" == "baobab" ]; then
-  ORACLE_ADDRESS=0x0d2fAcdd858F04AAb1263Ab10f3a04A512E0640C
+  ORACLE_ADDRESS=0x71F4d40dFe60349E67105E02AE5a1Db41fD5AF0e
 elif [ "$CHAIN" == "cypress" ]; then
   ORACLE_ADDRESS=0x159F3BB6609B4C709F15823F3544032942106042
 fi
