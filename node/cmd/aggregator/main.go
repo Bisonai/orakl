148d1c0684b89ae0e77e276828aa55a7049c9c22
Nick <148735107+nick-bisonai@users.noreply.github.com>
2024-06-28 14:27:40 +0900

    (OraklNode) Add aggregator entrypoint (#1700)
    * feat: add aggregator entrypoint
    
    * feat: update based on feedback
    
    * fix: updates based on feedback

---
+++ node/cmd/aggregator/main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"context"
+	"os"
+	"strings"
+	"sync"
+	"time"
+
+	"bisonai.com/orakl/node/pkg/admin"
+	"bisonai.com/orakl/node/pkg/aggregator"
+	"bisonai.com/orakl/node/pkg/bus"
+	"bisonai.com/orakl/node/pkg/fetcher"
+	libp2pSetup "bisonai.com/orakl/node/pkg/libp2p/setup"
+	"bisonai.com/orakl/node/pkg/utils/retrier"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func main() {
+	logLevel := os.Getenv("LOG_LEVEL")
+	if logLevel == "" {
+		logLevel = "info"
+	}
+	zerolog.SetGlobalLevel(getLogLevel(logLevel))
+
+	ctx := context.Background()
+	mb := bus.New(10)
+	var wg sync.WaitGroup
+
+	host, err := libp2pSetup.NewHost(ctx, libp2pSetup.WithHolePunch())
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to make host")
+		return
+	}
+
+	ps, err := libp2pSetup.MakePubsub(ctx, host)
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to make pubsub")
+		return
+	}
+
+	err = retrier.Retry(func() error {
+		return libp2pSetup.ConnectThroughBootApi(ctx, host)
+	}, 5, 10*time.Second, 30*time.Second)
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to setup libp2p")
+		return
+	}
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		adminErr := admin.Run(mb)
+		if adminErr != nil {
+			log.Fatal().Err(adminErr).Msg("Failed to start admin server")
+			os.Exit(1)
+			return
+		}
+	}()
+
+	log.Info().Msg("Syncing orakl config")
+	err = admin.SyncOraklConfig(ctx)
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to sync orakl config")
+		return
+	}
+	log.Info().Msg("Orakl config synced")
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		f := fetcher.New(mb)
+		fetcherErr := f.Run(ctx)
+		if fetcherErr != nil {
+			log.Fatal().Err(fetcherErr).Msg("Failed to start fetcher")
+			os.Exit(1)
+			return
+		}
+	}()
+	log.Info().Msg("Fetcher started")
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		a := aggregator.New(mb, host, ps)
+		aggregatorErr := a.Run(ctx)
+		if aggregatorErr != nil {
+			log.Fatal().Err(aggregatorErr).Msg("Failed to start aggregator")
+			os.Exit(1)
+		}
+	}()
+	log.Info().Msg("Aggregator started")
+
+	wg.Wait()
+}
+
+func getLogLevel(input string) zerolog.Level {
+	switch strings.ToLower(input) {
+	case "debug":
+		return zerolog.DebugLevel
+	case "info":
+		return zerolog.InfoLevel
+	case "warn":
+		return zerolog.WarnLevel
+	case "error":
+		return zerolog.ErrorLevel
+	case "fatal":
+		return zerolog.FatalLevel
+	case "panic":
+		return zerolog.PanicLevel
+	default:
+		log.Warn().Msg("Unrecognized log level, defaulting to InfoLevel")
+		return zerolog.InfoLevel
+	}
+}
