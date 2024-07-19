5dc540c5fe0d5178864731ac88a722331ecdd035
nick <nick@bisonai.com>
2024-07-19 11:01:43 +0900

    feat: handle sub response

--- node/pkg/websocketfetcher/providers/huobi/type.go
+++ node/pkg/websocketfetcher/providers/huobi/type.go
@@ -35,3 +35,10 @@ type Heartbeat struct {
 type HeartbeatResponse struct {
 	Pong int64 `json:"pong"`
 }
+
+type SubResponse struct {
+	ID     string `json:"id"`
+	Status string `json:"status"`
+	Subbed string `json:"subbed"`
+	Ts     int64  `json:"ts"`
+}
