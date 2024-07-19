f203c86d6093c2300950eb8a7a2ea50fe4d5bea9
nick <nick@bisonai.com>
2024-07-19 14:52:42 +0900

    feat: dal api freshness checker

---
+++ sentinel/pkg/checker/dal/app.go
@@ -0,0 +1,89 @@
+package dal
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+
+	"bisonai.com/orakl/sentinel/pkg/alert"
+	"bisonai.com/orakl/sentinel/pkg/request"
+	"bisonai.com/orakl/sentinel/pkg/secrets"
+	"github.com/rs/zerolog/log"
+)
+
+const DefaultDalCheckInterval = 10 * time.Second
+
+type OutgoingSubmissionData struct {
+	Symbol        string   `json:"symbol"`
+	Value         string   `json:"value"`
+	AggregateTime string   `json:"aggregateTime"`
+	Proof         []byte   `json:"proof"`
+	FeedHash      [32]byte `json:"feedHash"`
+	Decimals      string   `json:"decimals"`
+}
+
+func Start() error {
+	interval, err := time.ParseDuration(os.Getenv("DAL_CHECK_INTERVAL"))
+	if err != nil {
+		interval = DefaultDalCheckInterval
+	}
+
+	chain := os.Getenv("CHAIN")
+	if chain == "" {
+		return errors.New("CHAIN not found")
+	}
+
+	key := secrets.GetSecret("DAL_API_KEY")
+	if key == "" {
+		return errors.New("DAL_API_KEY not found")
+	}
+
+	endpoint := fmt.Sprintf("https://dal.%s.orakl.network/api/v1", chain)
+
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		err := checkDal(endpoint, key)
+		if err != nil {
+			log.Error().Str("Player", "DalChecker").Err(err).Msg("error in checkDal")
+		}
+	}
+	return nil
+}
+
+func checkDal(endpoint string, key string) error {
+	msg := ""
+
+	resp, err := request.Request[[]OutgoingSubmissionData](
+		request.WithEndpoint(endpoint+"/dal/latest-data-feeds/all"),
+		request.WithHeaders(map[string]string{"X-API-Key": key}),
+	)
+	if err != nil {
+		return err
+	}
+
+	for _, data := range resp {
+		rawTimestamp, err := strconv.ParseInt(data.AggregateTime, 10, 64)
+		if err != nil {
+			log.Error().Str("Player", "DalChecker").Err(err).Msg("failed to convert timestamp string to int64")
+			continue
+		}
+
+		timestamp := time.Unix(rawTimestamp, 0)
+		offset := time.Since(timestamp)
+		log.Debug().Str("Player", "DalChecker").Str("symbol", data.Symbol).Time("timestamp", timestamp).Dur("offset", offset).Msg("DAL price check")
+
+		if offset > 5*time.Second {
+			msg += fmt.Sprintf("(DAL) %s price update delayed by %s\n", data.Symbol, offset)
+		}
+	}
+
+	if msg != "" {
+		alert.SlackAlert(msg)
+	}
+
+	return nil
+}
