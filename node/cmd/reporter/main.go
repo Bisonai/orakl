4d7b3ab39af39b43f5cb628b53d80e670d401a3c
Intizar Tashov <intizar.tashov22@gmail.com>
2024-07-22 23:28:55 +0900

    [Separate Reporter Part 2] base report logic with rest api (#1873)
    * comment out / remove unused code from reporter
    
    * comment out tests and create reporter entrypoint
    
    * temp comment out deviation reporter
    
    * initial dal fetch and submit logic
    
    * report logic implemented
    
    * temp disable reporter tests
    
    * remove reporter from node entrypoint
    
    * replace zerolog with zeropglog in reporter entrypoing
    
    * shared chain helper and mutex

---
+++ node/cmd/reporter/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"bisonai.com/orakl/node/pkg/reporter"
+	"bisonai.com/orakl/node/pkg/zeropglog"
+	"github.com/rs/zerolog/log"
+)
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+
+	zeropglog := zeropglog.New()
+	go zeropglog.Run(ctx)
+
+	r := reporter.New()
+	err := r.Run(ctx)
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to start reporter")
+		cancel()
+		return
+	}
+
+	<-sigChan
+	log.Info().Msg("Reporter termination signal received")
+
+	cancel()
+
+	log.Info().Msg("Reporter service has stopped")
+}
--- node/pkg/error/sentinel.go
+++ node/pkg/error/sentinel.go
@@ -195,6 +195,7 @@ var (
 	ErrReporterStart                            = &CustomError{Service: Reporter, Code: InternalError, Message: "Failed to start reporters"}
 	ErrReporterStop                             = &CustomError{Service: Reporter, Code: InternalError, Message: "Failed to stop reporters"}
 	ErrReporterValidateAggregateTimestampValues = &CustomError{Service: Reporter, Code: InternalError, Message: "Failed to validate aggregate timestamp values"}
+	ErrReporterDalApiKeyNotFound                = &CustomError{Service: Reporter, Code: InternalError, Message: "DAL API key not found in reporter"}
 
 	ErrDalEmptyProofParam      = &CustomError{Service: Dal, Code: InvalidInputError, Message: "Empty proof param"}
 	ErrDalInvalidProofLength   = &CustomError{Service: Dal, Code: InvalidInputError, Message: "Invalid proof length"}
