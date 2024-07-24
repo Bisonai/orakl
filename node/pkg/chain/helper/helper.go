9a197eb12eb3e137d07d0bcfa5414306872d57a1
nick <nick@bisonai.com>
2024-07-24 16:11:22 +0900

    feat: increase delegator timeout

--- node/pkg/chain/helper/helper.go
+++ node/pkg/chain/helper/helper.go
@@ -6,7 +6,6 @@ import (
 	"math/big"
 	"os"
 	"strings"
-	"time"
 
 	"bisonai.com/orakl/node/pkg/chain/utils"
 	errorSentinel "bisonai.com/orakl/node/pkg/error"
@@ -155,7 +154,11 @@ func (t *ChainHelper) GetSignedFromDelegator(tx *types.Transaction) (*types.Tran
 		return nil, err
 	}
 
-	result, err := request.Request[signedTx](request.WithEndpoint(t.delegatorUrl+DelegatorEndpoint), request.WithMethod("POST"), request.WithBody(payload), request.WithTimeout(5*time.Second))
+	result, err := request.Request[signedTx](
+		request.WithEndpoint(t.delegatorUrl+DelegatorEndpoint),
+		request.WithMethod("POST"),
+		request.WithBody(payload),
+		request.WithTimeout(DelegatorTimeout))
 	if err != nil {
 		log.Error().Err(err).Msg("failed to request sign from delegator")
 		return nil, err
