313cb769cd5219c8c88a5091a411a9726cd0ab6a
Intizar <intizar.tashov22@gmail.com>
2024-07-26 19:23:49 +0900

    remove db from config struct

--- node/pkg/reporter/main_test.go
+++ node/pkg/reporter/main_test.go
@@ -5,13 +5,11 @@ import (
 	"os"
 	"testing"
 
-	"bisonai.com/orakl/node/pkg/db"
 	"github.com/rs/zerolog"
 )
 
 func TestMain(m *testing.M) {
 	zerolog.SetGlobalLevel(zerolog.InfoLevel)
 	code := m.Run()
-	db.ClosePool()
 	os.Exit(code)
 }
