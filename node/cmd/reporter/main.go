163ec0932876dabf01f931b37f3b22650b43ece7
Intizar Tashov <intizar.tashov22@gmail.com>
2024-07-26 17:45:38 +0900

    reporter health check endpoint and sentinel check (#1912)
    * reporter health endpoint
    
    * include reporter health check to sentinel
    
    * move healthcheck creation from run to cmd
    
    * remove sentinel updates

--- node/cmd/reporter/main.go
+++ node/cmd/reporter/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
@@ -22,12 +23,31 @@ func main() {
 	go zeropglog.Run(ctx)
 
 	r := reporter.New()
+
 	err := r.Run(ctx)
 	if err != nil {
 		log.Error().Err(err).Msg("Failed to start reporter")
 		cancel()
 		return
 	}
+
+	go func() {
+		port := os.Getenv("POR_PORT")
+		if port == "" {
+			port = "3000"
+		}
+
+		http.HandleFunc("/api/v1", func(w http.ResponseWriter, r *http.Request) {
+			_, err := w.Write([]byte("Orakl Reporter"))
+			if err != nil {
+				log.Error().Err(err).Msg("failed to write response")
+			}
+		})
+
+		if err := http.ListenAndServe(":"+port, nil); err != nil {
+			log.Fatal().Err(err).Msg("failed to start http server")
+		}
+	}()
 
 	<-sigChan
 	log.Info().Msg("Reporter termination signal received")
