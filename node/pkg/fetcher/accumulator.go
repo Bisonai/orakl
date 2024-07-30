6d41dd365f24d4121644d25aa76c0a80bf07a71a
nick <nick@bisonai.com>
2024-07-30 13:49:23 +0900

    Revert "fix: will be rollbacked if not effective (#1947)"
    This reverts commit f01799b1cdb836faeb20fca1d58d2c991aaea0c2.

--- node/pkg/fetcher/accumulator.go
+++ node/pkg/fetcher/accumulator.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"time"
 
+	"bisonai.com/orakl/node/pkg/common/keys"
 	"bisonai.com/orakl/node/pkg/db"
 	"github.com/rs/zerolog/log"
 )
@@ -38,7 +39,7 @@ func (a *Accumulator) accumulatorJob(ctx context.Context) {
 	if len(a.accumulatorChannel) == 0 {
 		return
 	}
-
+	localAggregatesDataRedis := make(map[string]interface{})
 	var localAggregatesDataPgsql [][]any
 
 loop:
@@ -46,14 +47,22 @@ loop:
 		select {
 		case data := <-a.accumulatorChannel:
 			localAggregatesDataPgsql = append(localAggregatesDataPgsql, []any{data.ConfigID, int64(data.Value), data.Timestamp})
+
+			redisKey := keys.LocalAggregateKey(data.ConfigID)
+			existingData, exists := localAggregatesDataRedis[redisKey]
+			if exists && existingData.(LocalAggregate).Timestamp.After(data.Timestamp) {
+				continue
+			}
+			localAggregatesDataRedis[redisKey] = data
 		default:
 			break loop
 		}
 	}
 
+	redisErr := db.MSetObject(ctx, localAggregatesDataRedis)
 	_, pgsqlErr := db.BulkCopy(ctx, "local_aggregates", []string{"config_id", "value", "timestamp"}, localAggregatesDataPgsql)
 
-	if pgsqlErr != nil {
-		log.Error().Err(pgsqlErr).Msg("failed to save local aggregates")
+	if redisErr != nil || pgsqlErr != nil {
+		log.Error().Err(redisErr).Err(pgsqlErr).Msg("failed to save local aggregates")
 	}
 }
