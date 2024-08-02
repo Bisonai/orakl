a59d240ad46694f98fea022610fe6ed5b73708c4
nick <nick@bisonai.com>
2024-08-03 03:44:59 +0900

    raft: improve mutex, concurrent msg read

--- node/pkg/raft/accessors.go
+++ node/pkg/raft/accessors.go
@@ -6,70 +6,10 @@ func (r *Raft) IncreaseTerm() {
 	r.Term++
 }
 
-func (r *Raft) UpdateTerm(newTerm int) {
-	r.Mutex.Lock()
-	defer r.Mutex.Unlock()
-	r.Term = newTerm
-}
-
-func (r *Raft) GetCurrentTerm() int {
-	r.Mutex.Lock()
-	defer r.Mutex.Unlock()
-	return r.Term
-}
-
-func (r *Raft) IncreaseVote() {
-	r.Mutex.Lock()
-	defer r.Mutex.Unlock()
-	r.VotesReceived++
-}
-
-func (r *Raft) UpdateVoteReceived(votes int) {
-	r.Mutex.Lock()
-	defer r.Mutex.Unlock()
-	r.VotesReceived = votes
-}
-
-func (r *Raft) GetVoteReceived() int {
-	r.Mutex.Lock()
-	defer r.Mutex.Unlock()
-	return r.VotesReceived
-}
-
-func (r *Raft) UpdateRole(role RoleType) {
-	r.Mutex.Lock()
-	defer r.Mutex.Unlock()
-	r.Role = role
-}
-
-func (r *Raft) GetRole() RoleType {
-	r.Mutex.Lock()
-	defer r.Mutex.Unlock()
-	return r.Role
-}
-
-func (r *Raft) GetVotedFor() string {
-	r.Mutex.Lock()
-	defer r.Mutex.Unlock()
-	return r.VotedFor
-}
-
 func (r *Raft) GetLeader() string {
 	r.Mutex.Lock()
 	defer r.Mutex.Unlock()
 	return r.LeaderID
-}
-
-func (r *Raft) UpdateLeader(leader string) {
-	r.Mutex.Lock()
-	defer r.Mutex.Unlock()
-	r.LeaderID = leader
-}
-
-func (r *Raft) UpdateVotedFor(votedFor string) {
-	r.Mutex.Lock()
-	defer r.Mutex.Unlock()
-	r.VotedFor = votedFor
 }
 
 func (r *Raft) SubscribersCount() int {
