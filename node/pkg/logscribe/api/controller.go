a1da3784ee7b050e7d508277310634ad25c7d9e0
Intizar <intizar.tashov22@gmail.com>
2024-08-05 14:02:19 +0900

    defer inserting logs in controller

--- node/pkg/logscribe/api/controller.go
+++ node/pkg/logscribe/api/controller.go
@@ -16,12 +16,13 @@ type LogInsertModel struct {
 }
 
 func insertLogs(c *fiber.Ctx) error {
-	logEntries := new([]LogInsertModel)
-	if err := c.BodyParser(logEntries); err != nil {
-		log.Error().Err(err).Msg("Failed to parse request body")
-		return c.Status(fiber.StatusBadRequest).SendString("Failed to parse request body")
-	}
-	c.Locals("logsChannel").(chan *[]LogInsertModel) <- logEntries
+	defer func(c *fiber.Ctx) {
+		logEntries := new([]LogInsertModel)
+		if err := c.BodyParser(logEntries); err != nil {
+			log.Error().Err(err).Msg("Failed to parse request body")
+		}
+		c.Locals("logsChannel").(chan *[]LogInsertModel) <- logEntries
+	}(c)
 
-	return c.Status(fiber.StatusOK).SendString("Logs inserted successfully")
+	return c.Status(fiber.StatusOK).SendString("Request received successfully")
 }
