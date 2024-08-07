f1bcfc3bbe35c2b02b3c8c5f44ba11b9d10bcfc5
nick <nick@bisonai.com>
2024-08-07 13:26:53 +0900

    feat: get log lv from env

--- node/cmd/dal/main.go
+++ node/cmd/dal/main.go
@@ -2,17 +2,45 @@ package main
 
 import (
 	"context"
+	"os"
+	"strings"
 
 	"bisonai.com/orakl/node/pkg/dal"
+	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
 
 func main() {
+	logLevel := os.Getenv("LOG_LEVEL")
+	if logLevel == "" {
+		logLevel = "info"
+	}
+	zerolog.SetGlobalLevel(getLogLevel(logLevel))
+
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
 	err := dal.Run(ctx)
 	if err != nil {
 		log.Fatal().Err(err).Msg("Failed to start DAL")
+	}
+}
+
+func getLogLevel(input string) zerolog.Level {
+	switch strings.ToLower(input) {
+	case "debug":
+		return zerolog.DebugLevel
+	case "info":
+		return zerolog.InfoLevel
+	case "warn":
+		return zerolog.WarnLevel
+	case "error":
+		return zerolog.ErrorLevel
+	case "fatal":
+		return zerolog.FatalLevel
+	case "panic":
+		return zerolog.PanicLevel
+	default:
+		return zerolog.InfoLevel
 	}
 }
