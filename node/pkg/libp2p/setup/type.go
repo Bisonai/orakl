cea77a4cfb195d4035de6780243467c2c4a07ef0
Nick <148735107+nick-bisonai@users.noreply.github.com>
2024-08-08 14:06:19 +0900

    feat: reduce default 120 sec -> 10 sec for msg ttl (#2046)

--- node/pkg/libp2p/setup/type.go
+++ node/pkg/libp2p/setup/type.go
@@ -5,6 +5,7 @@ import (
 	"crypto/rand"
 	"crypto/sha256"
 	"fmt"
+	"time"
 
 	"bisonai.com/orakl/node/pkg/secrets"
 
@@ -102,5 +103,5 @@ func NewHost(ctx context.Context, opts ...HostOption) (host.Host, error) {
 
 func MakePubsub(ctx context.Context, host host.Host) (*pubsub.PubSub, error) {
 	log.Debug().Msg("creating pubsub instance")
-	return pubsub.NewGossipSub(ctx, host)
+	return pubsub.NewGossipSub(ctx, host, pubsub.WithSeenMessagesTTL(10*time.Second))
 }
