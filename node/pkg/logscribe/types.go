4273687d7158457ca829745f9ca80a285ad8a9cf
Intizar <intizar.tashov22@gmail.com>
2024-08-09 18:08:04 +0900

    update tests and workflow

--- node/pkg/logscribe/types.go
+++ node/pkg/logscribe/types.go
@@ -1,6 +1,8 @@
 package logscribe
 
 import (
+	"time"
+
 	"bisonai.com/orakl/node/pkg/logscribe/api"
 	"github.com/google/go-github/github"
 )
@@ -11,7 +13,28 @@ type LogInsertModelWithID struct {
 }
 
 type App struct {
-	githubOwner  string
-	githubRepo   string
-	githubClient *github.Client
+	githubOwner          string
+	githubRepo           string
+	githubClient         *github.Client
+	processLogsInterval  time.Duration
+	bulkLogsCopyInterval time.Duration
+}
+
+type AppOption func(c *AppConfig)
+
+type AppConfig struct {
+	processLogsInterval  time.Duration
+	bulkLogsCopyInterval time.Duration
+}
+
+func WithProcessLogsInterval(interval time.Duration) AppOption {
+	return func(c *AppConfig) {
+		c.processLogsInterval = interval
+	}
+}
+
+func WithBulkLogsCopyInterval(interval time.Duration) AppOption {
+	return func(c *AppConfig) {
+		c.bulkLogsCopyInterval = interval
+	}
 }
