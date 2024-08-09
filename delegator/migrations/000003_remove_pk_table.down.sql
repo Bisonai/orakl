7f1c50ac580342a68f69fea76b215466573910cf
nick <nick@bisonai.com>
2024-08-09 17:03:46 +0900

    fix: remove migrated codes

--- delegator/migrations/000003_remove_pk_table.down.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE IF NOT EXISTS "fee_payers" (
-    "privateKey" VARCHAR(66) NOT NULL,
-    CONSTRAINT "fee_payers_privateKey_key" UNIQUE ("privateKey")
-);
--- delegator/organization/queries.go
+++
@@ -1,9 +0,0 @@
-package organization
-
-const (
-	InsertOrganization  = `INSERT INTO organizations (name) VALUES (@name) RETURNING *;`
-	GetOrganization     = `SELECT * FROM organizations;`
-	GetOrganizationById = `SELECT * FROM organizations WHERE organization_id = @id;`
-	UpdateOrganization  = `UPDATE organizations SET name = @name WHERE organization_id = @id RETURNING *;`
-	DeleteOrganization  = `DELETE FROM organizations WHERE organization_id = @id RETURNING *;`
-)
