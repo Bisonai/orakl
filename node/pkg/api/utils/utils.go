5b9d9a1253432f49de75e1a4b44341f534d6b217
nick <nick@bisonai.com>
2024-08-20 17:38:01 +0900

    feat: zerologging from error handler

--- node/pkg/api/utils/utils.go
+++ node/pkg/api/utils/utils.go
@@ -8,7 +8,6 @@ import (
 	"encoding/hex"
 	"errors"
 	"fmt"
-	"log"
 	"os"
 	"runtime/debug"
 	"strconv"
@@ -22,6 +21,7 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/recover"
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/rs/zerolog/log"
 )
 
 type AppConfig struct {
@@ -275,7 +275,7 @@ func CustomErrorHandler(c *fiber.Ctx, err error) error {
 
 	// Return status code with error message
 	// | ${status} | ${ip} | ${method} | ${path} | ${error}",
-	log.Printf("| %d | %s | %s | %s | %s\n", code, c.IP(), c.Method(), c.Path(), err.Error())
+	log.Error().Err(err).Str("call info", fmt.Sprintf("| %d | %s | %s | %s | %s\n", code, c.IP(), c.Method(), c.Path(), err.Error()))
 	return c.Status(code).SendString(err.Error())
 }
 
@@ -292,6 +292,6 @@ func CustomStackTraceHandler(_ *fiber.Ctx, e interface{}) {
 			break
 		}
 	}
-	log.Printf("| (%s) panic: %v \n", failPoint, e)
+	log.Debug().Any("stacktrace", stackTrace).Str("failPoint", failPoint).Msgf("panic: %v", e)
 	_, _ = os.Stderr.WriteString(fmt.Sprintf("%s\n", debug.Stack())) //nolint:errcheck // This will never fail
 }
