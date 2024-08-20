8217c64d39afd37c8971a9ec74c65ec02ca99b7f
Nick <148735107+nick-bisonai@users.noreply.github.com>
2024-08-20 09:13:37 +0900

    feat: update dalstats query & add migrations (#2144)

---
+++ node/migrations/node/000032_add_missing_index.down.sql
@@ -0,0 +1,5 @@
+DROP INDEX IF EXISTS local_aggregates_config_id_idx;
+DROP INDEX IF EXISTS local_aggregates_timestamp_idx;
+
+DROP INDEX IF EXISTS global_aggregates_config_id_idx;
+DROP INDEX IF EXISTS global_aggregates_timestamp_idx;
