b2f5f98538983888b85333067b3311d6479e3b1f
nick <nick@bisonai.com>
2024-08-28 15:17:21 +0900

    refactor: rename function name

--- node/pkg/utils/pool/pool.go
+++ node/pkg/utils/pool/pool.go
@@ -24,11 +24,11 @@ func (p *Pool) Run(ctx context.Context) {
 	p.IsRunning = true
 
 	for i := 0; i < p.workerCount; i++ {
-		go p.worker(poolCtx)
+		go p.work(poolCtx)
 	}
 }
 
-func (p *Pool) worker(ctx context.Context) {
+func (p *Pool) work(ctx context.Context) {
 	for {
 		select {
 		case job := <-p.jobChannel:
