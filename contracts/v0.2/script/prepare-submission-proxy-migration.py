cf8a990d9a0ec910fbe4159e6d600d258bc5d023
nick <nick@bisonai.com>
2024-11-12 15:58:02 +0900

    feat: deploy baobab

--- contracts/v0.2/script/prepare-submission-proxy-migration.py
+++ contracts/v0.2/script/prepare-submission-proxy-migration.py
@@ -5,19 +5,8 @@ from datetime import datetime
 
 file_path = "./addresses/datafeeds-addresses.json"
 
-def main():
-    with open(file_path, "r") as f:
-        data = json.load(f)
-
-
-    parser = argparse.ArgumentParser(description="parse args")
-    parser.add_argument('--network', type=str, default="baobab")
-
-    args = parser.parse_args()
-    network = args.network
-
+def prepareForSubmissionProxy(data, network):
     entries = []
-
     for symbol, networks in data.items():
         name = symbol
         for key, item in networks.items():
@@ -35,12 +24,45 @@ def main():
         "updateFeed": entries
     }
 
+    return result
+
+def prepareForFeed(data, network):
+    entries = []
+    for _, networks in data.items():
+        for key, item in networks.items():
+            if key == network:
+                entries.append(item["feed"])
+    result = {
+        "updateSubmitter": {
+            "submitter": "",
+            "feedAddresses": entries
+        }
+    }
+    return result
+
+def main():
+    with open(file_path, "r") as f:
+        data = json.load(f)
+
+    parser = argparse.ArgumentParser(description="parse args")
+    parser.add_argument('--network', type=str, default="baobab")
+    parser.add_argument('--contract', type=str, default="SubmissionProxy")
+
+    args = parser.parse_args()
+    network = args.network
+    contract = args.contract
+
+    if contract == "SubmissionProxy":
+        result = prepareForSubmissionProxy(data, network)
+    elif contract == "Feed":
+        result = prepareForFeed(data, network)
+    else:
+        raise ValueError("Invalid contract name")
+
     timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
 
     # Create the file path with the timestamp
-    write_file_path = f"./migration/{network}/SubmissionProxy/{timestamp}_update.json"
-
-    print(write_file_path)
+    write_file_path = f"./migration/{network}/{contract}/{timestamp}_update.json"
 
     # Ensure the directories exist
     os.makedirs(os.path.dirname(write_file_path), exist_ok=True)
