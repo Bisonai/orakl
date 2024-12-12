9aff1ded334c6b460a4890d9eacee73d2a10bc52
nick <nick@bisonai.com>
2024-12-12 18:11:33 +0900

    fix: migrate to coinex v2

--- node/pkg/websocketfetcher/providers/coinex/utils.go
+++ node/pkg/websocketfetcher/providers/coinex/utils.go
@@ -10,34 +10,34 @@ import (
 func ResponseToFeedDataList(data Response, feedMap map[string][]int32) ([]*common.FeedData, error) {
 	feedDataList := []*common.FeedData{}
 
-	for _, item := range data.Params {
-		for key, value := range item {
-			id, exists := feedMap[key]
-			if !exists {
-				log.Warn().Str("Player", "Coinex").Str("key", key).Msg("feed not found")
-				continue
-			}
-			price, err := common.PriceStringToFloat64(value.Last)
-			if err != nil {
-				log.Error().Str("Player", "Coinex").Err(err).Msg("error in PriceStringToFloat64")
-				continue
-			}
-			volume, err := common.VolumeStringToFloat64(value.Volume)
-			if err != nil {
-				log.Error().Str("Player", "Coinex").Err(err).Msg("error in VolumeStringToFloat64")
-				continue
-			}
-			timestamp := time.Now()
+	for _, item := range data.Data.StateList {
 
-			for _, id := range id {
-				feedData := new(common.FeedData)
-				feedData.FeedID = id
-				feedData.Value = price
-				feedData.Timestamp = &timestamp
-				feedData.Volume = volume
-				feedDataList = append(feedDataList, feedData)
-			}
+		id, exists := feedMap[item.Market]
+		if !exists {
+			log.Warn().Str("Player", "Coinex").Str("key", item.Market).Msg("feed not found")
+			continue
 		}
+		price, err := common.PriceStringToFloat64(item.Last)
+		if err != nil {
+			log.Error().Str("Player", "Coinex").Err(err).Msg("error in PriceStringToFloat64")
+			continue
+		}
+		volume, err := common.VolumeStringToFloat64(item.Volume)
+		if err != nil {
+			log.Error().Str("Player", "Coinex").Err(err).Msg("error in VolumeStringToFloat64")
+			continue
+		}
+		timestamp := time.Now()
+
+		for _, id := range id {
+			feedData := new(common.FeedData)
+			feedData.FeedID = id
+			feedData.Value = price
+			feedData.Timestamp = &timestamp
+			feedData.Volume = volume
+			feedDataList = append(feedDataList, feedData)
+		}
+
 	}
 
 	return feedDataList, nil
