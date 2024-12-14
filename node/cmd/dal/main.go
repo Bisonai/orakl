8bfa0b1efd14ec98ecfe456aeb35a47310a96b8e
nick <nick@bisonai.com>
2024-12-14 18:35:31 +0900

    feat: load env from file

--- node/cmd/dal/main.go
+++ node/cmd/dal/main.go
@@ -2,15 +2,32 @@ package main
 
 import (
 	"context"
+	"flag"
 
 	"bisonai.com/miko/node/pkg/dal"
 	"bisonai.com/miko/node/pkg/logscribeconsumer"
+	"github.com/joho/godotenv"
 	"github.com/rs/zerolog/log"
 )
+
+func LoadEnvFromFile() {
+	envFile := flag.String("env", "", "env file")
+	flag.Parse()
+
+	if *envFile != "" {
+		log.Info().Msgf("loading env file: %s", *envFile)
+		err := godotenv.Load(*envFile)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
 
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
+
+	LoadEnvFromFile()
 
 	err := logscribeconsumer.Start(ctx, logscribeconsumer.WithStoreService("dal"))
 	if err != nil {
