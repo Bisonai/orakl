564c117e6b58c67a30747f669c770a0a9ab8c425
nick <nick@bisonai.com>
2025-05-14 18:14:31 +0900

    fix: go mod tidy

--- node/script/test_websocketFetcher/main.go
+++ node/script/test_websocketFetcher/main.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 
 	"bisonai.com/miko/node/pkg/websocketfetcher/common"
-	"bisonai.com/miko/node/pkg/websocketfetcher/providers/bybit"
+	"bisonai.com/miko/node/pkg/websocketfetcher/providers/btse"
 	"github.com/rs/zerolog/log"
 )
 
@@ -16,35 +16,35 @@ func main() {
 	feed := []common.Feed{
 		{
 			ID:         1,
-			Name:       "bybit-wss-BTC-USDT",
-			Definition: json.RawMessage(`{"type": "wss", "provider": "bybit", "base": "btc", "quote": "usdt"}`),
+			Name:       "btse-wss-BTC-USDT",
+			Definition: json.RawMessage(`{"type": "wss", "provider": "btse", "base": "btc", "quote": "usdt"}`),
 			ConfigID:   1,
 		},
 		{
 			ID:         2,
-			Name:       "bybit-wss-ETH-USDT",
-			Definition: json.RawMessage(`{"type": "wss", "provider": "bybit", "base": "eth", "quote": "usdt"}`),
+			Name:       "btse-wss-ETH-USDT",
+			Definition: json.RawMessage(`{"type": "wss", "provider": "btse", "base": "eth", "quote": "usdt"}`),
 			ConfigID:   2,
 		},
 		{
 			ID:         3,
-			Name:       "bybit-wss-ADA-USDT",
-			Definition: json.RawMessage(`{"type": "wss", "provider": "bybit", "base": "ada", "quote": "usdt"}`),
+			Name:       "btse-wss-ADA-USDT",
+			Definition: json.RawMessage(`{"type": "wss", "provider": "btse", "base": "ada", "quote": "usdt"}`),
 			ConfigID:   3,
 		},
 		{
 			ID:         4,
-			Name:       "bybit-wss-BNB-USDT",
-			Definition: json.RawMessage(`{"type": "wss", "provider": "bybit", "base": "bnb", "quote": "usdt"}`),
+			Name:       "btse-wss-BNB-USDT",
+			Definition: json.RawMessage(`{"type": "wss", "provider": "btse", "base": "bnb", "quote": "usdt"}`),
 			ConfigID:   4,
 		},
 	}
 	feedMap := common.GetWssFeedMap(feed)
 
 	ch := make(chan *common.FeedData)
-	fetcher, err := bybit.New(ctx, common.WithFeedDataBuffer(ch), common.WithFeedMaps(feedMap["bybit"]))
+	fetcher, err := btse.New(ctx, common.WithFeedDataBuffer(ch), common.WithFeedMaps(feedMap["btse"]))
 	if err != nil {
-		log.Error().Err(err).Msg("failed to create bybit fetcher")
+		log.Error().Err(err).Msg("failed to create btse fetcher")
 		return
 	}
 
