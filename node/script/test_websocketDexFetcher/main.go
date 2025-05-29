d0e768940e3239466981fbf13c8b2f0675927492
nick <nick@bisonai.com>
2025-05-29 11:53:41 +0900

    feat: deploy contracts

--- node/script/test_websocketDexFetcher/main.go
+++ node/script/test_websocketDexFetcher/main.go
@@ -8,7 +8,7 @@ import (
 
 	"bisonai.com/miko/node/pkg/chain/websocketchainreader"
 	"bisonai.com/miko/node/pkg/websocketfetcher/common"
-	"bisonai.com/miko/node/pkg/websocketfetcher/providers/capybara"
+	"bisonai.com/miko/node/pkg/websocketfetcher/providers/uniswap"
 	"github.com/rs/zerolog/log"
 )
 
@@ -18,59 +18,16 @@ func main() {
 	feeds := []common.Feed{
 		{
 			ID:   1,
-			Name: "Capybara-WALK-USDT",
+			Name: "Dragonswap-ZP-KAIA",
 			Definition: json.RawMessage(`{
 				"chainId": "8217",
-				"address": "0x872e7e7422bcacdcb37f7fffb0cfe3f2f0d6c546",
-				"type": "CapybaraPool",
+				"address": "0xb1bcb17975407f2e9dbae748366b95e12f163a03",
+				"type": "UniswapPool",
 				"token0Decimals": 18,
-				"token1Decimals": 6,
-				"token0Address": "0x976232eb7eb92287ff06c5d145bd0d1c033eca58",
-				"token1Address": "0x5c13e303a62fc5dedf5b52d66873f2e59fedadc2"
+				"token1Decimals": 18,
+				"reciprocal": true
 				}`),
 			ConfigID: 1,
-		},
-		{
-			ID:   2,
-			Name: "Capybara-KRWO-USDT",
-			Definition: json.RawMessage(`{
-				"chainId": "8217",
-				"address": "0x4b63ec6284810f62cecba6f03cf17413b0f4cec3",
-				"type": "CapybaraPool",
-				"token0Decimals": 6,
-				"token1Decimals": 6,
-				"token0Address": "0x7fc692699f2216647a0e06225d8bdf8cdee40e7f",
-				"token1Address": "0x5c13e303a62fc5dedf5b52d66873f2e59fedadc2"
-			}`),
-			ConfigID: 2,
-		},
-		{
-			ID:   3,
-			Name: "Capybara-KAIA-USDT(Wormhole)",
-			Definition: json.RawMessage(`{
-				"chainId": "8217",
-				"address": "0x6389dbfa1427a3b0a89cddc7ea9bbda6e73dece7",
-				"type": "CapybaraPool",
-				"token0Decimals": 18,
-				"token1Decimals": 6,
-				"token0Address": "0x19aac5f612f524b754ca7e7c41cbfa2e981a4432",
-				"token1Address": "0x5c13e303a62fc5dedf5b52d66873f2e59fedadc2"
-			}`),
-			ConfigID: 3,
-		},
-		{
-			ID:   4,
-			Name: "Capybara-KAIA-USDT(Stargate)",
-			Definition: json.RawMessage(`{
-				"chainId": "8217",
-				"address": "0x1de1578476d9b4237f963eca5d37500fc33df3d1",
-				"type": "CapybaraPool",
-				"token0Decimals": 18,
-				"token1Decimals": 6,
-				"token0Address": "0x19aac5f612f524b754ca7e7c41cbfa2e981a4432",
-				"token1Address": "0x9025095263d1e548dc890a7589a4c78038ac40ab"
-			}`),
-			ConfigID: 4,
 		},
 	}
 
@@ -87,7 +44,7 @@ func main() {
 	}
 
 	ch := make(chan *common.FeedData)
-	fetcher := capybara.New(common.WithDexFeedDataBuffer(ch), common.WithWebsocketChainReader(chainReader), common.WithFeeds(feeds))
+	fetcher := uniswap.New(common.WithDexFeedDataBuffer(ch), common.WithWebsocketChainReader(chainReader), common.WithFeeds(feeds))
 	fetcher.Run(ctx)
 	for {
 		select {
