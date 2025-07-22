1eb7139f589f1e699146c659348aa71eead581b5
nick <nick@bisonai.com>
2025-07-22 11:40:31 +0900

    chore: reduce

--- node/script/test_websocketFetcher/main.go
+++ node/script/test_websocketFetcher/main.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 
 	"bisonai.com/miko/node/pkg/websocketfetcher/common"
-	"bisonai.com/miko/node/pkg/websocketfetcher/providers/btse"
+	"bisonai.com/miko/node/pkg/websocketfetcher/providers/gateio"
 	"github.com/rs/zerolog/log"
 )
 
@@ -16,35 +16,35 @@ func main() {
 	feed := []common.Feed{
 		{
 			ID:         1,
-			Name:       "btse-wss-BTC-USDT",
-			Definition: json.RawMessage(`{"type": "wss", "provider": "btse", "base": "btc", "quote": "usdt"}`),
+			Name:       "gateio-wss-BTC-USDT",
+			Definition: json.RawMessage(`{"type": "wss", "provider": "gateio", "base": "btc", "quote": "usdt"}`),
 			ConfigID:   1,
 		},
 		{
 			ID:         2,
-			Name:       "btse-wss-ETH-USDT",
-			Definition: json.RawMessage(`{"type": "wss", "provider": "btse", "base": "eth", "quote": "usdt"}`),
+			Name:       "gateio-wss-ETH-USDT",
+			Definition: json.RawMessage(`{"type": "wss", "provider": "gateio", "base": "eth", "quote": "usdt"}`),
 			ConfigID:   2,
 		},
 		{
 			ID:         3,
-			Name:       "btse-wss-ADA-USDT",
-			Definition: json.RawMessage(`{"type": "wss", "provider": "btse", "base": "ada", "quote": "usdt"}`),
+			Name:       "gateio-wss-ADA-USDT",
+			Definition: json.RawMessage(`{"type": "wss", "provider": "gateio", "base": "ada", "quote": "usdt"}`),
 			ConfigID:   3,
 		},
 		{
 			ID:         4,
-			Name:       "btse-wss-BNB-USDT",
-			Definition: json.RawMessage(`{"type": "wss", "provider": "btse", "base": "bnb", "quote": "usdt"}`),
+			Name:       "gateio-wss-BNB-USDT",
+			Definition: json.RawMessage(`{"type": "wss", "provider": "gateio", "base": "bnb", "quote": "usdt"}`),
 			ConfigID:   4,
 		},
 	}
 	feedMap := common.GetWssFeedMap(feed)
 
 	ch := make(chan *common.FeedData)
-	fetcher, err := btse.New(ctx, common.WithFeedDataBuffer(ch), common.WithFeedMaps(feedMap["btse"]))
+	fetcher, err := gateio.New(ctx, common.WithFeedDataBuffer(ch), common.WithFeedMaps(feedMap["gateio"]))
 	if err != nil {
-		log.Error().Err(err).Msg("failed to create btse fetcher")
+		log.Error().Err(err).Msg("failed to create gateio fetcher")
 		return
 	}
 
