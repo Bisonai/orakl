caf014f89de553a68625e62094eae8798c8e9a56
Nick <148735107+nick-bisonai@users.noreply.github.com>
2025-09-12 16:02:42 +0900

    chore: deploy m7 contracts (#2465)

--- contracts/v0.1/scripts/generate-contract-addresses.js
+++ contracts/v0.1/scripts/generate-contract-addresses.js
@@ -6,41 +6,12 @@ const { loadJson, storeJson } = require('./utils.cjs')
 const ValidChains = ['baobab', 'cypress']
 const deploymentsPath = path.join(__dirname, '../deployments/')
 
-const fetchTags = async () => {
-  const url = 'https://config.orakl.network/cypress_adapters.json'
-  let tags = {}
-
-  await axios
-    .get(url)
-    .catch((error) => {
-      console.error(`Error fetching tags: ${error}`)
-    })
-    .then((res) => {
-      res.data.result.forEach((feed) => {
-        const numFeeds = feed.feeds.length
-        let tag = ''
-
-        if (numFeeds > 8) {
-          tag = 'premium'
-        } else if (numFeeds > 5) {
-          tag = 'standard'
-        } else {
-          tag = 'basic'
-        }
-        tags[feed.name] = tag
-      })
-    })
-
-  return tags
-}
-
 const isValidPath = (_path) => {
   const fileName = path.basename(_path)
   return path.extname(fileName) === '.json'
 }
 
-const readDeployments = async (folderPath, tags) => {
-  const dataFeeds = {}
+const readDeployments = async (folderPath) => {
   const others = {}
 
   const readFolder = async (_folderPath) => {
@@ -54,6 +25,9 @@ const readDeployments = async (folderPath, tags) => {
         if (stats.isDirectory()) {
           await readFolder(filePath)
         } else if (isValidPath(filePath)) {
+          if (file.startsWith('Aggregator_') || file.startsWith('AggregatorProxy_')) {
+            return
+          }
           const network = filePath.replace(deploymentsPath, '').split('/')[0]
           if (!ValidChains.includes(network)) {
             return
@@ -66,31 +40,15 @@ const readDeployments = async (folderPath, tags) => {
 
           let contractName = path.basename(file, '.json')
 
-          if (contractName.startsWith('Aggregator') && contractName.includes('_')) {
-            const splitted = contractName.replace(' ', '').split('_')
-            const contractType = splitted[0]
-            const pairName = splitted[1]
-
-            if (!dataFeeds[pairName]) {
-              dataFeeds[pairName] = {}
-            }
-            if (!dataFeeds[pairName][network]) {
-              dataFeeds[pairName][network] = {}
-            }
-
-            dataFeeds[pairName][network][convertContractType(contractType)] = address
-            dataFeeds[pairName]['tag'] = tags[pairName]
-          } else {
-            if (!others[network]) {
-              others[network] = {}
-            }
-            const splitted = contractName.replace(' ', '').split('_')
-            if (splitted.length > 1) {
-              splitted.pop()
-              contractName = splitted.join('_')
-            }
-            others[network][contractName] = address
+          if (!others[network]) {
+            others[network] = {}
           }
+          const splitted = contractName.replace(' ', '').split('_')
+          if (splitted.length > 1) {
+            splitted.pop()
+            contractName = splitted.join('_')
+          }
+          others[network][contractName] = address
         }
       }),
     )
@@ -102,7 +60,7 @@ const readDeployments = async (folderPath, tags) => {
     console.error(`Failed to read directory: ${error}`)
   }
 
-  return { dataFeeds, others }
+  return others
 }
 
 async function readAddressFromFile(filePath) {
@@ -115,23 +73,9 @@ async function readAddressFromFile(filePath) {
   }
 }
 
-const convertContractType = (contractType) => {
-  if (contractType == 'Aggregator') {
-    return 'aggregator'
-  } else if (contractType == 'AggregatorProxy') {
-    return 'proxy'
-  } else {
-    return 'unknown'
-  }
-}
-
 async function main() {
-  const tags = await fetchTags()
-  const { dataFeeds, others } = await readDeployments(deploymentsPath, tags)
-  await storeJson(
-    path.join(deploymentsPath, 'datafeeds-addresses.json'),
-    JSON.stringify(dataFeeds, null, 2),
-  )
+  const others = await readDeployments(deploymentsPath)
+
   await storeJson(
     path.join(deploymentsPath, 'other-addresses.json'),
     JSON.stringify(others, null, 2),
