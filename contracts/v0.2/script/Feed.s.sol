a11ad69c9aef0463fa8479d7f31727e6bf62e180
Nick <148735107+nick-bisonai@users.noreply.github.com>
2025-12-23 15:32:34 +0900

    feat: deploy contract (#2487)

--- contracts/v0.2/script/Feed.s.sol
+++ contracts/v0.2/script/Feed.s.sol
@@ -118,7 +118,7 @@ contract DeployFeed is Script {
         }
     }
 
-     function compareStrings(string memory s1, string memory s2) public pure returns (bool) {
+    function compareStrings(string memory s1, string memory s2) public pure returns (bool) {
         return keccak256(abi.encodePacked(s1)) == keccak256(abi.encodePacked(s2));
     }
 }
