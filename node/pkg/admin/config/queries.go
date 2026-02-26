46e6bf010a0fd2ba39b7802d5ba31602c450f9fa
hamin <hamin@bisonai.com>
2026-02-26 14:13:04 +0900

    refactor: update SELECT queries to include specific columns for better clarity and performance

--- node/pkg/admin/config/queries.go
+++ node/pkg/admin/config/queries.go
@@ -2,8 +2,8 @@ package config
 
 const (
 	InsertConfigQuery     = "INSERT INTO configs (name, fetch_interval, aggregate_interval, submit_interval, decimals, feed_data_freshness) VALUES (@name, @fetch_interval, @aggregate_interval, @submit_interval, @decimals, @feed_data_freshness) RETURNING *"
-	SelectConfigQuery     = "SELECT * FROM configs"
-	SelectConfigByIdQuery = "SELECT * FROM configs WHERE id = @id"
+	SelectConfigQuery     = "SELECT id, name, fetch_interval, aggregate_interval, submit_interval, decimals, feed_data_freshness FROM configs"
+	SelectConfigByIdQuery = "SELECT id, name, fetch_interval, aggregate_interval, submit_interval, decimals, feed_data_freshness FROM configs WHERE id = @id"
 	DeleteConfigQuery     = "DELETE FROM configs WHERE id = @id RETURNING *"
 	InsertFeedQuery       = "INSERT INTO feeds (name, definition, config_id) VALUES (@name, @definition, @config_id)"
 	DeleteFeedQuery       = "DELETE FROM feeds WHERE id = @id RETURNING *"
